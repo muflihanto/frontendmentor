8e4a9df85e514882509a30fd9533df52634d9f5d
muflihanto <muflihanto@gmail.com>
2022-12-19 06:55:02 +0700

    add simple home page

---
+++ utils/getPages.js
@@ -0,0 +1,29 @@
+import fs from "fs";
+import path from "path";
+
+export default function getPath() {
+  const pagesDir = path.join(process.cwd(), "pages");
+  const pageNames = fs.readdirSync(pagesDir);
+  const excluded = ["index.js", "api"];
+  const filteredPages = pageNames.filter((page) => {
+    return page[0] !== "_" && !excluded.includes(page);
+  });
+  const titleCase = (str) => {
+    return str
+      .toLowerCase()
+      .split(" ")
+      .map(function (word) {
+        return word.replace(word[0], word[0].toUpperCase());
+      })
+      .join(" ");
+  };
+  const pageTitles = filteredPages.map((page) => {
+    return titleCase(page.split("-").join(" ").slice(0, -3));
+  });
+  return {
+    titles: pageTitles,
+    paths: filteredPages.map((page) => {
+      return page.slice(0, -3);
+    }),
+  };
+}
