15c6eb0a6ae545ba564c91a14d49677c29e72bf7
muflihanto <muflihanto@gmail.com>
2023-03-14 06:24:19 +0700

    add four-card-feature-section initial desktop styling

--- pages/four-card-feature-section.tsx
+++ pages/four-card-feature-section.tsx
@@ -14,7 +14,7 @@ const FourCardFeature = () => {
       <Head>
         <title>Frontend Mentor | Four card feature section</title>
       </Head>
-      <div className="App font-poppins bg-four-card-neutral-100 relative flex min-h-[100svh] items-center justify-center pt-[84px] pb-[78px]">
+      <div className="App font-poppins bg-four-card-neutral-100 relative flex min-h-[100svh] items-center justify-center pt-[84px] pb-[78px] lg:pt-[80px] lg:pb-[50px]">
         <Main />
         <Footer />
         {/* <Slider basePath="/four-card-feature-section/design/" /> */}
@@ -33,14 +33,14 @@ function Main() {
 
   return (
     <div className="flex max-w-[375px] flex-col items-center px-8 lg:max-w-none">
-      <div className="text-four-card-neutral-200">
-        <h1 className="text-four-card-neutral-300 text-center text-[24px] font-extralight tracking-[0.2px]">
+      <div className="text-four-card-neutral-200 lg:max-w-[520px]">
+        <h1 className="text-four-card-neutral-300 text-center text-[24px] font-extralight tracking-[0.2px] lg:text-[36px] lg:leading-[51px]">
           Reliable, efficient delivery
           <span className="font-semibold"> Powered by Technology</span>
         </h1>
-        <p className="mt-4 text-center text-[15px] leading-[25px] tracking-[0.1px]">Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful</p>
+        <p className="mt-4 text-center text-[15px] leading-[25px] tracking-[0.1px] lg:mt-[17px]">Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful</p>
       </div>
-      <div className="mt-[75px] max-lg:flex max-lg:flex-col max-lg:gap-y-[24px] lg:grid lg:grid-cols-[repeat(3,minmax(0px,350px))] lg:grid-rows-[repeat(4,minmax(0px,125px))] lg:gap-x-[28px] lg:gap-y-[30px]">
+      <div className="mt-[75px] max-lg:flex max-lg:flex-col max-lg:gap-y-[24px] lg:mt-[33px] lg:grid lg:grid-cols-[repeat(3,minmax(0px,350px))] lg:grid-rows-[repeat(4,minmax(0px,125px))] lg:gap-[30px]">
         {Object.keys(cards).map((el, index) => {
           const variant = el as CardVariant;
           return (
@@ -60,16 +60,16 @@ function Main() {
 }
 
 const Card: FC<CardProps> & CardSubComponents = ({ className, children }) => {
-  return <div className={`relative flex h-[222px] flex-col overflow-hidden rounded-lg bg-white px-[28px] pt-[31px] pb-[28px] shadow-xl shadow-[hsla(180,51%,29%,0.13)] before:absolute before:top-0 before:left-0 before:h-1 before:w-full lg:row-span-2 lg:h-[250px] ${className}`}>{children}</div>;
+  return <div className={`relative flex h-[222px] flex-col overflow-hidden rounded-lg bg-white px-[28px] pt-[31px] pb-[28px] shadow-xl shadow-[hsla(180,51%,29%,0.13)] before:absolute before:top-0 before:left-0 before:h-1 before:w-full lg:row-span-2 lg:h-[250px] lg:px-[32px] lg:pt-[36px] lg:pb-[32px] ${className}`}>{children}</div>;
 };
 
 const Heading: React.FC<{ children: ReactNode }> = ({ children }) => {
-  return <h2 className="text-four-card-neutral-300 text-[20px] font-bold leading-none tracking-[-0.2px]">{children}</h2>;
+  return <h2 className="text-four-card-neutral-300 text-[20px] font-semibold leading-none tracking-[-0.2px]">{children}</h2>;
 };
 Card.Heading = Heading;
 
 const Body: React.FC<{ children: ReactNode }> = ({ children }) => {
-  return <p className="text-four-card-neutral-200 mt-2 px-[1px] text-[13px] leading-[23px] tracking-[0.1px]">{children}</p>;
+  return <p className="text-four-card-neutral-200 mt-2 px-[1px] text-[13px] leading-[23px] tracking-[0.1px] lg:mt-[13px] lg:px-0 lg:tracking-[0.05px]">{children}</p>;
 };
 Card.Body = Body;
 
@@ -310,7 +310,7 @@ const Icon: FC<{ variant: CardVariant }> = ({ variant }) => {
     <svg
       xmlns="http://www.w3.org/2000/svg"
       viewBox="0 0 64 64"
-      className="mt-auto h-[57px] self-end"
+      className="mt-auto h-[57px] self-end lg:h-[64px]"
     >
       {icons[variant]}
     </svg>
@@ -320,7 +320,7 @@ const Icon: FC<{ variant: CardVariant }> = ({ variant }) => {
 function Footer() {
   return (
     <>
-      <footer className="absolute bottom-3 w-full text-center text-[11px] [&_a]:font-bold [&_a]:text-indigo-600 [&_a]:underline [&_a]:decoration-red-500 [&_a]:decoration-wavy">
+      <footer className="absolute bottom-3 w-full text-center text-[11px] [&_a]:font-semibold [&_a]:text-indigo-600 [&_a]:underline [&_a]:decoration-red-500 [&_a]:decoration-wavy">
         Challenge by{" "}
         <a
           href="https://www.frontendmentor.io?ref=challenge"
