c2a2cad48a0832fddc9f1e034018184a42b83f9a
muflihanto <muflihanto@gmail.com>
2023-03-16 13:08:40 +0700

    add single-price-grid-component initial desktop styling

--- pages/single-price-grid-component.tsx
+++ pages/single-price-grid-component.tsx
@@ -20,27 +20,27 @@ export default function SinglePriceGrid() {
 
 function Main() {
   return (
-    <div className="shadow-single-price-primary-cyan/20 grid w-[calc(100vw-64px)] max-w-[calc(375px-64px)] grid-cols-1 grid-rows-[repeat(3,auto)] overflow-hidden rounded shadow-xl">
-      <div className="h-[267px] bg-white px-6 pt-[29px] leading-none tracking-[-0.2px]">
-        <h1 className="text-single-price-primary-cyan text-[20px] font-bold">Join our community</h1>
-        <p className="text-single-price-primary-yellow mt-[26px] text-[15px] font-bold leading-[20px]">30-day, hassle-free money back guarantee</p>
-        <p className="text-single-price-neutral-200 mt-[16px] text-[14px] leading-[26px] tracking-[-0.15px]">Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who are serious about honing their skills.</p>
+    <div className="shadow-single-price-primary-cyan/20 grid w-[calc(100vw-64px)] max-w-[calc(375px-64px)] grid-cols-1 grid-rows-[repeat(3,auto)] overflow-hidden rounded shadow-xl lg:h-[475px] lg:w-[635px] lg:max-w-none lg:grid-cols-2 lg:grid-rows-[216px,auto] lg:rounded-[8px]">
+      <div className="h-[267px] bg-white px-6 pt-[29px] leading-none tracking-[-0.2px] lg:col-span-2 lg:h-auto lg:px-[40px] lg:pt-[42px]">
+        <h1 className="text-single-price-primary-cyan text-[20px] font-bold lg:text-[24px]">Join our community</h1>
+        <p className="text-single-price-primary-yellow mt-[26px] text-[15px] font-bold leading-[20px] lg:mt-[27px] lg:text-[18px]">30-day, hassle-free money back guarantee</p>
+        <p className="text-single-price-neutral-200 mt-[16px] text-[14px] leading-[26px] tracking-[-0.15px] lg:mt-[11px] lg:text-[16px]">Gain access to our full library of tutorials along with expert code reviews. Perfect for any developers who are serious about honing their skills.</p>
       </div>
-      <div className="bg-single-price-primary-cyan flex h-[228px] flex-col px-6 pt-[26px] pb-6 leading-none tracking-[-0.2px]">
+      <div className="bg-single-price-primary-cyan flex h-[228px] flex-col px-6 pt-[26px] pb-6 leading-none tracking-[-0.2px] lg:h-auto lg:px-[40px] lg:pt-[42px] lg:pb-[39px]">
         <h2 className="text-[18px] font-bold text-white">Monthly Subscription</h2>
-        <p className="mt-[21px] flex items-center gap-3">
+        <p className="mt-[21px] flex items-center gap-3 lg:mt-[22px]">
           <span className="text-[32px] font-bold text-white">&#36;29</span>
           <span className="text-single-price-neutral-100/60">per month</span>
         </p>
         <p className="text-single-price-neutral-100/90 mt-[11px] tracking-[-0.15px]">Full access for less than &#36;1 a day</p>
         <a
           href=""
-          className="bg-single-price-primary-yellow mt-auto flex h-12 items-center justify-center rounded font-bold text-white/75 shadow-lg shadow-black/10"
+          className="bg-single-price-primary-yellow focus-visible:outline-single-price-primary-yellow mt-auto flex h-12 items-center justify-center rounded font-bold text-white/75 shadow-lg shadow-black/10 hover:bg-[hsl(71,73%,48%)] focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2"
         >
           Sign Up
         </a>
       </div>
-      <div className="bg-single-price-primary-cyan/80 flex h-[227px] flex-col px-6 pt-[26px] pb-[27px] leading-none tracking-[-0.2px]">
+      <div className="bg-single-price-primary-cyan/80 flex h-[227px] flex-col px-6 pt-[26px] pb-[27px] leading-none tracking-[-0.2px] lg:h-auto lg:px-[40px] lg:py-[42px]">
         <h2 className="text-[18px] font-bold text-white">Why Us</h2>
         <ul className="mt-auto flex flex-col gap-[6px] [&>li]:text-[14px] [&>li]:text-white/60">
           <li>Tutorials by industry experts</li>
