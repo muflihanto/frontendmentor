28e0048b83378f6847172c251727d6ff6e756b70
muflihanto <muflihanto@gmail.com>
2023-03-18 08:49:39 +0700

    + import article-preview-component starter files * fix huddle-landing-page-with-single-introductory-section styling

--- pages/huddle-landing-page-with-single-introductory-section.tsx
+++ pages/huddle-landing-page-with-single-introductory-section.tsx
@@ -12,7 +12,10 @@ export default function HuddleLandingPageWithSingleIntroductorySection() {
       <div className="App font-open-sans bg-huddle-intro-primary-violet relative flex min-h-[100svh] flex-col items-center bg-[url('/huddle-landing-page-with-single-introductory-section/images/bg-mobile.svg')] bg-contain bg-[position:center_top] bg-no-repeat pt-[32px] pb-[40.15px] lg:bg-[url('/huddle-landing-page-with-single-introductory-section/images/bg-desktop.svg')] lg:bg-[position:center_left] lg:py-[55px]">
         <Main />
         <Footer />
-        {/* <Slider basePath="/huddle-landing-page-with-single-introductory-section/design/" /> */}
+        {/* <Slider
+          basePath="/huddle-landing-page-with-single-introductory-section/design/"
+          absolutePath="/huddle-landing-page-with-single-introductory-section/design/active-states.jpg"
+        /> */}
       </div>
     </>
   );
@@ -29,7 +32,7 @@ function Main() {
           <p className="mt-[13px] text-center text-white/75 lg:mt-[20px] lg:text-left lg:text-[18px]">Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.</p>
           <a
             href=""
-            className="text-huddle-intro-primary-violet font-poppins mx-auto mt-6 flex h-[40px] w-[200px] items-center justify-center rounded-full bg-white pt-[2.2px] text-[12px] shadow-xl focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white lg:ml-0 lg:h-[56px] lg:pt-0 lg:text-[18px]"
+            className="text-huddle-intro-primary-violet font-poppins hover:bg-huddle-intro-primary-magenta mx-auto mt-6 flex h-[40px] w-[200px] items-center justify-center rounded-full bg-white pt-[2.2px] text-[12px] shadow-xl hover:text-white focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-white lg:ml-0 lg:h-[56px] lg:pt-0 lg:text-[18px]"
           >
             Register
           </a>
@@ -42,7 +45,7 @@ function Main() {
 
 function Footer() {
   return (
-    <p className="[&_a]:text-huddle-intro-primary-magenta absolute bottom-3 z-20 w-full text-center text-[11px] text-white lg:bottom-10 lg:left-20 lg:text-left lg:text-[13px] [&_a]:font-bold [&_a]:underline [&_a]:decoration-white/50 [&_a]:decoration-wavy">
+    <p className="[&_a]:text-huddle-intro-primary-magenta absolute bottom-3 z-20 w-full text-center text-[11px] text-white lg:bottom-10 lg:px-20 lg:text-left lg:text-[13px] [&_a]:font-bold [&_a]:underline [&_a]:decoration-white/50 [&_a]:decoration-wavy">
       Challenge by{" "}
       <a
         href="https://www.frontendmentor.io?ref=challenge"
@@ -66,7 +69,7 @@ function Footer() {
 
 function SocialMedia() {
   return (
-    <div className="[&_a:hover]:border-huddle-intro-primary-magenta mt-[64px] flex items-center justify-center gap-[10px] place-self-center lg:absolute lg:bottom-[40px] lg:right-[80px] lg:mt-0 lg:gap-[16px] lg:place-self-start [&_a]:flex [&_a]:aspect-square [&_a]:w-[28px] [&_a]:items-center [&_a]:justify-center [&_a]:rounded-full [&_a]:border lg:[&_a]:w-10">
+    <div className="[&_a:hover]:border-huddle-intro-primary-magenta mt-[64px] flex items-center justify-center gap-[10px] place-self-center lg:absolute lg:bottom-[40px] lg:right-[80px] lg:z-40 lg:mt-0 lg:gap-[16px] lg:place-self-start [&_a]:flex [&_a]:aspect-square [&_a]:w-[28px] [&_a]:items-center [&_a]:justify-center [&_a]:rounded-full [&_a]:border lg:[&_a]:w-10">
       <a
         href=""
         className="group"
---
+++ pages/jotai.tsx
@@ -0,0 +1,87 @@
+import { atom, useAtom, Provider, useAtomValue } from "jotai";
+import { ChangeEventHandler } from "react";
+import Head from "next/head";
+
+// Create your atoms and derivatives
+const textAtom = atom("hello");
+const countAtom = atom(0);
+const globalCountAtom = atom(0);
+const uppercaseAtom = atom((get) => get(textAtom).toUpperCase());
+
+// Use them anywhere in your app
+const Input = () => {
+  const [text, setText] = useAtom(textAtom);
+  const handleChange: ChangeEventHandler<HTMLInputElement> = (e) => setText(e.target.value);
+
+  return (
+    <input
+      value={text}
+      onChange={handleChange}
+      className="rounded border-2 px-1"
+    />
+  );
+};
+
+const Uppercase = () => {
+  const [uppercase] = useAtom(uppercaseAtom);
+  return <div>Uppercase: {uppercase}</div>;
+};
+
+function ChildComponent() {
+  const [count, setCount] = useAtom(countAtom);
+  const globalAtom = useAtomValue(globalCountAtom);
+  return (
+    <div className="flex gap-2">
+      <p>{globalAtom}</p>
+      <p>{count}</p>
+      <button
+        onClick={() => setCount((v) => v + 1)}
+        className="rounded border px-1"
+      >
+        +
+      </button>
+    </div>
+  );
+}
+
+// Other Component
+function OtherComponent() {
+  const [count, setCount] = useAtom(globalCountAtom);
+  return (
+    <div className="flex gap-2">
+      <p>{count}</p>
+      <button
+        onClick={() => setCount((v) => v + 1)}
+        className="rounded border px-1"
+      >
+        +
+      </button>
+    </div>
+  );
+}
+
+// Now you have the components
+export default function App() {
+  return (
+    <>
+      <Head>
+        <title>Jotai</title>
+      </Head>
+      <Provider>
+        <div className="p-3">
+          <Input />
+          <Uppercase />
+        </div>
+        <div className="p-3">
+          <OtherComponent />
+        </div>
+        <div className="p-3">
+          <ChildComponent />
+        </div>
+        <div className="p-3">
+          <ChildComponent />
+        </div>
+      </Provider>
+    </>
+  );
+}
