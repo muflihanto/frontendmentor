f86ba0732f87f75cf0d70045fbbf1df747d3e345
muflihanto <muflihanto@gmail.com>
2023-03-24 21:50:09 +0700

    add base-apparel-coming-soon initial desktop styling

--- pages/base-apparel-coming-soon.tsx
+++ pages/base-apparel-coming-soon.tsx
@@ -19,11 +19,14 @@ export default function BaseApparelComingSoon() {
       <Head>
         <title>Frontend Mentor | Base Apparel coming soon page</title>
       </Head>
-      <div className="App font-josefin bg-base-apparel-gradient-white-200 relative min-h-[800px]">
+      <div className="App font-josefin bg-base-apparel-gradient-white-200 relative min-h-[800px] lg:min-h-[100svh] lg:bg-[url('/base-apparel-coming-soon/images/bg-pattern-desktop.svg')] lg:bg-[length:auto_100%] lg:bg-left lg:bg-no-repeat">
         <Header />
         <Main />
         <Footer />
-        {/* <Slider basePath="/base-apparel-coming-soon/design" /> */}
+        {/* <Slider
+          basePath="/base-apparel-coming-soon/design"
+          absolutePath="/base-apparel-coming-soon/design/active-states.jpg"
+        /> */}
       </div>
     </>
   );
@@ -31,8 +34,8 @@ export default function BaseApparelComingSoon() {
 
 function Header() {
   return (
-    <header className="flex h-[84px] w-full items-center bg-white px-8">
-      <div className="relative mb-[1px] aspect-[158/33] h-[21px]">
+    <header className="flex h-[84px] w-full items-center bg-white px-8 lg:h-[calc(164/800*100svh)] lg:w-[calc(100vw-min(45vw,61/80*100svh))] lg:bg-transparent lg:px-[min(calc(165/1440*100vw),165px)]">
+      <div className="relative mb-[1px] aspect-[158/33] h-[21px] lg:h-[33px]">
         <Image
           src="/base-apparel-coming-soon/images/logo.svg"
           fill
@@ -62,8 +65,8 @@ function Main() {
   }, [isSubmitSuccessful, reset]);
 
   return (
-    <main className="flex flex-col items-center">
-      <div className="relative aspect-[375/250] h-[250px]">
+    <main className="flex flex-col items-center lg:w-[calc(100vw-min(45vw,61/80*100svh))] lg:items-start lg:px-[min(calc(165/1440*100vw),165px)]">
+      <div className="relative aspect-[375/250] h-[250px] lg:absolute lg:right-0 lg:top-0 lg:aspect-[61/80] lg:h-full lg:max-w-[45vw]">
         <Image
           src="/base-apparel-coming-soon/images/"
           loader={({ src, width }) => {
@@ -71,16 +74,17 @@ function Main() {
           }}
           fill
           alt="Hero Image"
+          className="lg:object-cover lg:object-left"
         />
       </div>
-      <h1 className="mt-[64px] flex w-[250px] flex-col text-center text-[40px] uppercase leading-[42px] tracking-[11px]">
-        <span className="text-base-apparel-primary-100 font-thin">We&apos;re</span>
-        <span className="text-base-apparel-neutral pr-[2px] font-semibold">coming</span>
-        <span className="text-base-apparel-neutral font-semibold">soon</span>
+      <h1 className="mt-[64px] flex w-[250px] flex-col text-center text-[40px] uppercase leading-[42px] tracking-[11px] lg:mt-[calc(7/80*100svh)] lg:text-left lg:text-[min(64px,8vh)] lg:tracking-[17px]">
+        <span className="text-base-apparel-primary-100 font-thin lg:leading-[calc(min(64px,8vh)-1px)]">We&apos;re</span>
+        <span className="text-base-apparel-neutral pr-[2px] font-semibold lg:leading-[calc(min(64px,8vh)+7px)]">coming</span>
+        <span className="text-base-apparel-neutral font-semibold lg:leading-[calc(min(64px,8vh)+7px)]">soon</span>
       </h1>
-      <p className="text-base-apparel-primary-100 mt-[15px] px-9 text-center text-[14px] leading-[22px]">Hello fellow shoppers! We&apos;re currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.</p>
+      <p className="text-base-apparel-primary-100 mt-[15px] px-9 text-center text-[14px] leading-[22px] lg:mt-[19px] lg:pl-[1px] lg:pr-[50px] lg:text-left lg:text-[16px] lg:leading-[28px] lg:tracking-[.05px]">Hello fellow shoppers! We&apos;re currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.</p>
       <form
-        className="relative mt-[33px] h-12 w-[calc(100vw-64px)]"
+        className="relative mt-[33px] h-12 w-[calc(100vw-64px)] lg:mt-[40px] lg:h-14 lg:w-[445px]"
         onSubmit={onSubmit}
       >
         <input
@@ -88,10 +92,10 @@ function Main() {
           type="text"
           placeholder="Email Address"
           className={`${
-            errors.email ? "border-base-apparel-primary-200 border-2 bg-[url('/base-apparel-coming-soon/images/icon-error.svg')] bg-[position:center_right_70px] bg-no-repeat" : "border-base-apparel-primary-100/50"
-          } placeholder:text-base-apparel-primary-100/50 h-full w-full rounded-full border bg-transparent px-6 placeholder:text-[14px] focus-visible:outline focus-visible:outline-[1.5px] focus-visible:outline-offset-2 focus-visible:outline-transparent`}
+            errors.email ? "border-base-apparel-primary-200 border-2 bg-[url('/base-apparel-coming-soon/images/icon-error.svg')] bg-[position:center_right_70px] bg-no-repeat lg:bg-[position:top_53%_right_114px]" : "border-base-apparel-primary-100/50"
+          } placeholder:text-base-apparel-primary-100/50 text-base-apparel-neutral h-full w-full rounded-full border bg-transparent px-6 placeholder:text-[14px] focus-visible:outline focus-visible:outline-[1.5px] focus-visible:outline-offset-2 focus-visible:outline-transparent lg:px-8 lg:pt-[1px] lg:placeholder:text-[16px]`}
         />
-        <button className="from-base-apparel-gradient-red-100 to-base-apparel-gradient-red-200 shadow-base-apparel-neutral/10 absolute top-0 right-0 flex h-full w-[64px] items-center justify-center rounded-full bg-[linear-gradient(135deg,_var(--tw-gradient-stops))] shadow-xl focus-visible:outline focus-visible:outline-transparent">
+        <button className="from-base-apparel-gradient-red-100 to-base-apparel-gradient-red-200 shadow-base-apparel-neutral/10 absolute top-0 right-0 flex h-full w-[64px] items-center justify-center rounded-full bg-[linear-gradient(135deg,_var(--tw-gradient-stops))] shadow-xl before:absolute before:top-0 before:left-0 before:h-full before:w-full before:rounded-full before:transition-colors before:duration-75 before:content-[''] hover:shadow-[hsl(358,47%,83%)]/90 hover:before:bg-white/50 focus-visible:outline focus-visible:outline-transparent lg:w-[100px]">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             viewBox="0 0 12 20"
@@ -105,7 +109,7 @@ function Main() {
             />
           </svg>
         </button>
-        {errors.email ? <p className="text-base-apparel-primary-200 mt-1 w-full px-6 text-left text-[12px]">{errors.email.message}</p> : null}
+        {errors.email ? <p className="text-base-apparel-gradient-red-200 mt-2 w-full px-8 text-left text-[12px] lg:text-[13px]">{errors.email.message}</p> : null}
       </form>
     </main>
   );
@@ -113,7 +117,7 @@ function Main() {
 
 function Footer() {
   return (
-    <footer className="absolute bottom-3 w-full text-center text-[11px] [&_a]:font-bold [&_a]:underline [&_a]:decoration-red-500 [&_a]:decoration-wavy">
+    <footer className="absolute bottom-3 w-full text-center text-[11px] lg:px-[min(calc(165/1440*100vw),165px)] lg:text-left [&_a]:font-bold [&_a]:underline [&_a]:decoration-red-500 [&_a]:decoration-wavy">
       Challenge by{" "}
       <a
         href="https://www.frontendmentor.io?ref=challenge"
