f8fffbd8999e1e1567fcf248beadeda78b065339
muflihanto <muflihanto@gmail.com>
2023-04-11 22:04:40 +0700

    add fylo-dark-theme-landing-page initial intro component for mobile

--- pages/fylo-dark-theme-landing-page.tsx
+++ pages/fylo-dark-theme-landing-page.tsx
@@ -1,6 +1,7 @@
 import Head from "next/head";
 import Image from "next/image";
 import dynamic from "next/dynamic";
+import { CSSProperties } from "react";
 const Slider = dynamic(() => import("../components/Slider"), { ssr: false });
 
 export default function FyloDarkThemeLandingPage() {
@@ -10,6 +11,7 @@ export default function FyloDarkThemeLandingPage() {
         <title>Frontend Mentor | Fylo landing page with dark theme and features grid</title>
       </Head>
       <div className="App font-open-sans relative min-h-[100svh]">
+        <IntroSection />
         <Main />
         <Footer />
         <Slider basePath="/fylo-dark-theme-landing-page/design" />
@@ -18,21 +20,85 @@ export default function FyloDarkThemeLandingPage() {
   );
 }
 
+function FyloLogo({ className }: { className: string }) {
+  return (
+    <div className={`relative aspect-[176/52] ${className}`}>
+      <Image
+        src="/fylo-dark-theme-landing-page/images/logo.svg"
+        fill
+        alt="Fylo Logo"
+        className="object-contain"
+      />
+    </div>
+  );
+}
+
+function IntroSection() {
+  return (
+    <div
+      className="h-[820px] bg-[url('/fylo-dark-theme-landing-page/images/bg-curvy-mobile.svg'),linear-gradient(180deg,theme(colors.fylo-dark.primary.intro),theme(colors.fylo-dark.primary.intro)_var(--bg-color-stop),theme(colors.fylo-dark.primary.main)_var(--bg-color-stop),theme(colors.fylo-dark.primary.main))] bg-[position:top_280px_center,center_center] bg-no-repeat"
+      style={
+        {
+          "--bg-color-stop": "420px",
+        } as CSSProperties
+      }
+    >
+      <Header />
+      <Hero />
+    </div>
+  );
+}
+
+function Hero() {
+  return (
+    <div className="flex flex-col items-center px-6 pt-[24px] max-lg:mx-auto max-lg:max-w-screen-sm lg:grid lg:grid-cols-[auto,640px] lg:grid-rows-1 lg:px-[80px] lg:pt-[21px]">
+      <div className="relative ml-[1px] aspect-[720/534] w-[calc(100%-23px)] lg:col-start-2 lg:row-start-1 lg:w-full lg:place-self-end">
+        <Image
+          src="/fylo-dark-theme-landing-page/images/illustration-intro.png"
+          alt="Hero Image Illustration"
+          className="object-contain"
+          fill
+        />
+      </div>
+      <div className="lg:col-start-1 lg:row-start-1 lg:self-start lg:pt-[76px]">
+        <h1 className="font-raleway text-fylo-dark-neutral mt-[32px] text-center text-[24px] font-bold leading-[36px] lg:mt-0 lg:text-left lg:text-[40px] lg:leading-[60px]">All your files in one secure location, accessible anywhere.</h1>
+        <p className="text-fylo-dark-neutral/75 mt-[15.5px] pl-4 pr-4 text-center text-[14px] lg:mt-[16px] lg:px-0 lg:pr-10 lg:text-left lg:text-[18px] lg:leading-[24px]">Fylo stores all your most important files in one secure location. Access them wherever you need, share and collaborate with friends family, and co-workers.</p>
+        <a
+          href=""
+          className="from-fylo-dark-accent-cyan to-fylo-dark-accent-blue text-fylo-dark-neutral font-raleway mx-auto mt-8 flex h-[48px] w-[240px] flex-col items-center justify-center rounded-full bg-gradient-to-br text-[14px] font-bold"
+        >
+          Get Started
+        </a>
+      </div>
+    </div>
+  );
+}
+
+function Header() {
+  return (
+    <header className="flex h-[72px] items-center justify-between px-20 max-lg:px-[max(calc(50vw-320px),24px)] lg:h-[179px]">
+      <FyloLogo className="-ml-1 w-20 lg:w-[166px]" />
+      <nav>
+        <ul className="text-fylo-dark-neutral font-raleway flex items-center gap-[25px] text-[12px] leading-none lg:gap-[58px] lg:text-[16px]">
+          <li>
+            <a href="">Features</a>
+          </li>
+          <li>
+            <a href="">Team</a>
+          </li>
+          <li>
+            <a href="">Sign In</a>
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
+
 function Main() {
   return (
     <>
-      {`
-         Features
-         Team
-         Sign In
-       
-         All your files in one secure location, accessible anywhere.
-       
-         Fylo stores all your most important files in one secure location. Access them wherever 
-         you need, share and collaborate with friends family, and co-workers.
-       
-         Get Started
-       
+      {/* {`
          Access your files, anywhere
        
          The ability to use a smartphone, tablet, or computer to access your account means your 
@@ -102,14 +168,14 @@ function Main() {
          Contact Us
          Terms
          Privacy
-      `}
+      `} */}
     </>
   );
 }
 
 function Footer() {
   return (
-    <footer className="absolute bottom-3 w-full text-center text-[11px] [&_a]:font-bold [&_a]:underline [&_a]:decoration-red-500 [&_a]:decoration-wavy">
+    <footer className="text-fylo-dark-neutral absolute bottom-3 w-full text-center text-[11px] [&_a]:font-bold [&_a]:underline [&_a]:decoration-red-500 [&_a]:decoration-wavy">
       Challenge by{" "}
       <a
         href="https://www.frontendmentor.io?ref=challenge"
