381cf32da52c19277c016bc465be51cb8cb3f951
muflihanto <muflihanto@gmail.com>
2023-05-09 20:37:27 +0700

    add age-calculator-app initial desktop styling

--- pages/age-calculator-app.tsx
+++ pages/age-calculator-app.tsx
@@ -66,7 +66,7 @@ export default function AgeCalculatorApp() {
       <Head>
         <title>Frontend Mentor | Age calculator app</title>
       </Head>
-      <div className="App bg-age-calculator-neutral-200 relative flex min-h-[100svh] flex-col items-center pt-[88px]">
+      <div className="App bg-age-calculator-neutral-200 relative flex min-h-[100svh] flex-col items-center py-[88px] lg:justify-center lg:py-[80px]">
         <Main />
         <Footer />
         {/* <Slider basePath="/age-calculator-app/design" /> */}
@@ -97,45 +97,45 @@ function Main() {
   });
 
   return (
-    <div className="bg-age-calculator-neutral-100 h-[486px] w-[calc(100vw-32px)] max-w-md rounded-[24px] rounded-br-[100px] px-6 pt-[47px]">
+    <div className="bg-age-calculator-neutral-100 h-[486px] w-[calc(100vw-32px)] max-w-md rounded-[24px] rounded-br-[100px] px-6 pt-[47px] lg:h-[652px] lg:max-w-[840px] lg:px-[56px] lg:pt-[55px]">
       <form
         onSubmit={onSubmit}
-        className="flex flex-col items-center"
+        className="flex w-full flex-col items-center lg:relative"
         noValidate
       >
-        <div className="grid grid-cols-3 grid-rows-1 gap-4">
+        <div className="grid grid-cols-3 grid-rows-1 gap-4 lg:w-auto lg:gap-8 lg:self-start">
           <label
-            className="flex flex-col gap-[5px]"
+            className="flex flex-col gap-[5px] lg:gap-[9px]"
             htmlFor="day"
           >
-            <p className="text-age-calculator-neutral-400 font-poppins text-[12px] font-bold uppercase tracking-[3px]">Day</p>
+            <p className="text-age-calculator-neutral-400 font-poppins text-[12px] font-bold uppercase tracking-[3px] lg:text-[14px] lg:tracking-[3.2px]">Day</p>
             <input
               placeholder="DD"
-              className="text-age-calculator-neutral-500 font-poppins border-age-calculator-neutral-300 h-[54px] w-full rounded-md border px-[15px] text-[20px] font-extrabold"
+              className="text-age-calculator-neutral-500 font-poppins border-age-calculator-neutral-300 h-[54px] w-full rounded-md border px-[15px] text-[20px] font-extrabold lg:h-[72px] lg:w-[160px] lg:px-[23px] lg:pb-[1px] lg:text-[32px] lg:placeholder:font-bold"
               type="number"
               {...register("day", { required: true, valueAsNumber: true, min: 1, minLength: 1 })}
             />
           </label>
           <label
-            className="flex flex-col gap-[5px]"
+            className="flex flex-col gap-[5px] lg:gap-[9px]"
             htmlFor="month"
           >
-            <p className="text-age-calculator-neutral-400 font-poppins text-[12px] font-bold uppercase tracking-[3px]">Month</p>
+            <p className="text-age-calculator-neutral-400 font-poppins text-[12px] font-bold uppercase tracking-[3px] lg:text-[14px] lg:tracking-[3.2px]">Month</p>
             <input
               placeholder="MM"
-              className="text-age-calculator-neutral-500 font-poppins border-age-calculator-neutral-300 h-[54px] w-full rounded-md border px-[15px] text-[20px] font-extrabold"
+              className="text-age-calculator-neutral-500 font-poppins border-age-calculator-neutral-300 h-[54px] w-full rounded-md border px-[15px] text-[20px] font-extrabold lg:h-[72px] lg:w-[160px] lg:px-[23px] lg:pb-[1px] lg:text-[32px] lg:placeholder:font-bold"
               type="number"
               {...register("month", { required: true, valueAsNumber: true, min: 1, minLength: 1 })}
             />
           </label>
           <label
-            className="flex flex-col gap-[5px]"
+            className="flex flex-col gap-[5px] lg:gap-[9px]"
             htmlFor="year"
           >
-            <p className="text-age-calculator-neutral-400 font-poppins text-[12px] font-bold uppercase tracking-[3px]">Year</p>
+            <p className="text-age-calculator-neutral-400 font-poppins text-[12px] font-bold uppercase tracking-[3px] lg:text-[14px] lg:tracking-[3.2px]">Year</p>
             <input
               placeholder="YYYY"
-              className="text-age-calculator-neutral-500 font-poppins border-age-calculator-neutral-300 h-[54px] w-full rounded-md border px-[15px] text-[20px] font-extrabold"
+              className="text-age-calculator-neutral-500 font-poppins border-age-calculator-neutral-300 h-[54px] w-full rounded-md border px-[15px] text-[20px] font-extrabold lg:h-[72px] lg:w-[160px] lg:px-[23px] lg:pb-[1px] lg:text-[32px] lg:placeholder:font-bold"
               type="number"
               min={1}
               max={new Date().getFullYear()}
@@ -143,33 +143,37 @@ function Main() {
             />
           </label>
         </div>
-        <button className="bg-age-calculator-primary-purple hover:bg-age-calculator-neutral-500 flex h-16 w-16 translate-y-1/2 items-center justify-center rounded-full p-5">
+        <button className="bg-age-calculator-primary-purple hover:bg-age-calculator-neutral-500 flex h-16 w-16 translate-y-[calc(50%-1px)] items-center justify-center rounded-full p-5 lg:absolute lg:bottom-0 lg:right-0 lg:h-[96px] lg:w-[96px]">
           <svg
             xmlns="http://www.w3.org/2000/svg"
             width={46}
             height={44}
             viewBox="0 0 46 44"
+            className="stroke-[3px] lg:stroke-2"
           >
             <g
               fill="none"
               stroke="#FFF"
-              strokeWidth={4}
+              // strokeWidth={3}
             >
               <path d="M1 22.019C8.333 21.686 23 25.616 23 44M23 44V0M45 22.019C37.667 21.686 23 25.616 23 44" />
             </g>
           </svg>
         </button>
-        <hr className="border-age-calculator-neutral-200 w-full border-t-2" />
+        <hr className="border-age-calculator-neutral-200 w-full border-t-2 lg:mt-[47px]" />
       </form>
-      <div className="text-age-calculator-neutral-500 font-poppins mt-[65px] flex flex-col gap-[6px] text-[56px] font-extrabold italic leading-none tracking-[-1px]">
+      <div className="text-age-calculator-neutral-500 font-poppins mt-[65px] flex flex-col gap-[6px] text-[56px] font-extrabold italic leading-none tracking-[-1px] lg:mt-[51px] lg:gap-[9px] lg:text-[105px] lg:tracking-[-3px]">
         <p>
-          <span className="text-age-calculator-primary-purple">{!!diff.year ? diff.year : "--"}</span> years
+          <span className="text-age-calculator-primary-purple lg:tracking-[-3px]">{!!diff.year ? diff.year : "- -"}</span>
+          <span className="lg:ml-[12px]">years</span>
         </p>
         <p>
-          <span className="text-age-calculator-primary-purple">{!!diff.month ? diff.month : "--"}</span> months
+          <span className="text-age-calculator-primary-purple lg:tracking-[-3px]">{!!diff.month ? diff.month : "- -"}</span>
+          <span className="lg:ml-[12px]">months</span>
         </p>
         <p>
-          <span className="text-age-calculator-primary-purple">{!!diff.day ? diff.day : "--"}</span> days
+          <span className="text-age-calculator-primary-purple lg:tracking-[-3px]">{!!diff.day ? diff.day : "- -"}</span>
+          <span className="lg:ml-[12px]">days</span>
         </p>
       </div>
     </div>
