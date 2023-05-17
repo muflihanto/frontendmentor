22f1555b68781c547bbde5798b244bf724604127
muflihanto <muflihanto@gmail.com>
2023-05-17 16:48:34 +0700

    add ecommerce-product-page MobileMenu transition

--- components/ecommerce-product-page/MobileMenu.tsx
+++ components/ecommerce-product-page/MobileMenu.tsx
@@ -1,11 +1,12 @@
-import { useSetAtom } from "jotai";
+import { useAtom } from "jotai";
 import { menuOpenAtom } from "../../pages/ecommerce-product-page";
 import { createPortal } from "react-dom";
 import { useEffectOnce, useOnClickOutside } from "usehooks-ts";
-import { useRef } from "react";
+import { Fragment, useRef } from "react";
+import { Transition } from "@headlessui/react";
 
 export default function MobileMenu() {
-  const setMenuOpen = useSetAtom(menuOpenAtom);
+  const [menuOpen, setMenuOpen] = useAtom(menuOpenAtom);
   const ref = useRef(null);
 
   useEffectOnce(() => {
@@ -17,6 +18,8 @@ export default function MobileMenu() {
         setMenuOpen(false);
       }
     };
+
+    (ref.current as any).focus();
 
     window.addEventListener("resize", handleResize);
 
@@ -31,75 +34,104 @@ export default function MobileMenu() {
   });
 
   return createPortal(
-    <div className="fixed left-0 top-0 z-50 h-[100svh] w-screen bg-black/75 lg:hidden">
-      <div
-        className="left-o absolute top-0 h-full w-[66.75%] bg-white px-4 py-[15px]"
-        ref={ref}
-      >
-        <button
-          className="flex h-8 w-8 items-center justify-center rounded"
-          onClick={() => {
-            setMenuOpen(false);
-          }}
+    <Transition
+      as={Fragment}
+      appear={true}
+      show={menuOpen}
+      enter="transition-opacity duration-75"
+      enterFrom="opacity-0"
+      enterTo="opacity-100"
+      leave="transition-opacity duration-75"
+      leaveFrom="opacity-100"
+      leaveTo="opacity-0"
+    >
+      <div className="fixed left-0 top-0 z-50 h-[100svh] w-screen bg-black/75 lg:hidden">
+        <Transition.Child
+          as={Fragment}
+          enter="transition-all duration-75"
+          enterFrom="-translate-x-10"
+          enterTo="translate-x-0"
+          leave="transition-all duration-75"
+          leaveFrom="translate-x-0"
+          leaveTo="-translate-x-10"
         >
-          <svg
-            viewBox="0 0 14 15"
-            className="w-[14px]"
-            xmlns="http://www.w3.org/2000/svg"
+          <div
+            className="absolute left-0 top-0 h-full w-[66.75%] bg-white px-4 py-[15px]"
+            ref={ref}
+            tabIndex={1}
           >
-            <path
-              d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
-              fill="#69707D"
-              fillRule="evenodd"
-            />
-          </svg>
-        </button>
-        <nav className="mt-[42px]">
-          <ul className="flex flex-col gap-[19px]">
-            <li>
-              <a
-                className="text-ecommerce-neutral-500 font-kumbh-sans w-full rounded px-[9px] py-2 text-[18px] font-bold"
-                href=""
+            <button
+              className="flex h-8 w-8 items-center justify-center rounded"
+              onClick={() => {
+                setMenuOpen(false);
+              }}
+              tabIndex={2}
+            >
+              <svg
+                viewBox="0 0 14 15"
+                className="w-[14px]"
+                xmlns="http://www.w3.org/2000/svg"
               >
-                Collections
-              </a>
-            </li>
-            <li>
-              <a
-                className="text-ecommerce-neutral-500 font-kumbh-sans w-full rounded px-[9px] py-2 text-[18px] font-bold"
-                href=""
-              >
-                Men
-              </a>
-            </li>
-            <li>
-              <a
-                className="text-ecommerce-neutral-500 font-kumbh-sans w-full rounded px-[9px] py-2 text-[18px] font-bold"
-                href=""
-              >
-                Women
-              </a>
-            </li>
-            <li>
-              <a
-                className="text-ecommerce-neutral-500 font-kumbh-sans w-full rounded px-[9px] py-2 text-[18px] font-bold"
-                href=""
-              >
-                About
-              </a>
-            </li>
-            <li>
-              <a
-                className="text-ecommerce-neutral-500 font-kumbh-sans w-full rounded px-[9px] py-2 text-[18px] font-bold"
-                href=""
-              >
-                Contact
-              </a>
-            </li>
-          </ul>
-        </nav>
+                <path
+                  d="m11.596.782 2.122 2.122L9.12 7.499l4.597 4.597-2.122 2.122L7 9.62l-4.595 4.597-2.122-2.122L4.878 7.5.282 2.904 2.404.782l4.595 4.596L11.596.782Z"
+                  fill="#69707D"
+                  fillRule="evenodd"
+                />
+              </svg>
+            </button>
+            <nav className="mt-[42px]">
+              <ul className="flex flex-col gap-[19px]">
+                <li>
+                  <a
+                    className="text-ecommerce-neutral-500 font-kumbh-sans w-full rounded px-[9px] py-2 text-[18px] font-bold"
+                    href=""
+                    tabIndex={1}
+                  >
+                    Collections
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="text-ecommerce-neutral-500 font-kumbh-sans w-full rounded px-[9px] py-2 text-[18px] font-bold"
+                    href=""
+                    tabIndex={1}
+                  >
+                    Men
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="text-ecommerce-neutral-500 font-kumbh-sans w-full rounded px-[9px] py-2 text-[18px] font-bold"
+                    href=""
+                    tabIndex={1}
+                  >
+                    Women
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="text-ecommerce-neutral-500 font-kumbh-sans w-full rounded px-[9px] py-2 text-[18px] font-bold"
+                    href=""
+                    tabIndex={1}
+                  >
+                    About
+                  </a>
+                </li>
+                <li>
+                  <a
+                    className="text-ecommerce-neutral-500 font-kumbh-sans w-full rounded px-[9px] py-2 text-[18px] font-bold"
+                    href=""
+                    tabIndex={1}
+                  >
+                    Contact
+                  </a>
+                </li>
+              </ul>
+            </nav>
+          </div>
+        </Transition.Child>
       </div>
-    </div>,
+    </Transition>,
     document.body
   );
 }
