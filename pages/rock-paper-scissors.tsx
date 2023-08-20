bde88ab9e2501b997d958d83c229931d6b79a56f
muflihanto <muflihanto@gmail.com>
2023-08-20 15:51:26 +0700

    add rock-paper-scissors initial RulesModal component

--- pages/rock-paper-scissors.tsx
+++ pages/rock-paper-scissors.tsx
@@ -1,7 +1,11 @@
 import Head from "next/head";
-// import Image from "next/image";
+import Image from "next/image";
+import { atom, useAtom } from "jotai";
+import { useEffect } from "react";
 // import dynamic from "next/dynamic";
 // const Slider = dynamic(() => import("../components/SliderTs"), { ssr: false });
+
+const rulesAtom = atom(false);
 
 export default function RockPaperScissors() {
   return (
@@ -9,11 +13,67 @@ export default function RockPaperScissors() {
       <Head>
         <title>Frontend Mentor | Rock, Paper, Scissors</title>
       </Head>
-      <div className="App font-barlow-semi-condensed relative min-h-[100svh] font-semibold">
+      <div className="App font-barlow-semi-condensed from-rock-paper-scissor-background-100 to-rock-paper-scissor-background-200 relative min-h-[100svh] bg-gradient-to-b to-[130%] font-semibold">
         <Main />
+        <RulesModal />
         <Footer />
-        {/* <Slider absolutePath="/rock-paper-scissors/design/original/mobile-rules-modal.jpg" /> */}
+        {/* <Slider
+          // absolutePath="/rock-paper-scissors/design/original/mobile-rules-modal.jpg"
+          absolutePath="/rock-paper-scissors/design/original/mobile-step-1.jpg"
+          // absolutePath="/rock-paper-scissors/design/original/desktop-rules-modal.jpg"
+        /> */}
       </div>
+    </>
+  );
+}
+
+function RulesModal() {
+  const [open, setOpen] = useAtom(rulesAtom);
+
+  useEffect(() => {
+    if (open) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [open]);
+
+  return (
+    <>
+      <button
+        className="absolute bottom-[55px] left-1/2 h-[42px] w-[130px] -translate-x-1/2 rounded-[10px] border-2 border-opacity-50 uppercase tracking-[2.5px] text-white"
+        onClick={() => {
+          setOpen(true);
+        }}
+      >
+        rules
+      </button>
+
+      {open ? (
+        <div className="fixed left-0 top-0 z-20 h-screen w-screen bg-transparent md:bg-black/50">
+          <div className="absolute flex h-full w-full flex-col items-center bg-white pb-[64px] pt-[88px] md:left-1/2 md:top-1/2 md:h-[415px] md:w-[400px] md:-translate-x-1/2 md:-translate-y-1/2 md:rounded-xl md:p-8 md:pt-[33px]">
+            <h1 className="text-rock-paper-scissor-neutral-dark text-[31px] font-bold uppercase tracking-[.5px] md:self-start md:leading-none">rules</h1>
+            <svg
+              className="mt-[105px] w-[304px] md:mt-12"
+              viewBox="0 0 304 270"
+            >
+              <use href="/rock-paper-scissors/images/image-rules.svg#rules-image" />
+            </svg>
+            <button
+              className="relative mt-auto aspect-square w-5 md:absolute md:right-[31px] md:top-[38px]"
+              onClick={() => {
+                setOpen(false);
+              }}
+            >
+              <Image
+                fill
+                src="/rock-paper-scissors/images/icon-close.svg"
+                alt="Close Button"
+              />
+            </button>
+          </div>
+        </div>
+      ) : null}
     </>
   );
 }
