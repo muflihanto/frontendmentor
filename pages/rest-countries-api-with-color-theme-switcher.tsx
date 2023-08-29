fd535b82f018850df889e7dd1bf476807a917bd3
muflihanto <muflihanto@gmail.com>
2023-08-29 19:59:47 +0700

    add rest-countries-api-with-color-theme-switcher initial header component

--- pages/rest-countries-api-with-color-theme-switcher.tsx
+++ pages/rest-countries-api-with-color-theme-switcher.tsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
-// import Image from "next/image";
+import Image from "next/image";
+import { useDarkMode } from "usehooks-ts";
 // import dynamic from "next/dynamic";
 // const Slider = dynamic(() => import("../components/SliderTs"), { ssr: false });
 
@@ -20,6 +21,7 @@ export default function RestCountriesApiWithColorThemeSwitcher() {
         <title>Frontend Mentor | Rest Countries Api With Color Theme Switcher</title>
       </Head>
       <div className="App font-nunito-sans relative min-h-[100svh] font-light">
+        <Header />
         <Main />
         <Footer />
         {/* <Slider
@@ -31,10 +33,31 @@ export default function RestCountriesApiWithColorThemeSwitcher() {
   );
 }
 
+function Header() {
+  const { isDarkMode, toggle } = useDarkMode(false);
+  return (
+    <header className="shadow-rest-countries-darkblue-100/5 flex h-20 items-center justify-between px-4 shadow-md">
+      <h1 className="text-rest-countries-darkblue-200 text-[14px] font-extrabold tracking-[-.1px]">Where in the world?</h1>
+      <button
+        className="text-rest-countries-darkblue-300 flex items-center px-px pt-px text-[12px] font-normal"
+        onClick={() => toggle()}
+      >
+        <span className="relative mr-[10px] aspect-square h-[14px]">
+          <Image
+            src={isDarkMode ? "/rest-countries-api-with-color-theme-switcher/images/moon.svg" : "/rest-countries-api-with-color-theme-switcher/images/moon-outline.svg"}
+            fill
+            alt="Moon Icon"
+          />
+        </span>
+        <span>Dark Mode</span>
+      </button>
+    </header>
+  );
+}
+
 function Main() {
   return (
-    <div className="p-4">
-      <h1 className="text-[14px] font-extrabold">Where in the world?</h1>
+    <div className="">
       {`
          
       `}
