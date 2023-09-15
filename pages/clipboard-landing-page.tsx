a46935bdc72817a402a8c7a30be5aabe2a3f6bf3
muflihanto <muflihanto@gmail.com>
2023-09-15 20:31:08 +0700

    add clipboard-landing-page hover state styling

--- pages/clipboard-landing-page.tsx
+++ pages/clipboard-landing-page.tsx
@@ -19,7 +19,10 @@ export default function ClipboardLandingPage() {
       <div className="App font-bai-jamjuree relative flex min-h-[100svh] flex-col items-center">
         <Main />
         <Footer />
-        {/* <Slider basePath="/clipboard-landing-page/design" /> */}
+        {/* <Slider
+          basePath="/clipboard-landing-page/design"
+          absolutePath="/clipboard-landing-page/design/active-states.jpg"
+        /> */}
       </div>
     </>
   );
@@ -30,13 +33,13 @@ function CTA({ className = "" }: { className?: string }) {
     <div className={"flex w-full flex-col gap-6 text-[18px] font-semibold tracking-[.4px] lg:flex-row lg:justify-center lg:gap-4 " + className}>
       <a
         href=""
-        className="bg-clipboard-primary-cyan shadow-clipboard-primary-cyan/20 flex h-[56px] items-center justify-center rounded-full border-b-[3px] border-b-[hsl(171,66%,37%)] pt-1 text-white shadow-xl lg:w-[226px]"
+        className="bg-clipboard-primary-cyan shadow-clipboard-primary-cyan/20 hover:bg-clipboard-primary-cyan/75 flex h-[56px] items-center justify-center rounded-full border-b-[3px] border-b-[hsl(171,66%,37%)] pt-1 text-white shadow-xl hover:border-opacity-40 lg:w-[226px]"
       >
         Download for iOS
       </a>
       <a
         href=""
-        className="bg-clipboard-primary-blue shadow-clipboard-primary-blue/20 flex h-[56px] items-center justify-center rounded-full border-b-[3px] border-b-[hsl(232,65%,58%)] pt-1 text-white shadow-xl lg:w-[226px]"
+        className="bg-clipboard-primary-blue shadow-clipboard-primary-blue/20 hover:bg-clipboard-primary-blue/75 flex h-[56px] items-center justify-center rounded-full border-b-[3px] border-b-[hsl(232,65%,58%)] pt-1 text-white shadow-xl hover:border-opacity-40 lg:w-[226px]"
       >
         Download for Mac
       </a>
@@ -50,24 +53,8 @@ function HeroSection() {
       <svg
         className="w-[125px]"
         viewBox="0 0 125 125"
-        xmlns="http://www.w3.org/2000/svg"
       >
-        <g
-          stroke="#26BBA4"
-          strokeWidth="10"
-          fill="none"
-          fillRule="evenodd"
-        >
-          <circle
-            cx="62.5"
-            cy="62.5"
-            r="57.5"
-          />
-          <path
-            d="M85.481 85.481c-12.692 12.692-33.27 12.692-45.962 0s-12.692-33.27 0-45.962 33.27-12.692 45.962 0"
-            strokeLinecap="round"
-          />
-        </g>
+        <use href="/clipboard-landing-page/images/logo.svg#clipboard-logo" />
       </svg>
       <h1 className="text-clipboard-neutral-200 mt-[64px] text-center text-[32px] font-semibold leading-[40px] tracking-[-.5px] lg:mt-[65px] lg:text-[46px] lg:tracking-[-.75px]">A history of everything you copy</h1>
       <p className="text-clipboard-neutral-100 mt-4 text-center leading-[26px] tracking-[.1px] lg:mt-[25px] lg:max-w-[720px] lg:text-[20px] lg:leading-[30px]">Clipboard allows you to track and organize everything you copy. Instantly access your clipboard on all your devices.</p>
@@ -252,40 +239,40 @@ function Main() {
 
 function SocialIcons({ className = "" }: { className?: string }) {
   return (
-    <div className={`flex items-center gap-10 lg:gap-6 ${className}`}>
-      <svg
-        className="w-6"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
+    <div className={`mb-0.5 flex items-center gap-10 lg:gap-6 ${className}`}>
+      <a
+        href=""
+        className="text-clipboard-neutral-200 hover:text-clipboard-primary-cyan"
       >
-        <path
-          d="M22.675 0H1.325C.593 0 0 .593 0 1.325v21.351C0 23.407.593 24 1.325 24H12.82v-9.294H9.692v-3.622h3.128V8.413c0-3.1 1.893-4.788 4.659-4.788 1.325 0 2.463.099 2.795.143v3.24l-1.918.001c-1.504 0-1.795.715-1.795 1.763v2.313h3.587l-.467 3.622h-3.12V24h6.116c.73 0 1.323-.593 1.323-1.325V1.325C24 .593 23.407 0 22.675 0z"
-          fill="#4C545C"
-          fillRule="nonzero"
-        />
-      </svg>
-      <svg
-        className="w-6"
-        viewBox="0 0 24 20"
-        xmlns="http://www.w3.org/2000/svg"
+        <svg
+          className="w-6"
+          viewBox="0 0 24 24"
+        >
+          <use href="/clipboard-landing-page/images/icon-facebook.svg#icon-facebook" />
+        </svg>
+      </a>
+      <a
+        href=""
+        className="text-clipboard-neutral-200 hover:text-clipboard-primary-cyan"
       >
-        <path
-          d="M24 2.557a9.83 9.83 0 01-2.828.775A4.932 4.932 0 0023.337.608a9.864 9.864 0 01-3.127 1.195A4.916 4.916 0 0016.616.248c-3.179 0-5.515 2.966-4.797 6.045A13.978 13.978 0 011.671 1.149a4.93 4.93 0 001.523 6.574 4.903 4.903 0 01-2.229-.616c-.054 2.281 1.581 4.415 3.949 4.89a4.935 4.935 0 01-2.224.084 4.928 4.928 0 004.6 3.419A9.9 9.9 0 010 17.54a13.94 13.94 0 007.548 2.212c9.142 0 14.307-7.721 13.995-14.646A10.025 10.025 0 0024 2.557z"
-          fill="#4C545C"
-          fill-rule="nonzero"
-        />
-      </svg>
-      <svg
-        className="w-6"
-        viewBox="0 0 24 24"
-        xmlns="http://www.w3.org/2000/svg"
+        <svg
+          className="w-6"
+          viewBox="0 0 24 20"
+        >
+          <use href="/clipboard-landing-page/images/icon-twitter.svg#icon-twitter" />
+        </svg>
+      </a>
+      <a
+        href=""
+        className="text-clipboard-neutral-200 hover:text-clipboard-primary-cyan"
       >
-        <path
-          d="M12 2.163c3.204 0 3.584.012 4.85.07 3.252.148 4.771 1.691 4.919 4.919.058 1.265.069 1.645.069 4.849 0 3.205-.012 3.584-.069 4.849-.149 3.225-1.664 4.771-4.919 4.919-1.266.058-1.644.07-4.85.07-3.204 0-3.584-.012-4.849-.07-3.26-.149-4.771-1.699-4.919-4.92-.058-1.265-.07-1.644-.07-4.849 0-3.204.013-3.583.07-4.849.149-3.227 1.664-4.771 4.919-4.919 1.266-.057 1.645-.069 4.849-.069zM12 0C8.741 0 8.333.014 7.053.072 2.695.272.273 2.69.073 7.052.014 8.333 0 8.741 0 12c0 3.259.014 3.668.072 4.948.2 4.358 2.618 6.78 6.98 6.98C8.333 23.986 8.741 24 12 24c3.259 0 3.668-.014 4.948-.072 4.354-.2 6.782-2.618 6.979-6.98.059-1.28.073-1.689.073-4.948 0-3.259-.014-3.667-.072-4.947-.196-4.354-2.617-6.78-6.979-6.98C15.668.014 15.259 0 12 0zm0 5.838a6.162 6.162 0 100 12.324 6.162 6.162 0 000-12.324zM12 16a4 4 0 110-8 4 4 0 010 8zm6.406-11.845a1.44 1.44 0 100 2.881 1.44 1.44 0 000-2.881z"
-          fill="#4C545C"
-          fill-rule="nonzero"
-        />
-      </svg>
+        <svg
+          className="w-6"
+          viewBox="0 0 24 24"
+        >
+          <use href="/clipboard-landing-page/images/icon-instagram.svg#icon-instagram" />
+        </svg>
+      </a>
     </div>
   );
 }
@@ -303,25 +290,50 @@ function Footer() {
         <nav className="text-clipboard-neutral-200 mt-[42px] text-center text-[18px] lg:ml-[min(calc(30px+(100vw-1024px)*(105/416)),135px)] lg:mt-0 lg:pt-[1px]">
           <ul className="flex flex-col items-center gap-[23px] lg:h-[80px] lg:flex-wrap lg:items-start lg:gap-6 lg:gap-x-[min(calc(70/1440*100vw),70px)] lg:leading-none lg:[&>li]:w-[120px] lg:[&>li]:text-left">
             <li>
-              <a href="">FAQs</a>
+              <a
+                href=""
+                className="hover:text-clipboard-primary-cyan"
+              >
+                FAQs
+              </a>
             </li>
             <li>
-              <a href="">Contact Us</a>
+              <a
+                href=""
+                className="hover:text-clipboard-primary-cyan"
+              >
+                Contact Us
+              </a>
             </li>
             <li>
-              <a href="">Privacy Policy</a>
+              <a
+                href=""
+                className="hover:text-clipboard-primary-cyan"
+              >
+                Privacy Policy
+              </a>
             </li>
             <li>
-              <a href="">Press Kit</a>
+              <a
+                href=""
+                className="hover:text-clipboard-primary-cyan"
+              >
+                Press Kit
+              </a>
             </li>
             <li>
-              <a href="">Install Guide</a>
+              <a
+                href=""
+                className="hover:text-clipboard-primary-cyan"
+              >
+                Install Guide
+              </a>
             </li>
           </ul>
         </nav>
         <SocialIcons className="mt-[49px] lg:ml-auto lg:mt-0 lg:self-center lg:pb-[18px]" />
       </div>
-      <p className="absolute bottom-3 left-0 w-full text-center text-[11px] lg:bottom-8 lg:px-[165px] lg:text-right [&_a]:font-bold [&_a]:underline [&_a]:decoration-red-500 [&_a]:decoration-wavy">
+      <p className="[&_a:hover]:text-clipboard-primary-cyan absolute bottom-3 left-0 w-full text-center text-[11px] lg:bottom-8 lg:px-[165px] lg:text-right [&_a]:font-bold [&_a]:underline [&_a]:decoration-red-500 [&_a]:decoration-wavy">
         Challenge by{" "}
         <a
           href="https://www.frontendmentor.io?ref=challenge"
