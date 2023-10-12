d58e8887e58b1ad513c820e86f929b841872b197
muflihanto <muflihanto@gmail.com>
2023-10-12 08:21:23 +0700

    optimize results-summary-component svgs

--- pages/results-summary-component.tsx
+++ pages/results-summary-component.tsx
@@ -1,5 +1,7 @@
 import Head from "next/head";
 import { hankenGrotesk } from "../utils/fonts/hankenGrotesk";
+import type { ComponentProps } from "react";
+import { cn } from "../utils/cn";
 
 // import dynamic from "next/dynamic";
 // const Slider = dynamic(() => import("../components/SliderTs"), { ssr: false });
@@ -52,21 +54,7 @@ function Main() {
         </h2>
         <ul className="mt-[22px] flex flex-col gap-4 md:mt-[26px] md:text-[18px]">
           <li className="flex h-[56px] w-full items-center rounded-md bg-result-summary-primary-red/5 px-[16px] pb-[2px] md:rounded-lg">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width={20}
-              height={20}
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="#F55"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="1.25"
-                d="M10.833 8.333V2.5l-6.666 9.167h5V17.5l6.666-9.167h-5Z"
-              />
-            </svg>
+            <Icons variant="reaction" />
             <p className="ml-3 text-result-summary-primary-red">Reaction</p>
             <div className="ml-auto font-bold text-result-summary-neutral-400/50">
               <span className="mr-[4px] text-result-summary-neutral-400">
@@ -76,36 +64,7 @@ function Main() {
             </div>
           </li>
           <li className="flex h-[56px] w-full items-center rounded-md bg-result-summary-primary-yellow/5 px-[16px] pb-[2px] md:rounded-lg">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width={20}
-              height={20}
-              fill="none"
-              viewBox="0 0 20 20"
-              className="translate-y-[1px]"
-            >
-              <path
-                stroke="#FFB21E"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="1.25"
-                d="M5.833 11.667a2.5 2.5 0 1 0 .834 4.858"
-              />
-              <path
-                stroke="#FFB21E"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="1.25"
-                d="M3.553 13.004a3.333 3.333 0 0 1-.728-5.53m.025-.067a2.083 2.083 0 0 1 2.983-2.824m.199.054A2.083 2.083 0 1 1 10 3.75v12.917a1.667 1.667 0 0 1-3.333 0M10 5.833a2.5 2.5 0 0 0 2.5 2.5m1.667 3.334a2.5 2.5 0 1 1-.834 4.858"
-              />
-              <path
-                stroke="#FFB21E"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="1.25"
-                d="M16.447 13.004a3.334 3.334 0 0 0 .728-5.53m-.025-.067a2.083 2.083 0 0 0-2.983-2.824M10 3.75a2.085 2.085 0 0 1 2.538-2.033 2.084 2.084 0 0 1 1.43 2.92m-.635 12.03a1.667 1.667 0 0 1-3.333 0"
-              />
-            </svg>
+            <Icons variant="memory" className="translate-y-[1px]" />
             <p className="ml-3 text-result-summary-primary-yellow">Memory</p>
             <div className="ml-auto font-bold text-result-summary-neutral-400/50">
               <span className="mr-[4px] text-result-summary-neutral-400">
@@ -115,22 +74,7 @@ function Main() {
             </div>
           </li>
           <li className="flex h-[56px] w-full items-center rounded-md bg-result-summary-primary-teal/5 px-[16px] pb-[2px] md:rounded-lg">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width={20}
-              height={20}
-              fill="none"
-              viewBox="0 0 20 20"
-              className="translate-y-[1px]"
-            >
-              <path
-                stroke="#00BB8F"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="1.25"
-                d="M7.5 10h5M10 18.333A8.333 8.333 0 1 0 1.667 10c0 1.518.406 2.942 1.115 4.167l-.699 3.75 3.75-.699A8.295 8.295 0 0 0 10 18.333Z"
-              />
-            </svg>
+            <Icons variant="verbal" className="translate-y-[1px]" />
             <p className="ml-3 text-result-summary-primary-teal">Verbal</p>
             <div className="ml-auto font-bold text-result-summary-neutral-400/50">
               <span className="mr-[4px] text-result-summary-neutral-400">
@@ -140,28 +84,7 @@ function Main() {
             </div>
           </li>
           <li className="flex h-[56px] w-full items-center rounded-md bg-result-summary-primary-blue/5 px-[16px] md:rounded-lg">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              width={20}
-              height={20}
-              fill="none"
-              viewBox="0 0 20 20"
-            >
-              <path
-                stroke="#1125D6"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="1.25"
-                d="M10 11.667a1.667 1.667 0 1 0 0-3.334 1.667 1.667 0 0 0 0 3.334Z"
-              />
-              <path
-                stroke="#1125D6"
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="1.25"
-                d="M17.5 10c-1.574 2.492-4.402 5-7.5 5s-5.926-2.508-7.5-5C4.416 7.632 6.66 5 10 5s5.584 2.632 7.5 5Z"
-              />
-            </svg>
+            <Icons variant="visual" />
             <p className="ml-3 text-result-summary-primary-blue">Visual</p>
             <div className="ml-auto pb-[1px] font-bold text-result-summary-neutral-400/50">
               <span className="mr-[4px] text-result-summary-neutral-400">
@@ -176,6 +99,17 @@ function Main() {
         </button>
       </div>
     </div>
+  );
+}
+
+type IconVariants = "memory" | "reaction" | "verbal" | "visual";
+function Icons(props: ComponentProps<"svg"> & { variant: IconVariants }) {
+  return (
+    <svg viewBox="0 0 20 20" className={cn(["w-5", props.className])}>
+      <use
+        href={`/results-summary-component/assets/images/icon-${props.variant}.svg#icon-${props.variant}`}
+      />
+    </svg>
   );
 }
 
