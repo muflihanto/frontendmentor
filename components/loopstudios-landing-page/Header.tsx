3b7480dafa6b261baf23c4bedacc4aaec92c00de
muflihanto <muflihanto@gmail.com>
2023-10-17 19:03:29 +0700

    optimize some svgs

--- components/loopstudios-landing-page/Header.tsx
+++ components/loopstudios-landing-page/Header.tsx
@@ -17,7 +17,7 @@ const Header: NextPage = () => {
     }
   }, [isMenuOpen]);
   return (
-    <header className="absolute top-0 left-0 w-full">
+    <header className="absolute left-0 top-0 w-full">
       <div className="relative z-20 flex items-center justify-between px-6 py-10 lg:px-[calc(165/1440*100vw)] lg:py-16">
         <div className="relative aspect-[6/1] w-[144px] lg:w-[192px]">
           <Image
@@ -34,61 +34,37 @@ const Header: NextPage = () => {
           }}
         >
           {isMenuOpen ? (
-            <svg
-              width="20"
-              height="20"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <path
-                d="M17.778.808l1.414 1.414L11.414 10l7.778 7.778-1.414 1.414L10 11.414l-7.778 7.778-1.414-1.414L8.586 10 .808 2.222 2.222.808 10 8.586 17.778.808z"
-                fill="#FFF"
-                fillRule="evenodd"
-              />
+            <svg viewBox="0 0 20 20" className="w-5">
+              <use href="/loopstudios-landing-page/images/icon-close.svg#icon-close" />
             </svg>
           ) : (
-            <svg
-              width="24"
-              height="16"
-              xmlns="http://www.w3.org/2000/svg"
-            >
-              <g
-                fill="#FFF"
-                fillRule="evenodd"
-              >
-                <path d="M0 0h24v2H0zM0 7h24v2H0zM0 14h24v2H0z" />
-              </g>
+            <svg viewBox="0 0 24 16" className="w-6">
+              <use href="/loopstudios-landing-page/images/icon-hamburger.svg#icon-hamburger" />
             </svg>
           )}
         </button>
-        <NavItems className="[&_a]:text-loopstudios-primary-white/90 hidden gap-[33px] lg:flex [&_li:hover]:relative [&_li:hover]:before:absolute [&_li:hover]:before:-bottom-[11px] [&_li:hover]:before:left-1/2 [&_li:hover]:before:h-1 [&_li:hover]:before:w-[45%] [&_li:hover]:before:-translate-x-1/2 [&_li:hover]:before:border-b-2 [&_li:hover]:before:content-[''] [&_a]:text-[15px]" />
+        <NavItems className="hidden gap-[33px] lg:flex [&_a]:text-[15px] [&_a]:text-loopstudios-primary-white/90 [&_li:hover]:relative [&_li:hover]:before:absolute [&_li:hover]:before:-bottom-[11px] [&_li:hover]:before:left-1/2 [&_li:hover]:before:h-1 [&_li:hover]:before:w-[45%] [&_li:hover]:before:-translate-x-1/2 [&_li:hover]:before:border-b-2 [&_li:hover]:before:content-['']" />
       </div>
       {isMenuOpen && (
-        <nav className="absolute top-0 left-0 right-0 z-10 block lg:hidden">
-          <NavItems className="bg-loopstudios-primary-black [&_a]:text-loopstudios-primary-white/75 [&_a]:font-josefin flex h-screen w-screen flex-col items-start justify-center gap-[13px] px-6 pb-[5px] [&_a]:text-[24px] [&_a]:uppercase [&_a]:tracking-[1px]" />
+        <nav className="absolute left-0 right-0 top-0 z-10 block lg:hidden">
+          <NavItems className="flex h-screen w-screen flex-col items-start justify-center gap-[13px] bg-loopstudios-primary-black px-6 pb-[5px] [&_a]:font-josefin [&_a]:text-[24px] [&_a]:uppercase [&_a]:tracking-[1px] [&_a]:text-loopstudios-primary-white/75" />
         </nav>
       )}
     </header>
   );
 };
 
+const links = ["About", "Careers", "Events", "Products", "Support"];
 const NavItems = ({ className }: { className: string }) => {
   return (
     <ul className={className}>
-      <li>
-        <a href="">About</a>
-      </li>
-      <li>
-        <a href="">Careers</a>
-      </li>
-      <li>
-        <a href="">Events</a>
-      </li>
-      <li>
-        <a href="">Products</a>
-      </li>
-      <li>
-        <a href="">Support</a>
-      </li>
+      {links.map((el) => {
+        return (
+          <li key={el}>
+            <a href="">{el}</a>
+          </li>
+        );
+      })}
     </ul>
   );
 };
