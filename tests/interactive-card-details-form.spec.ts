e8b882425cd790e78869b421518ec090f99a014a
muflihanto <muflihanto@gmail.com>
2023-12-08 13:28:11 +0700

    write initial e2e tests for interactive-card-details-form page

--- tests/interactive-card-details-form.spec.ts
+++ tests/interactive-card-details-form.spec.ts
@@ -1,19 +1,16 @@
 import { test, expect } from "@playwright/test";
 
-test.describe("FrontendMentor Challenge - Age calculator app Page", () => {
-  /** Go to Age calculator app page before each test */
+test.describe("FrontendMentor Challenge - Interactive card details form Page", () => {
+  /** Go to Interactive card details form page before each test */
   test.beforeEach("Open", async ({ page }) => {
-    await page.goto("/age-calculator-app");
+    await page.goto("/interactive-card-details-form");
   });
 
   /** Test if the page has a correct title */
   test("has title", async ({ page }) => {
-    await expect(page).toHaveTitle("Frontend Mentor | Age calculator app");
-  });
-
-  /** Test if the page has a form */
-  test("has a form", async ({ page }) => {
-    await expect(page.getByText("DayMonthYear")).toBeVisible();
+    await expect(page).toHaveTitle(
+      "Frontend Mentor | Interactive card details form",
+    );
   });
 
   /** Test if the page has a correct footer */
