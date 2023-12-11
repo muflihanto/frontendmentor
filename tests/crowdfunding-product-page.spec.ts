d429bbc72a129c5947be7011e253e40a8e026fc8
muflihanto <muflihanto@gmail.com>
2023-12-11 16:45:56 +0700

    write initial e2e tests for crowdfunding-product-page

---
+++ tests/crowdfunding-product-page.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("FrontendMentor Challenge - Crowdfunding product Page", () => {
+  /** Go to Crowdfunding product page before each test */
+  test.beforeEach("Open", async ({ page }) => {
+    await page.goto("/crowdfunding-product-page");
+  });
+
+  /** Test if the page has a correct title */
+  test("has title", async ({ page }) => {
+    await expect(page).toHaveTitle(
+      "Frontend Mentor | Crowdfunding product page",
+    );
+  });
+
+  /** Test if the page has a correct heading */
+  test("has a heading", async ({ page }) => {
+    await expect(
+      page.getByRole("heading", {
+        level: 1,
+        name: "Mastercraft Bamboo Monitor Riser",
+      }),
+    ).toBeVisible();
+  });
+});
