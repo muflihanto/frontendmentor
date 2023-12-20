31a2f15d8a3e2cee453714a9aa58da42d55c8bbe
muflihanto <muflihanto@gmail.com>
2023-12-20 19:10:52 +0700

    write initial e2e tests for url-shortening-api page

---
+++ tests/url-shortening-api.spec.ts
@@ -0,0 +1,25 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("FrontendMentor Challenge - Shortly URL shortening API Challenge Page", () => {
+  /** Go to Shortly URL shortening API Challenge page before each test */
+  test.beforeEach("Open", async ({ page }) => {
+    await page.goto("/url-shortening-api");
+  });
+
+  /** Test if the page has a correct title */
+  test("has title", async ({ page }) => {
+    await expect(page).toHaveTitle(
+      "Frontend Mentor | Shortly URL shortening API Challenge",
+    );
+  });
+
+  /** Test if the page has a heading */
+  test("has a heading", async ({ page }) => {
+    await expect(
+      page.getByRole("heading", {
+        level: 1,
+        name: "More than just shorter links",
+      }),
+    ).toBeVisible();
+  });
+});
