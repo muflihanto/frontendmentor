e792d6e5de9b9d339e51155ac6fd21eabdd74a65
muflihanto <muflihanto@gmail.com>
2023-12-21 19:34:04 +0700

    write initial e2e tests for multi-step-form page

---
+++ tests/multi-step-form.spec.ts
@@ -0,0 +1,23 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("FrontendMentor Challenge - Multi-step form Page", () => {
+  /** Go to Multi-step form page before each test */
+  test.beforeEach("Open", async ({ page }) => {
+    await page.goto("/multi-step-form");
+  });
+
+  /** Test if the page has a correct title */
+  test("has title", async ({ page }) => {
+    await expect(page).toHaveTitle("Frontend Mentor | Multi-step form");
+  });
+
+  /** Test if the page has a heading */
+  test("has a heading", async ({ page }) => {
+    await expect(
+      page.getByRole("heading", {
+        level: 1,
+        name: "Personal info",
+      }),
+    ).toBeVisible();
+  });
+});
