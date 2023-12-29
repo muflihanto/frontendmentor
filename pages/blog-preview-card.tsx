b8fd3d3e4ec0f61aca98a9c7decc116e3fa26661
muflihanto <muflihanto@gmail.com>
2023-12-29 19:51:00 +0700

    blog-preview-card: add mobile styling

--- pages/blog-preview-card.tsx
+++ pages/blog-preview-card.tsx
@@ -29,28 +29,39 @@ export default function BlogPreviewCard() {
 
 function Main() {
   return (
-    <div className="bg relative z-0 flex min-h-full w-full max-w-[384px] flex-col justify-start gap-4 rounded-3xl border border-blog-neutral-300 bg-blog-neutral-100 p-6 drop-shadow-[8px_8px_0px_rgb(0,0,0)]">
+    <main className="relative z-0 m-6 mt-[23px] flex min-h-[500px] w-full max-w-[384px] flex-col justify-start rounded-[20px] border-[1.5px] border-blog-neutral-300 bg-blog-neutral-100 p-[23px] font-medium text-blog-neutral-300 drop-shadow-[8.5px_8.5px_0px_rgb(0,0,0)]">
       <header>
-        <figure className="relative aspect-[336/201] w-full overflow-hidden rounded-lg">
+        <figure className="relative aspect-[278/200] w-full overflow-hidden rounded-xl sm:aspect-[336/201]">
           <Image
             fill
             src="/blog-preview-card/assets/images/illustration-article.svg"
             alt="Illustration Article"
+            className="object-cover"
           />
         </figure>
       </header>
-      {`
+      <p className="mt-[23px] inline-block w-fit grow-0 rounded bg-blog-primary px-[12px] py-1 text-[12px] font-bold">
         Learning
-
-        Published 21 Dec 2023
-      `}
-      <h1>HTML & CSS foundations</h1>
-      {`
-        These languages are the backbone of every website, defining structure, content, and presentation.
-      
-        Greg Hooper
-      `}
-    </div>
+      </p>
+      <p className="mt-3 text-[12px]">Published 21 Dec 2023</p>
+      <h1 className="mt-[13px] text-xl font-extrabold">
+        HTML & CSS foundations
+      </h1>
+      <p className="mb-6 mt-[13px] text-[14px] text-blog-neutral-200">
+        These languages are the backbone of every website, defining structure,
+        content, and presentation.
+      </p>
+      <div className="mt-auto flex items-center gap-3">
+        <span className="relative size-8">
+          <Image
+            src="/blog-preview-card/assets/images/image-avatar.webp"
+            alt="Greg Hooper Avatar"
+            fill
+          />
+        </span>
+        <span className="text-[14px] font-extrabold">Greg Hooper</span>
+      </div>
+    </main>
   );
 }
 
