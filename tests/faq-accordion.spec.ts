5e0ad035432b888214a99d142add692827be5cdb
muflihanto <muflihanto@gmail.com>
2023-12-30 19:39:20 +0700

    faq-accordion: add more tests

--- tests/faq-accordion.spec.ts
+++ tests/faq-accordion.spec.ts
@@ -1,4 +1,26 @@
 import { test, expect } from "@playwright/test";
+const faqs = [
+  {
+    question: "What is Frontend Mentor, and how will it help me?",
+    answer:
+      "Frontend Mentor offers realistic coding challenges to help developers improve their frontend coding skills with projects in HTML, CSS, and JavaScript. It's suitable for all levels and ideal for portfolio building.",
+  },
+  {
+    question: "Is Frontend Mentor free?",
+    answer:
+      "Yes, Frontend Mentor offers both free and premium coding challenges, with the free option providing access to a range of projects suitable for all skill levels.",
+  },
+  {
+    question: "Can I use Frontend Mentor projects in my portfolio?",
+    answer:
+      "Yes, you can use projects completed on Frontend Mentor in your portfolio. It's an excellent way to showcase your skills to potential employers!",
+  },
+  {
+    question: "How can I get help if I'm stuck on a Frontend Mentor challenge?",
+    answer:
+      "The best place to get help is inside Frontend Mentor's Discord community. There's a help channel where you can ask questions and seek support from other community members.",
+  },
+];
 
 test.describe("FrontendMentor Challenge - FAQ Accordion Page", () => {
   /** Go to FAQ Accordion page before each test */
@@ -9,6 +31,30 @@ test.describe("FrontendMentor Challenge - FAQ Accordion Page", () => {
   /** Test if the page has a correct title */
   test("has title", async ({ page }) => {
     await expect(page).toHaveTitle("Frontend Mentor | FAQ Accordion");
+  });
+
+  /** Test if the page has a banner */
+  test("has a banner", async ({ page }) => {
+    await expect(page.getByRole("banner")).toBeVisible();
+  });
+
+  /** Test if the page has a correct faqs */
+  test("has correct faqs", async ({ page }) => {
+    for (const [index, faq] of Object.entries(faqs)) {
+      const question = page
+        .locator("details")
+        .filter({ hasText: faq.question });
+      const parent = page
+        .locator("div:has(>details)")
+        .filter({ has: question });
+      const answer = parent.getByRole("definition");
+      await expect(question).toBeVisible();
+      if (index === "0") {
+        await expect(answer).toBeVisible();
+      } else {
+        await expect(answer).toBeHidden();
+      }
+    }
   });
 
   /** Test if the page has a heading */
