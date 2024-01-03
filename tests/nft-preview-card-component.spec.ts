70602069f67f8eb21e350c3073a900e13a5c4305
muflihanto <muflihanto@gmail.com>
2024-01-03 17:54:44 +0700

    nft-preview-card-component: add more tests

--- tests/nft-preview-card-component.spec.ts
+++ tests/nft-preview-card-component.spec.ts
@@ -15,11 +15,36 @@ test.describe("FrontendMentor Challenge - NFT preview card component Page", () =
 
   /** Test if the page has a correct heading */
   test("has a heading", async ({ page }) => {
+    const heading = page.getByRole("heading", {
+      level: 1,
+      name: "Equilibrium #3429",
+    });
+    await expect(heading).toBeVisible();
+    await heading.hover();
+    // Test hover state
+    await expect(heading).toHaveCSS("color", "rgb(0, 255, 247)");
+  });
+
+  /** Test if the page has a header image */
+  test("has a header image", async ({ page }) => {
+    await expect(page.getByAltText("Equilibrium Image")).toBeVisible();
+  });
+
+  /** Test if the page has a footer */
+  test("has a footer", async ({ page }) => {
     await expect(
-      page.getByRole("heading", {
-        level: 1,
-        name: "Equilibrium #3429",
-      }),
+      page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
     ).toBeVisible();
+  });
+
+  /** Test if the page has correct creator info */
+  test("has creator info", async ({ page }) => {
+    const creator = page
+      .locator("p")
+      .filter({ hasText: "Creation ofJules Wyvern" });
+    await expect(creator).toBeVisible();
+    const name = creator.locator("span").filter({ hasText: "Jules Wyvern" });
+    await name.hover();
+    await expect(name).toHaveCSS("color", "rgb(0, 255, 247)");
   });
 });
