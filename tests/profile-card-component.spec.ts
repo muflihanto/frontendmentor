1c91ef4ce5504b554d44757010bcf472af8240dd
muflihanto <muflihanto@gmail.com>
2024-01-04 20:18:55 +0700

    profile-card-component: add more tests

--- tests/profile-card-component.spec.ts
+++ tests/profile-card-component.spec.ts
@@ -11,6 +11,11 @@ test.describe("FrontendMentor Challenge - Profile card component Page", () => {
     await expect(page).toHaveTitle("Frontend Mentor | Profile card component");
   });
 
+  /** Test if the page has a correct header image */
+  test("has a header image", async ({ page }) => {
+    await expect(page.getByRole("img", { name: "Header Image" })).toBeVisible();
+  });
+
   /** Test if the page has a correct heading */
   test("has a heading", async ({ page }) => {
     await expect(
@@ -19,5 +24,30 @@ test.describe("FrontendMentor Challenge - Profile card component Page", () => {
         name: "Victor Crest",
       }),
     ).toBeVisible();
+  });
+
+  /** Test if the page has correct user's avatar */
+  test("has user's avatar", async ({ page }) => {
+    await expect(page.getByRole("img", { name: "Avatar" })).toBeVisible();
+  });
+
+  /** Test if the page has correct user's city info */
+  test("has user's city info", async ({ page }) => {
+    await expect(page.getByText("London")).toBeVisible();
+  });
+
+  /** Test if the page has correct user's followers info */
+  test("has user's followers info", async ({ page }) => {
+    await expect(page.getByText("80K Followers")).toBeVisible();
+  });
+
+  /** Test if the page has correct user's likes info */
+  test("has user's likes info", async ({ page }) => {
+    await expect(page.getByText("803K Likes")).toBeVisible();
+  });
+
+  /** Test if the page has correct user's photos info */
+  test("has user's photos info", async ({ page }) => {
+    await expect(page.getByText("1.4K Photos")).toBeVisible();
   });
 });
