c0c064c62997f73ba58cae0a2df75c6dbe110fed
muflihanto <muflihanto@gmail.com>
2024-01-04 16:10:21 +0700

    order-summary-component: add more tests

--- tests/order-summary-component.spec.ts
+++ tests/order-summary-component.spec.ts
@@ -20,4 +20,37 @@ test.describe("FrontendMentor Challenge - Order summary card Page", () => {
       }),
     ).toBeVisible();
   });
+
+  /** Test if the page has a correct header image */
+  test("has a header image", async ({ page }) => {
+    await expect(page.getByRole("img", { name: "Illustration" })).toBeVisible();
+  });
+
+  /** Test if the page has a correct order summary text */
+  test("has an order summary text", async ({ page }) => {
+    await expect(
+      page.getByText(
+        "You can now listen to millions of songs, audiobooks, and podcasts on any device ",
+      ),
+    ).toBeVisible();
+  });
+
+  /** Test if the page has a change plan button */
+  test("has a change plan button", async ({ page }) => {
+    const link = page.getByRole("link", { name: "Change" });
+    await expect(link).toBeVisible();
+    // Test hover state
+    await link.hover();
+    await expect(link).toHaveCSS("color", "rgb(117, 106, 241)");
+    await expect(link).toHaveCSS("text-decoration-line", "none");
+  });
+
+  /** Test if the page has a cta button */
+  test("has a cta button", async ({ page }) => {
+    const button = page.getByRole("link", { name: "Proceed to Payment" });
+    await expect(button).toBeVisible();
+    // Test hover state
+    await button.hover();
+    await expect(button).toHaveCSS("background-color", "rgb(117, 106, 241)");
+  });
 });
