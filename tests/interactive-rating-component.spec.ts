eecb934e8e315d05d37d816a8289c42dbce466dd
muflihanto <muflihanto@gmail.com>
2024-01-05 20:57:07 +0700

    interactive-rating-component: add more tests

--- tests/interactive-rating-component.spec.ts
+++ tests/interactive-rating-component.spec.ts
@@ -23,17 +23,43 @@ test.describe("FrontendMentor Challenge - Interactive rating component Page", ()
     ).toBeVisible();
   });
 
-  /** Test if the page has a cta button */
-  test("has a cta button", async ({ page }) => {
-    await expect(page.getByRole("button", { name: "Submit" })).toBeVisible();
-  });
-
   /** Test if the page has input elements */
   test("has input elements", async ({ page }) => {
     const inputs = await page.locator("fieldset input").all();
     expect(inputs).toHaveLength(5);
     for (const input of inputs) {
       await expect(input).toBeVisible();
+    }
+  });
+
+  /** Test if the page has a submit button */
+  test("has a submit button", async ({ page }) => {
+    await expect(page.getByRole("button", { name: "Submit" })).toBeVisible();
+  });
+
+  /** Test if the page has body text */
+  test("has body text", async ({ page }) => {
+    await expect(
+      page.getByText(
+        "Please let us know how we did with your support request. All feedback is appreci",
+      ),
+    ).toBeVisible();
+  });
+
+  /** Test if the form input can be submitted */
+  test("can submit form", async ({ page }) => {
+    const inputs = await page.locator("fieldset input").all();
+    for (const [index, input] of Object.entries(inputs)) {
+      const submit = page.getByRole("button", { name: "Submit" });
+      await input.check();
+      await submit.click();
+      await expect(
+        page.getByRole("heading", { name: "Thank you!" }),
+      ).toBeVisible();
+      await expect(
+        page.getByText(`You selected ${Number(index) + 1} out of 5`),
+      ).toBeVisible();
+      await page.reload();
     }
   });
 
