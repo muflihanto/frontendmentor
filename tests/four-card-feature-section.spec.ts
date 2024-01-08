66950a466dd14f6c302e85f93f2de685a99c1364
muflihanto <muflihanto@gmail.com>
2024-01-08 19:13:07 +0700

    four-card-feature-section: add more tests

--- tests/four-card-feature-section.spec.ts
+++ tests/four-card-feature-section.spec.ts
@@ -22,4 +22,42 @@ test.describe("FrontendMentor Challenge - Four card feature section Page", () =>
       }),
     ).toBeVisible();
   });
+
+  /** Test if the page has a correct body text */
+  test("has a body text", async ({ page }) => {
+    await expect(
+      page.getByText(
+        "Our Artificial Intelligence powered tools use millions of project data points to ensure that your project is successful",
+      ),
+    ).toBeVisible();
+  });
+
+  test.describe("has four cards", () => {
+    const cards = {
+      Supervisor: "Monitors activity to identify project roadblocks",
+      "Team Builder":
+        "Scans our talent network to create the optimal team for your project",
+      Karma: "Regularly evaluates our talent to ensure quality",
+      Calculator:
+        "Uses data from past projects to provide better delivery estimates",
+    };
+
+    for (const card of Object.entries(cards)) {
+      test(`has a ${card[0]} card`.toLowerCase(), async ({ page }) => {
+        const cardParent = page.getByText(`${card[0]}${card[1]}`);
+        await expect(
+          cardParent.getByRole("heading", { name: card[0] }),
+        ).toBeVisible();
+        await expect(cardParent.getByText(card[1])).toBeVisible();
+        await expect(cardParent.locator("svg")).toBeVisible();
+      });
+    }
+  });
+
+  /** Test if the page has a correct footer */
+  test("has a footer", async ({ page }) => {
+    await expect(
+      page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
+    ).toBeVisible();
+  });
 });
