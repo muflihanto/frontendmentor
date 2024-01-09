86546631771dbb26f2c585d8ff31e5691c3d5c5f
muflihanto <muflihanto@gmail.com>
2024-01-09 16:31:38 +0700

    base-apparel-coming-soon: add more tests

--- tests/base-apparel-coming-soon.spec.ts
+++ tests/base-apparel-coming-soon.spec.ts
@@ -22,4 +22,37 @@ test.describe("FrontendMentor Challenge - Base Apparel coming soon Page", () => 
       }),
     ).toBeVisible();
   });
+
+  /** Test if the page has a correct logo */
+  test("has a logo", async ({ page }) => {
+    await expect(
+      page.getByRole("img", { name: "Base Apparel Logo" }),
+    ).toBeVisible();
+  });
+
+  /** Test if the page has correct body text */
+  test("has body text", async ({ page }) => {
+    await expect(
+      page.getByText(
+        "Hello fellow shoppers! We're currently building our new fashion store. Add your email below to stay up-to-date with announcements and our launch deals.",
+      ),
+    ).toBeVisible();
+  });
+
+  /** Test if the page has a correct hero image */
+  test("has a hero image", async ({ page }) => {
+    await expect(page.getByRole("img", { name: "Hero Image" })).toBeVisible();
+  });
+
+  /** Test if the page has a correct form */
+  test("has a form", async ({ page }) => {
+    await expect(page.locator("form").first()).toBeVisible();
+  });
+
+  /** Test if the page has a correct footer */
+  test("has a footer", async ({ page }) => {
+    await expect(
+      page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
+    ).toBeVisible();
+  });
 });
