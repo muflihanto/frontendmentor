7240cd72296d957b32b1cf0e84c42a11dbb7e236
muflihanto <muflihanto@gmail.com>
2024-01-10 18:59:26 +0700

    intro-component-with-signup-form: add more tests

--- tests/intro-component-with-signup-form.spec.ts
+++ tests/intro-component-with-signup-form.spec.ts
@@ -44,6 +44,38 @@ test.describe("FrontendMentor Challenge - Intro component with sign up form Page
     await expect(page.locator("form")).toBeVisible();
   });
 
+  test.describe("form should works", () => {
+    test("has all fields", async ({ page }) => {
+      const inputs = await page.locator("form input").all();
+      const names = ["firstName", "lastName", "email", "password"];
+      expect(inputs).toHaveLength(4);
+      const inputNames = [];
+      for (const input of inputs) {
+        inputNames.push(await input.getAttribute("name"));
+      }
+      expect(inputNames).toStrictEqual(names);
+    });
+
+    test("has a submit button", async ({ page }) => {
+      const submit = page.locator("form button");
+      await expect(submit).toBeVisible();
+    });
+
+    test("invalid input should trigger error warning", ({ page: _page }) => {
+      test.fixme();
+    });
+
+    test("has a terms and services agreement", async ({ page }) => {
+      await expect(
+        page
+          .locator("form")
+          .getByText(
+            "By clicking the button, you are agreeing to our Terms and Services",
+          ),
+      ).toBeVisible();
+    });
+  });
+
   /** Test if the page has a correct footer */
   test("has a footer", async ({ page }) => {
     await expect(
