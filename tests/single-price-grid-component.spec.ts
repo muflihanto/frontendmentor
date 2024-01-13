e73275c331c662d0fd2ae107d68ba5a4e4419686
muflihanto <muflihanto@gmail.com>
2024-01-13 19:25:40 +0700

    single-price-grid-component: add more tests

--- tests/single-price-grid-component.spec.ts
+++ tests/single-price-grid-component.spec.ts
@@ -22,4 +22,43 @@ test.describe("FrontendMentor Challenge - Single Price Grid Component Page", () 
       }),
     ).toBeVisible();
   });
+
+  test.describe("has price section", () => {
+    /** Test if the section has a heading */
+    test("has a heading", async ({ page }) => {
+      await expect(
+        page.getByRole("heading", { name: "Monthly Subscription" }),
+      ).toBeVisible();
+    });
+
+    /** Test if the section has a correct price */
+    test("has a price", async ({ page }) => {
+      await expect(page.getByText("$29per month")).toBeVisible();
+    });
+
+    /** Test if the section has a sign up button */
+    test("has a sign up button", async ({ page }) => {
+      await expect(page.getByRole("link", { name: "Sign Up" })).toBeVisible();
+    });
+  });
+
+  /** Test if the page has a why us section */
+  test("has a why us section", async ({ page }) => {
+    const heading = page.getByRole("heading", { name: "Why Us" });
+    const whyUs = [
+      "Tutorials by industry experts",
+      "Peer & expert code review",
+      "Coding exercises",
+      "Access to our GitHub repos",
+      "Community forum",
+      "Flashcard decks",
+      "New videos every week",
+    ];
+    const whyUsElements = await heading.locator("+ul li").all();
+    await expect(heading).toBeVisible();
+    for (const [index, el] of Object.entries(whyUsElements)) {
+      await expect(el).toBeVisible();
+      expect(await el.textContent()).toEqual(whyUs[Number(index)]);
+    }
+  });
 });
