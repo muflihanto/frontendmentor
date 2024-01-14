c202942e28690a5345c891422c3c80780342cd20
muflihanto <muflihanto@gmail.com>
2024-01-14 20:56:39 +0700

    3-column-preview-card-component: add more tests

--- tests/3-column-preview-card-component.spec.ts
+++ tests/3-column-preview-card-component.spec.ts
@@ -21,4 +21,45 @@ test.describe("FrontendMentor Challenge - 3-column preview card component Page",
       }),
     ).toHaveCount(3);
   });
+
+  /** Test if the page has all cards */
+  test("has all cards", async ({ page }) => {
+    const cards = [
+      {
+        heading: "Sedans",
+        description:
+          "Choose a sedan for its affordability and excellent fuel economy. Ideal for cruising in the city or on your next road trip.",
+      },
+      {
+        heading: "SUVs",
+        description:
+          "Take an SUV for its spacious interior, power, and versatility. Perfect for your next family vacation and off-road adventures.",
+      },
+      {
+        heading: "Luxury",
+        description:
+          "Cruise in the best car brands without the bloated prices. Enjoy the enhanced comfort of a luxury rental and arrive in style.",
+      },
+    ];
+    for (const card of cards) {
+      const container = page.getByText(`${card.heading}${card.description}`);
+      await expect(
+        container.getByRole("heading", {
+          level: 2,
+          name: card.heading,
+        }),
+      ).toBeVisible();
+      await expect(container.getByText(card.description)).toBeVisible();
+      await expect(
+        container.getByRole("link", { name: "Learn More" }).first(),
+      ).toBeVisible();
+    }
+  });
+
+  /** Test if the page has correct footer */
+  test("has footer", async ({ page }) => {
+    await expect(
+      page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
+    ).toBeVisible();
+  });
 });
