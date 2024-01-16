09ad82e35c5ca3d6da9890be6ef51a4391ec29aa
muflihanto <muflihanto@gmail.com>
2024-01-16 19:24:00 +0700

    huddle-landing-page-with-single-introductory-section: add more tests

--- tests/huddle-landing-page-with-single-introductory-section.spec.ts
+++ tests/huddle-landing-page-with-single-introductory-section.spec.ts
@@ -13,6 +13,11 @@ test.describe("FrontendMentor Challenge - Huddle landing page with single introd
     );
   });
 
+  /** Test if the page has a correct huddle logo */
+  test("has a huddle logo", async ({ page }) => {
+    await expect(page.getByRole("img", { name: "Huddle Logo" })).toBeVisible();
+  });
+
   /** Test if the page has a correct heading */
   test("has a heading", async ({ page }) => {
     await expect(
@@ -20,6 +25,34 @@ test.describe("FrontendMentor Challenge - Huddle landing page with single introd
         level: 1,
         name: "Build The Community Your Fans Will Love",
       }),
+    ).toBeVisible();
+  });
+
+  /** Test if the page has a correct body text */
+  test("has a body text", async ({ page }) => {
+    await expect(
+      page.getByText(
+        "Huddle re-imagines the way we build communities. You have a voice, but so does your audience. Create connections with your users as you engage in genuine discussion.",
+      ),
+    ).toBeVisible();
+  });
+
+  /** Test if the page has a correct register link */
+  test("has a register link", async ({ page }) => {
+    await expect(page.getByRole("link", { name: "Register" })).toBeVisible();
+  });
+
+  /** Test if the page has a correct mockup illustration */
+  test("has a mockup illustration", async ({ page }) => {
+    await expect(
+      page.getByRole("img", { name: "Huddle Illustration Mockup" }),
+    ).toBeVisible();
+  });
+
+  /** Test if the page has a correct footer */
+  test("has a footer", async ({ page }) => {
+    await expect(
+      page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
     ).toBeVisible();
   });
 });
