49a5a6ac3c36c830b9403c116d75200772ad55ef
muflihanto <muflihanto@gmail.com>
2024-01-27 17:20:12 +0700

    crowdfunding-product-page: add more tests

--- tests/crowdfunding-product-page.spec.ts
+++ tests/crowdfunding-product-page.spec.ts
@@ -22,4 +22,29 @@ test.describe("FrontendMentor Challenge - Crowdfunding product Page", () => {
       }),
     ).toBeVisible();
   });
+
+  /** Test if the page has a top navigation bar inside a header */
+  test.describe("has a page header header", () => {
+    test("header is visible and in viewport", async ({ page }) => {
+      const header = page.locator("header");
+      await expect(header).toBeVisible();
+      await expect(header).toBeInViewport();
+    });
+    test("crowdfund logo is visible and in viewport", async ({ page }) => {
+      const logo = page
+        .locator("header")
+        .getByRole("img", { name: "Crowdfunding Logo" });
+      await expect(logo).toBeVisible();
+      await expect(logo).toBeInViewport();
+    });
+    test("navigation is visible and in viewport", async ({ page }) => {
+      const navbar = page.locator("header").getByRole("navigation");
+      await expect(navbar).toBeVisible();
+      await expect(navbar).toBeInViewport();
+      const links = ["About", "Discover", "Get Started"];
+      for (const link of links) {
+        await expect(navbar.getByRole("link", { name: link })).toBeVisible();
+      }
+    });
+  });
 });
