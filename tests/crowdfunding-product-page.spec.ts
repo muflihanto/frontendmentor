b2b25f18937377de51f3b3eaecf53b27a8d71f04
muflihanto <muflihanto@gmail.com>
2024-01-27 20:05:02 +0700

    crowdfunding-product-page: add more tests

--- tests/crowdfunding-product-page.spec.ts
+++ tests/crowdfunding-product-page.spec.ts
@@ -13,16 +13,6 @@ test.describe("FrontendMentor Challenge - Crowdfunding product Page", () => {
     );
   });
 
-  /** Test if the page has a correct heading */
-  test("has a heading", async ({ page }) => {
-    await expect(
-      page.getByRole("heading", {
-        level: 1,
-        name: "Mastercraft Bamboo Monitor Riser",
-      }),
-    ).toBeVisible();
-  });
-
   /** Test if the page has a top navigation bar inside a header */
   test.describe("has a page header header", () => {
     test("header is visible and in viewport", async ({ page }) => {
@@ -45,6 +35,37 @@ test.describe("FrontendMentor Challenge - Crowdfunding product Page", () => {
       for (const link of links) {
         await expect(navbar.getByRole("link", { name: link })).toBeVisible();
       }
+    });
+  });
+
+  /** Test if the page has an 'Overview' card */
+  test.describe("has an 'Overview' card", () => {
+    test("card is visible", async ({ page }) => {
+      const card = page.locator("div").nth(6);
+      await expect(card).toBeVisible();
+    });
+    test("card has all elements", async ({ page }) => {
+      const card = page.locator("div").nth(6);
+      await expect(
+        card.getByRole("img", { name: "Mastercraft Brand Logo" }),
+      ).toBeVisible();
+      await expect(
+        card.getByRole("heading", {
+          level: 1,
+          name: "Mastercraft Bamboo Monitor Riser",
+        }),
+      ).toBeVisible();
+      await expect(
+        card.getByText(
+          "A beautifully & handcrafted monitor stand to reduce neck and eye strain.",
+        ),
+      ).toBeVisible();
+      await expect(
+        card.getByRole("button", { name: "Back this project" }),
+      ).toBeVisible();
+      await expect(
+        card.getByRole("button", { name: "Bookmark" }),
+      ).toBeVisible();
     });
   });
 });
