20d48aac31134720da1549c953ff36779b899fa8
muflihanto <muflihanto@gmail.com>
2024-02-02 19:23:14 +0700

    fylo-data-storage-component: add more tests

--- tests/fylo-data-storage-component.spec.ts
+++ tests/fylo-data-storage-component.spec.ts
@@ -13,10 +13,45 @@ test.describe("FrontendMentor Challenge - Fylo data storage component Page", () 
     );
   });
 
-  /** Test if the page has a header image */
-  test("has a header image", async ({ page }) => {
+  /** Test if the page has a header */
+  test.describe("has a header", () => {
+    test("header is visible", async ({ page }) => {
+      await expect(page.getByRole("banner")).toBeVisible();
+      await expect(page.getByRole("banner")).toBeInViewport();
+    });
+    test("has all elements", async ({ page }) => {
+      const header = page.getByRole("banner");
+      await expect(
+        header.getByRole("img", { name: "Fylo Company Logo" }),
+      ).toBeVisible();
+      const links = await header.getByRole("list").getByRole("link").all();
+      expect(links).toHaveLength(3);
+      for (const link of links) {
+        await expect(link.getByRole("img")).toBeVisible();
+      }
+    });
+  });
+
+  /** Test if the page has a status section */
+  test.describe("has a status section", () => {
+    test("section is visible", async ({ page }) => {
+      await expect(page.locator("div").nth(4)).toBeVisible();
+      await expect(page.getByText("185 GB Left")).toBeVisible();
+    });
+    test("has all elements", async ({ page }) => {
+      const section = page.locator("div").nth(4);
+      await expect(
+        section.getByText("You‘ve used 815 GB of your storage"),
+      ).toBeVisible();
+      await expect(section.getByText("0 GB", { exact: true })).toBeVisible();
+      await expect(section.getByText("1000 GB")).toBeVisible();
+    });
+  });
+
+  /** Test if the page has a footer */
+  test("has a footer", async ({ page }) => {
     await expect(
-      page.getByRole("img", { name: "Fylo Company Logo" }),
+      page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
     ).toBeVisible();
   });
 });
