69374ed5e169ad3c4ad3f5386f2cd2666450667e
muflihanto <muflihanto@gmail.com>
2024-02-10 18:56:49 +0700

    insure-landing-page: add more tests

--- tests/insure-landing-page.spec.ts
+++ tests/insure-landing-page.spec.ts
@@ -11,6 +11,27 @@ test.describe("FrontendMentor Challenge - Insure landing Page", () => {
     await expect(page).toHaveTitle("Frontend Mentor | Insure landing page");
   });
 
+  /** Test if the page has a header*/
+  test.describe("has a header", () => {
+    test("header is visible", async ({ page }) => {
+      await expect(page.getByRole("banner")).toBeVisible();
+      await expect(page.getByRole("banner")).toBeInViewport();
+    });
+    test("has all elements", async ({ page }) => {
+      const header = page.getByRole("banner");
+      // has an insure logo
+      await expect(
+        header.getByRole("img", { name: "insure logo" }),
+      ).toBeVisible();
+      // has a navigation links
+      const nav = header.getByRole("navigation");
+      const links = ["How we work", "Blog", "Account", "View plans"];
+      for (const link of links) {
+        await expect(nav.getByRole("link", { name: link })).toBeVisible();
+      }
+    });
+  });
+
   /** Test if the page has a heading */
   test("has a heading", async ({ page }) => {
     await expect(
