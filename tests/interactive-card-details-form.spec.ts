d692a86516c69cd9423846ac79db8a2956cd0ee7
muflihanto <muflihanto@gmail.com>
2024-02-15 13:08:19 +0700

    interactive-card-details-form: add more tests

--- tests/interactive-card-details-form.spec.ts
+++ tests/interactive-card-details-form.spec.ts
@@ -13,6 +13,34 @@ test.describe("FrontendMentor Challenge - Interactive card details form Page", (
     );
   });
 
+  /** Test if the page has a front card preview */
+  test.describe("has a front card preview", () => {
+    test("card is visible", async ({ page }) => {
+      await expect(page.locator("div").nth(5)).toBeVisible();
+    });
+    test("has all elements", async ({ page }) => {
+      const card = page.locator("div").nth(5);
+      // has an initial card number
+      await expect(card.getByText("0000 0000 0000 0000")).toBeVisible();
+      // has an initial cardholder name
+      await expect(card.getByText("Jane Appleseed")).toBeVisible();
+      // has an initial exp. date
+      await expect(card.getByText("00/00")).toBeVisible();
+    });
+  });
+
+  /** Test if the page has a back card preview */
+  test.describe("has a back card preview", () => {
+    test("card is visible", async ({ page }) => {
+      await expect(page.locator("div").nth(4)).toBeVisible();
+    });
+    test("has all elements", async ({ page }) => {
+      const card = page.locator("div").nth(4);
+      // has an initial cvc number
+      await expect(card.getByText("000", { exact: true })).toBeVisible();
+    });
+  });
+
   /** Test if the page has a correct footer */
   test("has a footer", async ({ page }) => {
     await expect(page.getByRole("contentinfo")).toBeVisible();
