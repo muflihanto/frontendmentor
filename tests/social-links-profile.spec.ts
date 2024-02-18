a37a13029788ccb8ce0f380d2ebcbc4ca0f93a9e
muflihanto <muflihanto@gmail.com>
2024-02-18 19:11:49 +0700

    social-links-profile: add more tests

--- tests/social-links-profile.spec.ts
+++ tests/social-links-profile.spec.ts
@@ -11,10 +11,53 @@ test.describe("FrontendMentor Challenge - Social links profile Page", () => {
     await expect(page).toHaveTitle("Frontend Mentor | Social links profile");
   });
 
-  /** Test if the page has a correct heading */
-  test("has a heading", async ({ page }) => {
+  /** Test if the page has a main card */
+  test("has a main card", async ({ page }) => {
+    const card = page.getByRole("main");
+    await expect(card).toBeVisible();
     await expect(
-      page.getByRole("heading", { level: 1, name: "Jessica Randall" }),
+      card.getByRole("img", { name: "Jessica Randall's Avatar" }),
+    ).toBeVisible();
+    await expect(
+      card.getByRole("heading", { level: 1, name: "Jessica Randall" }),
+    ).toBeVisible();
+    await expect(card.getByText("London, United Kingdom")).toBeVisible();
+    await expect(
+      card.getByText('"Front-end developer and avid reader."'),
+    ).toBeVisible();
+    // has social links
+    const links = [
+      {
+        name: "GitHub",
+        href: "",
+      },
+      {
+        name: "Frontend Mentor",
+        href: "",
+      },
+      {
+        name: "LinkedIn",
+        href: "",
+      },
+      {
+        name: "Twitter",
+        href: "",
+      },
+      {
+        name: "Instagram",
+        href: "",
+      },
+    ];
+    const linkContainer = card.locator("ul");
+    for (const { name } of links) {
+      await expect(linkContainer.getByRole("link", { name })).toBeVisible();
+    }
+  });
+
+  /** Test if the page has a footer */
+  test("has a footer", async ({ page }) => {
+    await expect(
+      page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
     ).toBeVisible();
   });
 });
