ae4543b908c7962103bbe848df3d5244c7821ca5
muflihanto <muflihanto@gmail.com>
2024-02-23 19:13:24 +0700

    recipe-page: add more tests

--- tests/recipe-page.spec.ts
+++ tests/recipe-page.spec.ts
@@ -12,17 +12,37 @@ test.describe("FrontendMentor Challenge - Recipe Page", () => {
   });
 
   /** Test if the page has a main card */
-  test("has a main card", async ({ page }) => {
-    const card = page.getByRole("main");
-    await expect(card).toBeVisible();
-    await expect(
-      card.getByRole("heading", { level: 1, name: "Simple Omelette Recipe" }),
-    ).toBeVisible();
-    await expect(
-      card.getByText(
-        " An easy and quick dish, perfect for any meal. This classic omelette combines beaten eggs cooked to perfection, optionally filled with your choice of cheese, vegetables, or meats.",
-      ),
-    ).toBeVisible();
+  test.describe("has a main card", () => {
+    test("has a header", async ({ page }) => {
+      const header = page.getByRole("banner");
+      await expect(header).toBeVisible();
+      await expect(
+        header.getByRole("img", { name: "Omelette on a plate" }),
+      ).toBeVisible();
+    });
+
+    test("has main element", async ({ page }) => {
+      const card = page.getByRole("main");
+      await expect(card).toBeVisible();
+      await expect(
+        card.getByRole("heading", { level: 1, name: "Simple Omelette Recipe" }),
+      ).toBeVisible();
+      await expect(
+        card.getByText(
+          " An easy and quick dish, perfect for any meal. This classic omelette combines beaten eggs cooked to perfection, optionally filled with your choice of cheese, vegetables, or meats.",
+        ),
+      ).toBeVisible();
+    });
+
+    test("has 'Preparation time' section", async ({ page }) => {
+      const section = page.locator("section").first();
+      await section.scrollIntoViewIfNeeded();
+      await expect(
+        section.getByRole("heading", { name: "Preparation time" }),
+      ).toBeVisible();
+      const listItem = await section.getByRole("listitem").all();
+      expect(listItem).toHaveLength(3);
+    });
   });
 
   /** Test if the page has a footer */
