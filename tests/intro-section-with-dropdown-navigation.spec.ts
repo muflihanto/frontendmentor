74124c6777e2e7b938c9466be66cdd07b30db53b
muflihanto <muflihanto@gmail.com>
2024-02-24 21:02:55 +0700

    intro-section-with-dropdown-navigation: add more tests

--- tests/intro-section-with-dropdown-navigation.spec.ts
+++ tests/intro-section-with-dropdown-navigation.spec.ts
@@ -13,6 +13,37 @@ test.describe("FrontendMentor Challenge - Intro section with dropdown navigation
     );
   });
 
+  /** Test if the page has a header */
+  test("has a header", async ({ page }) => {
+    const header = page.getByRole("banner");
+    await expect(header.getByRole("img").first()).toBeVisible();
+    const nav = header.getByRole("navigation");
+    const listItems = [
+      {
+        parent: "Features",
+        children: ["Todo List", "Calendar", "Reminders", "Planning"],
+      },
+      { parent: "Company", children: ["History", "Our Team", "Blog"] },
+      { parent: "Careers" },
+      { parent: "About" },
+    ];
+    for (const listItem of listItems) {
+      if (listItem.children !== undefined) {
+        await expect(
+          nav.locator("summary").filter({ hasText: listItem.parent }),
+        ).toBeVisible();
+      } else {
+        await expect(
+          nav.locator("li").filter({ hasText: listItem.parent }),
+        ).toBeVisible();
+      }
+    }
+    const links = ["Login", "Register"];
+    for (const link of links) {
+      await expect(nav.getByRole("link", { name: link })).toBeVisible();
+    }
+  });
+
   /** Test if the page has a correct heading */
   test("has a heading", async ({ page }) => {
     await expect(
