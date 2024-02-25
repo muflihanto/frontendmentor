7ae32201b158991fa4880fc77fa22f9cd7a8aae1
muflihanto <muflihanto@gmail.com>
2024-02-25 17:34:40 +0700

    intro-section-with-dropdown-navigation: add more tests

--- tests/intro-section-with-dropdown-navigation.spec.ts
+++ tests/intro-section-with-dropdown-navigation.spec.ts
@@ -1,6 +1,16 @@
 import { test, expect } from "@playwright/test";
 
 test.describe("FrontendMentor Challenge - Intro section with dropdown navigation Page", () => {
+  const listItems = [
+    {
+      parent: "Features",
+      children: ["Todo List", "Calendar", "Reminders", "Planning"],
+    },
+    { parent: "Company", children: ["History", "Our Team", "Blog"] },
+    { parent: "Careers" },
+    { parent: "About" },
+  ];
+
   /** Go to Intro section with dropdown navigation page before each test */
   test.beforeEach("Open", async ({ page }) => {
     await page.goto("/intro-section-with-dropdown-navigation");
@@ -18,15 +28,6 @@ test.describe("FrontendMentor Challenge - Intro section with dropdown navigation
     const header = page.getByRole("banner");
     await expect(header.getByRole("img").first()).toBeVisible();
     const nav = header.getByRole("navigation");
-    const listItems = [
-      {
-        parent: "Features",
-        children: ["Todo List", "Calendar", "Reminders", "Planning"],
-      },
-      { parent: "Company", children: ["History", "Our Team", "Blog"] },
-      { parent: "Careers" },
-      { parent: "About" },
-    ];
     for (const listItem of listItems) {
       if (listItem.children !== undefined) {
         await expect(
@@ -41,6 +42,24 @@ test.describe("FrontendMentor Challenge - Intro section with dropdown navigation
     const links = ["Login", "Register"];
     for (const link of links) {
       await expect(nav.getByRole("link", { name: link })).toBeVisible();
+    }
+  });
+
+  /** Test if the dropdown menu works */
+  test("dropdown menu works", async ({ page }) => {
+    const nav = page.getByRole("banner").getByRole("navigation");
+    for (const listItem of listItems) {
+      if (listItem.children !== undefined) {
+        const menu = nav
+          .locator("summary")
+          .filter({ hasText: listItem.parent });
+        await expect(menu).toBeVisible();
+        await menu.click();
+        // TODO: simulate hover
+        await expect(page.getByText(listItem.children.join(""))).toBeVisible();
+        await menu.click();
+        await page.waitForTimeout(500);
+      }
     }
   });
 
@@ -73,5 +92,12 @@ test.describe("FrontendMentor Challenge - Intro section with dropdown navigation
   /** Test if the page has a hero image */
   test("has a hero image", async ({ page }) => {
     await expect(page.getByRole("img", { name: "Hero Image" })).toBeVisible();
+  });
+
+  /** Test if the page has a footer */
+  test("has a footer", async ({ page }) => {
+    await expect(
+      page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
+    ).toBeVisible();
   });
 });
