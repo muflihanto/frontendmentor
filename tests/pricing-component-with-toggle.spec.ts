d3354f891d4ce4e65aa4c2d9c43e653c22f67625
muflihanto <muflihanto@gmail.com>
2024-02-26 20:05:48 +0700

    pricing-component-with-toggle: add more tests

--- tests/pricing-component-with-toggle.spec.ts
+++ tests/pricing-component-with-toggle.spec.ts
@@ -1,5 +1,35 @@
 import { test, expect } from "@playwright/test";
 
+const prices = {
+  basic: {
+    storage: "500 GB",
+    users: 2,
+    bandwith: "3 GB",
+    price: {
+      annually: 199.99,
+      monthly: 19.99,
+    },
+  },
+  professional: {
+    storage: "1 TB",
+    users: 5,
+    bandwith: "10 GB",
+    price: {
+      annually: 249.99,
+      monthly: 24.99,
+    },
+  },
+  master: {
+    storage: "2 TB",
+    users: 10,
+    bandwith: "20 GB",
+    price: {
+      annually: 399.99,
+      monthly: 39.99,
+    },
+  },
+};
+
 test.describe("FrontendMentor Challenge - Pricing component with toggle Page", () => {
   /** Go to Pricing component with toggle page before each test */
   test.beforeEach("Open", async ({ page }) => {
@@ -17,6 +47,45 @@ test.describe("FrontendMentor Challenge - Pricing component with toggle Page", (
   test("has a heading", async ({ page }) => {
     await expect(
       page.getByRole("heading", { level: 1, name: "Our Pricing" }),
+    ).toBeVisible();
+  });
+
+  /** Test if the page has a toggle group */
+  test("has a toggle group", async ({ page }) => {
+    const container = page.getByText("AnnuallyMonthly");
+    await expect(container.getByText("Annually")).toBeVisible();
+    await expect(container.getByRole("button")).toBeVisible();
+    await expect(container.getByText("Monthly")).toBeVisible();
+  });
+
+  /** Test if the page has a footer */
+  test.describe("has all cards", () => {
+    const cardIndexes = [5, 12, 19];
+    for (const [index, [plan, detail]] of Object.entries(
+      Object.entries(prices),
+    )) {
+      test(`has a ${plan} card`, async ({ page }) => {
+        const card = page.locator("div").nth(cardIndexes[Number(index)]);
+        await expect(card).toBeVisible();
+        await expect(card.getByText(plan, { exact: true })).toBeVisible();
+        await expect(card.getByText(`$${detail.price.annually}`)).toBeVisible();
+        await expect(card.getByText(`${detail.storage} Storage`)).toBeVisible();
+        await expect(
+          card.getByText(`${detail.users} Users Allowed`),
+        ).toBeVisible();
+        await expect(
+          card.getByText(`Send up to ${detail.bandwith}`),
+        ).toBeVisible();
+        await expect(card.getByRole("link")).toBeVisible();
+        await expect(card.getByRole("link")).toHaveText("Learn More");
+      });
+    }
+  });
+
+  /** Test if the page has a footer */
+  test("has a footer", async ({ page }) => {
+    await expect(
+      page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
     ).toBeVisible();
   });
 });
