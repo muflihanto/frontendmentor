3ddcfebc088d1adbac0dccd2954ef90a5e003215
muflihanto <muflihanto@gmail.com>
2024-02-26 19:45:19 +0700

    advice-generator-app: add more tests

--- tests/advice-generator-app.spec.ts
+++ tests/advice-generator-app.spec.ts
@@ -11,8 +11,30 @@ test.describe("FrontendMentor Challenge - Advice generator app Page", () => {
     await expect(page).toHaveTitle("Frontend Mentor | Advice generator app");
   });
 
-  /** Test if the page has a correct dice button */
-  test("has a dice button", async ({ page }) => {
-    await expect(page.getByRole("button", { name: "Icon Dice" })).toBeVisible();
+  /** Test if the page works  */
+  test.describe("page works correctly", () => {
+    test("show loading state", async ({ page }) => {
+      await expect(page.getByText("Advice #...")).toBeVisible();
+      await expect(page.getByRole("status")).toBeVisible();
+    });
+    test("dice button works", async ({ page }) => {
+      const button = page.getByRole("button", { name: "Icon Dice" });
+      await expect(button).toBeVisible();
+      await page.waitForTimeout(1000);
+      await expect(page.getByRole("status")).not.toBeVisible();
+      await expect(page.locator("div").nth(3)).toHaveText(/^\".*\"$/);
+    });
+  });
+
+  /** Test if the page has a divider */
+  test("has a divider", async ({ page }) => {
+    await expect(page.getByRole("img", { name: "Line Divider" })).toBeVisible();
+  });
+
+  /** Test if the page has a footer */
+  test("has a footer", async ({ page }) => {
+    await expect(
+      page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
+    ).toBeVisible();
   });
 });
