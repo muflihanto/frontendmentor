a0f3442f68857cb4df05b04419b6ea105f63db94
muflihanto <muflihanto@gmail.com>
2024-02-29 16:56:31 +0700

    clipboard-landing-page: add more test

--- tests/clipboard-landing-page.spec.ts
+++ tests/clipboard-landing-page.spec.ts
@@ -11,13 +11,26 @@ test.describe("FrontendMentor Challenge - Clipboard landing Page", () => {
     await expect(page).toHaveTitle("Frontend Mentor | Clipboard landing page");
   });
 
-  /** Test if the page has a heading */
-  test("has a heading", async ({ page }) => {
+  /** Test if the page has a main section */
+  test("has a main section", async ({ page }) => {
+    const main = page.getByRole("main");
+    await expect(main.getByRole("img")).toBeVisible();
     await expect(
-      page.getByRole("heading", {
+      main.getByRole("heading", {
         level: 1,
         name: "A history of everything you copy",
       }),
+    ).toBeVisible();
+    await expect(
+      main.getByText(
+        "Clipboard allows you to track and organize everything you copy. Instantly access",
+      ),
+    ).toBeVisible();
+    await expect(
+      main.getByRole("link", { name: "Download for iOS" }),
+    ).toBeVisible();
+    await expect(
+      main.getByRole("link", { name: "Download for Mac" }),
     ).toBeVisible();
   });
 });
