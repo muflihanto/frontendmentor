faaddb70ca0d8bdb3ca812a47f47b923cfdb1f07
muflihanto <muflihanto@gmail.com>
2024-02-29 17:01:56 +0700

    clipboard-landing-page: add more test

--- tests/clipboard-landing-page.spec.ts
+++ tests/clipboard-landing-page.spec.ts
@@ -14,6 +14,8 @@ test.describe("FrontendMentor Challenge - Clipboard landing Page", () => {
   /** Test if the page has a main section */
   test("has a main section", async ({ page }) => {
     const main = page.getByRole("main");
+    await expect(main).toBeVisible();
+    await expect(main).toBeInViewport();
     await expect(main.getByRole("img")).toBeVisible();
     await expect(
       main.getByRole("heading", {
@@ -23,7 +25,7 @@ test.describe("FrontendMentor Challenge - Clipboard landing Page", () => {
     ).toBeVisible();
     await expect(
       main.getByText(
-        "Clipboard allows you to track and organize everything you copy. Instantly access",
+        "Clipboard allows you to track and organize everything you copy. Instantly access your clipboard on all your devices.",
       ),
     ).toBeVisible();
     await expect(
@@ -31,6 +33,47 @@ test.describe("FrontendMentor Challenge - Clipboard landing Page", () => {
     ).toBeVisible();
     await expect(
       main.getByRole("link", { name: "Download for Mac" }),
+    ).toBeVisible();
+  });
+
+  /** Test if the page has a 'Snippet' section */
+  test("has a 'Snippet' section", async ({ page }) => {
+    const section = page.locator("section").first();
+    await section.scrollIntoViewIfNeeded();
+    await expect(
+      section.getByRole("heading", { name: "Keep track of your snippets" }),
+    ).toBeVisible();
+    await expect(
+      section.getByText(
+        "Clipboard instantly stores any item you copy in the cloud, meaning you can access your snippets immediately on all your devices. Our Mac and iOS apps will help you organize everything.",
+      ),
+    ).toBeVisible();
+    await expect(
+      section.getByRole("img", { name: "Computer Illustration" }),
+    ).toBeVisible();
+    await expect(
+      section.getByRole("heading", { name: "Quick Search" }),
+    ).toBeVisible();
+    await expect(
+      section.getByText(
+        "Easily search your snippets by content, category, web address, application, and more.",
+      ),
+    ).toBeVisible();
+    await expect(
+      section.getByRole("heading", { name: "iCloud Sync" }),
+    ).toBeVisible();
+    await expect(
+      section.getByText(
+        "Instantly saves and syncs snippets across all your devices.",
+      ),
+    ).toBeVisible();
+    await expect(
+      section.getByRole("heading", { name: "Complete History" }),
+    ).toBeVisible();
+    await expect(
+      section.getByText(
+        "Retrieve any snippets from the first moment you started using the app.",
+      ),
     ).toBeVisible();
   });
 });
