6cfbabe47e774b7fe9e720ed56a69ffbda7a4a13
muflihanto <muflihanto@gmail.com>
2024-03-06 20:07:50 +0700

    fylo-dark-theme-landing-page: add more test

--- tests/fylo-dark-theme-landing-page.spec.ts
+++ tests/fylo-dark-theme-landing-page.spec.ts
@@ -49,4 +49,39 @@ test.describe("FrontendMentor Challenge - Fylo landing page with dark theme and 
       section.getByRole("link", { name: "Get Started" }),
     ).toBeVisible();
   });
+
+  /** Test if the page has a 'Features' section */
+  test("has a 'Features' section", async ({ page }) => {
+    const section = page.locator("div").nth(8);
+    await section.scrollIntoViewIfNeeded();
+    await expect(section).toBeVisible();
+    const features = [
+      {
+        heading: "Access your files, anywhere",
+        body: "The ability to use a smartphone, tablet, or computer to access your account means your files follow you everywhere.",
+      },
+      {
+        heading: "Security you can trust",
+        body: "2-factor authentication and user-controlled encryption are just a couple of the security features we allow to help secure your files.",
+      },
+      {
+        heading: "Real-time collaboration",
+        body: "Securely share files and folders with friends, family and colleagues for live collaboration. No email attachments required.",
+      },
+      {
+        heading: "Store any type of file",
+        body: "Whether you're sharing holidays photos or work documents, Fylo has you covered allowing for all file types to be securely stored and shared.",
+      },
+    ];
+    const grids = await section.locator(">div").all();
+    expect(grids).toHaveLength(4);
+    for (const [index, grid] of Object.entries(grids)) {
+      const indexNum = Number(index);
+      await expect(grid.getByRole("img")).toBeVisible();
+      await expect(
+        grid.getByRole("heading", { name: features[indexNum].heading }),
+      ).toBeVisible();
+      await expect(grid.getByText(features[indexNum].body)).toBeVisible();
+    }
+  });
 });
