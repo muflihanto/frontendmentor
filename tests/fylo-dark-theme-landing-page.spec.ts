ce20cace927b28e34389e9544466579b9084aa3b
muflihanto <muflihanto@gmail.com>
2024-03-06 19:48:51 +0700

    fylo-dark-theme-landing-page: add more test

--- tests/fylo-dark-theme-landing-page.spec.ts
+++ tests/fylo-dark-theme-landing-page.spec.ts
@@ -13,6 +13,19 @@ test.describe("FrontendMentor Challenge - Fylo landing page with dark theme and 
     );
   });
 
+  /** Test if the page has a header */
+  test("has a header", async ({ page }) => {
+    const header = page.getByRole("banner");
+    await expect(header).toBeVisible();
+    await expect(header).toBeInViewport();
+    await expect(header.getByRole("img", { name: "Fylo Logo" })).toBeVisible();
+    const nav = header.getByRole("navigation");
+    const navlinks = ["Features", "Team", "Sign In"];
+    for (const link of navlinks) {
+      await expect(nav.getByRole("link", { name: link })).toBeVisible();
+    }
+  });
+
   /** Test if the page has a heading */
   test("has a heading", async ({ page }) => {
     await expect(
