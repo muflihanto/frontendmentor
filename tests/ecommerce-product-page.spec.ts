4d602fef218a68e6b993515ffa5dd8d9e165fb84
muflihanto <muflihanto@gmail.com>
2024-03-17 20:18:25 +0700

    ecommerce-product-page: add more tests

--- tests/ecommerce-product-page.spec.ts
+++ tests/ecommerce-product-page.spec.ts
@@ -11,6 +11,25 @@ test.describe("FrontendMentor Challenge - E-commerce product Page", () => {
     await expect(page).toHaveTitle("Frontend Mentor | E-commerce product page");
   });
 
+  /** Test if the page has a header */
+  test("has a header", async ({ page }) => {
+    const header = page.getByRole("banner");
+    await expect(header).toBeVisible();
+    await expect(header).toBeInViewport();
+    await expect(header.getByRole("img").first()).toBeVisible();
+    const navlinks = ["Collections", "Men", "Women", "About", "Contact"];
+    const nav = page.getByRole("navigation");
+    for (const link of navlinks) {
+      await expect(
+        nav.getByRole("link", { name: link, exact: true }),
+      ).toBeVisible();
+    }
+    await expect(page.locator("#cart-toggle")).toBeVisible();
+    await expect(
+      page.getByRole("button", { name: "User's avatar" }),
+    ).toBeVisible();
+  });
+
   /** Test if the page has a heading */
   test("has a heading", async ({ page }) => {
     await expect(
@@ -18,6 +37,13 @@ test.describe("FrontendMentor Challenge - E-commerce product Page", () => {
         level: 1,
         name: "Fall Limited Edition Sneakers",
       }),
+    ).toBeVisible();
+  });
+
+  /** Test if the page has a footer */
+  test("has a footer", async ({ page }) => {
+    await expect(
+      page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
     ).toBeVisible();
   });
 });
