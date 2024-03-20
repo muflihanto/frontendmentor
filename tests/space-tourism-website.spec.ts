9986401b074655267e2fce3d9b1d716835cc627e
muflihanto <muflihanto@gmail.com>
2024-03-20 16:42:40 +0700

    space-tourism-website: add more test

--- tests/space-tourism-website.spec.ts
+++ tests/space-tourism-website.spec.ts
@@ -35,8 +35,20 @@ test.describe("FrontendMentor Challenge - Space Tourism Website Page", () => {
     }
   });
 
-  /** Test if the page has a heading */
-  test("has a heading", async ({ page }) => {
-    await expect(page.getByRole("heading", { name: "Space" })).toBeVisible();
+  /** Test if the page has a main section */
+  test("has a main section", async ({ page }) => {
+    const section = page.locator("div").nth(8);
+    await expect(section).toBeVisible();
+    await expect(section).toBeInViewport();
+    await expect(
+      section.getByRole("heading", { name: "So, you want to travel to" }),
+    ).toBeVisible();
+    await expect(section.getByRole("heading", { name: "Space" })).toBeVisible();
+    await expect(
+      section.getByText(
+        "Let’s face it; if you want to go to space, you might as well genuinely go to outer space and not hover kind of on the edge of it. Well sit back, and relax because we’ll give you a truly out of this world experience!",
+      ),
+    ).toBeVisible();
+    await expect(section.getByRole("link", { name: "Explore" })).toBeVisible();
   });
 });
