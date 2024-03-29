56543cd13211915f9dc7afe7f0a5530c89d18bf7
muflihanto <muflihanto@gmail.com>
2024-03-29 18:43:47 +0700

    room-homepage: add more tests

--- tests/room-homepage.spec.ts
+++ tests/room-homepage.spec.ts
@@ -1,5 +1,24 @@
 import { test, expect } from "@playwright/test";
 
+const links = [
+  {
+    href: "",
+    display: "Home",
+  },
+  {
+    href: "",
+    display: "Shop",
+  },
+  {
+    href: "",
+    display: "About",
+  },
+  {
+    href: "",
+    display: "Contact",
+  },
+];
+
 test.describe("FrontendMentor Challenge - Room homepage Page", () => {
   /** Go to Room homepage page before each test */
   test.beforeEach("Open", async ({ page }) => {
@@ -11,6 +30,19 @@ test.describe("FrontendMentor Challenge - Room homepage Page", () => {
     await expect(page).toHaveTitle("Frontend Mentor | Room homepage");
   });
 
+  /** Test if the page has a header */
+  test("has a header", async ({ page }) => {
+    const header = page.getByRole("banner");
+    await expect(header).toBeVisible();
+    await expect(header).toBeInViewport();
+    await expect(header.getByRole("img")).toBeVisible();
+    for (const link of links) {
+      await expect(
+        header.getByRole("link", { name: link.display }),
+      ).toBeVisible();
+    }
+  });
+
   /** Test if the page has a heading */
   test("has a heading", async ({ page }) => {
     await expect(
@@ -18,6 +50,13 @@ test.describe("FrontendMentor Challenge - Room homepage Page", () => {
         level: 1,
         name: "Discover innovative ways to decorate",
       }),
+    ).toBeVisible();
+  });
+
+  /** Test if the page has a footer */
+  test("has a footer", async ({ page }) => {
+    await expect(
+      page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
     ).toBeVisible();
   });
 });
