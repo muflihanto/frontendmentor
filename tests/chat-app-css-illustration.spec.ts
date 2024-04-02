60a2605a9a40207d185957e4d18e44e244cdca69
muflihanto <muflihanto@gmail.com>
2024-04-02 08:16:22 +0700

    chat-app-css-illustration: add more tests

--- tests/chat-app-css-illustration.spec.ts
+++ tests/chat-app-css-illustration.spec.ts
@@ -13,6 +13,14 @@ test.describe("FrontendMentor Challenge - Chat app CSS illustration Page", () =>
     );
   });
 
+  /** Test if the page has a gradient background image */
+  test("has a gradient background image", async ({ page }) => {
+    await expect(page.locator(".absolute").first()).toHaveCSS(
+      "background-image",
+      "linear-gradient(200deg, rgb(233, 66, 255) -40%, rgb(136, 56, 255))",
+    );
+  });
+
   /** Test if the page has a heading */
   test("has a heading", async ({ page }) => {
     await expect(
@@ -20,6 +28,22 @@ test.describe("FrontendMentor Challenge - Chat app CSS illustration Page", () =>
         level: 1,
         name: "Simple booking",
       }),
+    ).toBeVisible();
+  });
+
+  /** Test if the page has a description text */
+  test("has a description text", async ({ page }) => {
+    await expect(
+      page.getByText(
+        "Stay in touch with our dog walkers through the chat interface. This makes it easy to discuss arrangements and make bookings. Once the walk has been completed you can rate your walker and book again all through the chat.",
+      ),
+    ).toBeVisible();
+  });
+
+  /** Test if the page has a footer */
+  test("has a footer", async ({ page }) => {
+    await expect(
+      page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
     ).toBeVisible();
   });
 });
