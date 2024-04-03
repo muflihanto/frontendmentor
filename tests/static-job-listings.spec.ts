cfb68945118ff7d14c9b061b522d48bf56fbd609
muflihanto <muflihanto@gmail.com>
2024-04-03 18:32:08 +0700

    static-job-listings: add more tests

--- tests/static-job-listings.spec.ts
+++ tests/static-job-listings.spec.ts
@@ -11,6 +11,24 @@ test.describe("FrontendMentor Challenge - Job Listings Page", () => {
     await expect(page).toHaveTitle("Frontend Mentor | Job Listings");
   });
 
+  /** Test if the page has a header */
+  test("has a header", async ({ page }) => {
+    const header = page.getByRole("banner");
+    await expect(header).toBeVisible();
+    await expect(header).toBeInViewport();
+    await expect(header).toHaveCSS(
+      "background-image",
+      'url("http://localhost:3000/static-job-listings/images/bg-header-desktop.svg")',
+    );
+  });
+
+  /** Test if the page has correct initial cards */
+  test("has correct initial cards", async ({ page }) => {
+    const container = page.locator("div").nth(3);
+    await expect(container).toBeVisible();
+    expect(await container.locator(">div").all()).toHaveLength(10);
+  });
+
   /** Test if the page has a footer */
   test("has a footer", async ({ page }) => {
     await expect(
