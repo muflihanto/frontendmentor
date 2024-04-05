5531a21aae04ddfca2d7fd1f8b5fcb4e1865c7d4
muflihanto <muflihanto@gmail.com>
2024-04-05 18:56:42 +0700

    easybank-landing-page: add more test

--- tests/easybank-landing-page.spec.ts
+++ tests/easybank-landing-page.spec.ts
@@ -1,5 +1,7 @@
 import { test, expect } from "@playwright/test";
 
+const navs = ["Home", "About", "Contact", "Blog", "Careers"];
+
 test.describe("FrontendMentor Challenge - Easybank landing Page", () => {
   /** Go to Easybank landing page before each test */
   test.beforeEach("Open", async ({ page }) => {
@@ -9,6 +11,18 @@ test.describe("FrontendMentor Challenge - Easybank landing Page", () => {
   /** Test if the page has a correct title */
   test("has title", async ({ page }) => {
     await expect(page).toHaveTitle("Frontend Mentor | Easybank landing page");
+  });
+
+  /** Test if the page has a header */
+  test("has a header", async ({ page }) => {
+    const header = page.locator("div").nth(2);
+    await expect(header.getByRole("img")).toBeVisible();
+    for (const nav of navs) {
+      await expect(header.getByRole("link", { name: nav })).toBeVisible();
+    }
+    await expect(
+      header.getByRole("button", { name: "Request Invite" }),
+    ).toBeVisible();
   });
 
   /** Test if the page has a heading */
