f4687b9e1df44a78bc61c2782cba9cc2b81afbcb
muflihanto <muflihanto@gmail.com>
2024-04-08 20:40:18 +0700

    manage-landing-page: add more test

--- tests/manage-landing-page.spec.ts
+++ tests/manage-landing-page.spec.ts
@@ -11,6 +11,20 @@ test.describe("FrontendMentor Challenge - Manage landing Page", () => {
     await expect(page).toHaveTitle("Frontend Mentor | Manage landing page");
   });
 
+  /** Test if the page has a header */
+  test("has a header", async ({ page }) => {
+    const header = page.getByRole("banner");
+    await expect(header.getByRole("img")).toBeVisible();
+    const navs = ["Pricing", "Product", "About Us", "Careers", "Community"];
+    const navEl = header.locator("div").first();
+    for (const nav of navs) {
+      await expect(navEl.getByRole("link", { name: nav })).toBeVisible();
+    }
+    await expect(
+      header.getByRole("button", { name: "Get Started" }),
+    ).toBeVisible();
+  });
+
   /** Test if the page has a heading */
   test("has a heading", async ({ page }) => {
     await expect(
