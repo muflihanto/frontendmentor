67440b14533bd52e1fd4e1345007e7c81f60f1f8
muflihanto <muflihanto@gmail.com>
2024-04-09 10:58:35 +0700

    manage-landing-page: add more test

--- tests/manage-landing-page.spec.ts
+++ tests/manage-landing-page.spec.ts
@@ -30,6 +30,7 @@ test.describe("FrontendMentor Challenge - Manage landing Page", () => {
     const section = page.locator("div").nth(7);
     const grid1 = section.locator(">div").first();
     const grid2 = section.locator(">div").nth(1);
+    await expect(section).toBeVisible();
     await expect(
       grid1.getByRole("img", { name: "Intro Illustration" }),
     ).toBeVisible();
@@ -46,5 +47,46 @@ test.describe("FrontendMentor Challenge - Manage landing Page", () => {
     await expect(
       grid2.getByRole("button", { name: "Get Started" }),
     ).toBeVisible();
+  });
+
+  /** Test if the page has a 'Selling Point' section */
+  test("has a 'Selling Point' section", async ({ page }) => {
+    const sellingPoints = [
+      {
+        idx: "01",
+        title: "Track company-wide progress",
+        desc: "See how your day-to-day tasks fit into the wider vision. Go from tracking progress at the milestone level all the way done to the smallest of details. Never lose sight of the bigger picture again.",
+      },
+      {
+        idx: "02",
+        title: "Advanced built-in reports",
+        desc: "Set internal delivery estimates and track progress toward company goals. Our customisable dashboard helps you build out the reports you need to keep key stakeholders informed.",
+      },
+      {
+        idx: "03",
+        title: "Everything you need in one place",
+        desc: "Stop jumping from one service to another to communicate, store files, track tasks and share documents. Manage offers an all-in-one team productivity solution.",
+      },
+    ];
+    const section = page.locator("div").nth(10);
+    const grid1 = section.locator(">div").first();
+    const grid2 = section.locator(">div").nth(1);
+    await section.scrollIntoViewIfNeeded();
+    await expect(section).toBeVisible();
+    await expect(
+      grid1.getByRole("heading", { name: "What’s different about Manage?" }),
+    ).toBeVisible();
+    await expect(
+      grid1.getByText(
+        "Manage provides all the functionality your team needs, without the complexity. Our software is tailor-made for modern digital product teams.",
+      ),
+    ).toBeVisible();
+    for (const point of sellingPoints) {
+      await expect(grid2.getByText(point.idx)).toBeVisible();
+      await expect(
+        grid2.getByRole("heading", { name: point.title }),
+      ).toBeVisible();
+      await expect(grid2.getByText(point.desc)).toBeVisible();
+    }
   });
 });
