bd16244a948a04ba2c1c4a4112026f3e897bf81f
muflihanto <muflihanto@gmail.com>
2024-04-11 19:46:43 +0700

    url-shortening-api: add more test

--- tests/url-shortening-api.spec.ts
+++ tests/url-shortening-api.spec.ts
@@ -25,6 +25,25 @@ test.describe("FrontendMentor Challenge - Shortly URL shortening API Challenge P
     await expect(header.getByRole("link", { name: "Sign Up" })).toBeVisible();
   });
 
+  /** Test if the page has an Intro section */
+  test("has an Intro section", async ({ page }) => {
+    const section = page.locator("div").nth(10);
+    await expect(
+      section.getByRole("heading", { name: "More than just shorter links" }),
+    ).toBeVisible();
+    await expect(
+      section.getByText(
+        "Build your brand’s recognition and get detailed insights on how your links are performing.",
+      ),
+    ).toBeVisible();
+    await expect(
+      section.getByRole("button", { name: "Get Started" }),
+    ).toBeVisible();
+    await expect(
+      section.getByRole("img", { name: "Illustration Working" }),
+    ).toBeVisible();
+  });
+
   /** Test if the page has a heading */
   test("has a heading", async ({ page }) => {
     await expect(
