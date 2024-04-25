35ab62f503e81348781146f3f536505473c05cc7
muflihanto <muflihanto@gmail.com>
2024-04-25 16:11:17 +0700

    multi-step-form: add more test

--- tests/multi-step-form.spec.ts
+++ tests/multi-step-form.spec.ts
@@ -19,6 +19,7 @@ test.describe("FrontendMentor Challenge - Multi-step form Page", () => {
 
     let page: Page;
     let card: Locator;
+    let form: Locator;
 
     test.beforeAll(async ({ browser }) => {
       page = await browser.newPage();
@@ -29,6 +30,43 @@ test.describe("FrontendMentor Challenge - Multi-step form Page", () => {
       card = page.getByRole("main");
       await expect(card).toBeVisible();
       await expect(card).toBeInViewport();
+    });
+
+    test("has initial elements", async () => {
+      const stepNav = card.locator("div").first();
+      const navs = await stepNav.locator(">div").all();
+      expect(navs).toHaveLength(4);
+      const steps = ["Your Info", "Select Plan", "Add-ons", "Summary"];
+      for (const [idx, nav] of Object.entries(navs)) {
+        const index = Number(idx) + 1;
+        await expect(nav.getByRole("link", { name: `${index}` })).toBeVisible();
+        await expect(nav.getByText(`Step ${index}`)).toBeVisible();
+        await expect(nav.getByText(steps[index - 1])).toBeVisible();
+      }
+      form = card.locator("form");
+      await expect(
+        form.getByRole("heading", { name: "Personal info" }),
+      ).toBeVisible();
+      await expect(
+        form.getByText(
+          "Please provide your name, email address, and phone number.",
+        ),
+      ).toBeVisible();
+      await expect(form.getByText("Name", { exact: true })).toBeVisible();
+      await expect(form.getByPlaceholder("e.g. Stephen King")).toBeVisible();
+      await expect(
+        form.getByText("Email Address", { exact: true }),
+      ).toBeVisible();
+      await expect(
+        form.getByPlaceholder("e.g. stephenking@lorem.com"),
+      ).toBeVisible();
+      await expect(
+        form.getByText("Phone Number", { exact: true }),
+      ).toBeVisible();
+      await expect(form.getByPlaceholder("e.g. +1 234 567 890")).toBeVisible();
+      await expect(
+        form.getByRole("button", { name: "Next Step" }),
+      ).toBeVisible();
     });
   });
 });
