9b467aabc1dbe31de357fdc3caf8ffec9c230332
muflihanto <muflihanto@gmail.com>
2024-04-25 16:00:11 +0700

    multi-step-form: add more test

--- tests/multi-step-form.spec.ts
+++ tests/multi-step-form.spec.ts
@@ -1,9 +1,11 @@
-import { test, expect } from "@playwright/test";
+import { test, expect, type Page, type Locator } from "@playwright/test";
+
+const pageUrl = "/multi-step-form";
 
 test.describe("FrontendMentor Challenge - Multi-step form Page", () => {
   /** Go to Multi-step form page before each test */
   test.beforeEach("Open", async ({ page }) => {
-    await page.goto("/multi-step-form");
+    await page.goto(pageUrl);
   });
 
   /** Test if the page has a correct title */
@@ -11,13 +13,22 @@ test.describe("FrontendMentor Challenge - Multi-step form Page", () => {
     await expect(page).toHaveTitle("Frontend Mentor | Multi-step form");
   });
 
-  /** Test if the page has a heading */
-  test("has a heading", async ({ page }) => {
-    await expect(
-      page.getByRole("heading", {
-        level: 1,
-        name: "Personal info",
-      }),
-    ).toBeVisible();
+  /** Test if the page has a main card */
+  test.describe("has a main card", () => {
+    test.describe.configure({ mode: "serial" });
+
+    let page: Page;
+    let card: Locator;
+
+    test.beforeAll(async ({ browser }) => {
+      page = await browser.newPage();
+      await page.goto(pageUrl);
+    });
+
+    test("card is visble", async () => {
+      card = page.getByRole("main");
+      await expect(card).toBeVisible();
+      await expect(card).toBeInViewport();
+    });
   });
 });
