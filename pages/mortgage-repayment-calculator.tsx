57fd7ca28e44abc40b72e2a0dfee08a655a647a0
muflihanto <muflihanto@gmail.com>
2024-07-15 21:13:33 +0700

    mortgage-repayment-calculator: add initial form styling

--- pages/mortgage-repayment-calculator.tsx
+++ pages/mortgage-repayment-calculator.tsx
@@ -5,6 +5,8 @@ import {
   // PlusJakartaSansItalic,
 } from "../utils/fonts/plusJakartaSans";
 import { cn } from "../utils/cn";
+import { CalculatorIcon } from "@heroicons/react/20/solid";
+
 // import dynamic from "next/dynamic";
 // const Slider = dynamic(() => import("../components/SliderTs"), { ssr: false });
 
@@ -16,7 +18,7 @@ export default function MortgageRepaymentCalculator() {
       </Head>
       <div
         className={cn(
-          "App min-h-[100svh] relative font-medium",
+          "App relative min-h-[100svh] font-medium",
           PlusJakartaSans.variable,
           "font-plus-jakarta",
           "overflow-x-hidden",
@@ -33,27 +35,110 @@ export default function MortgageRepaymentCalculator() {
   );
 }
 
+function MortgageForm() {
+  return (
+    <form className="w-full px-6 py-[31px] text-mortgage-neutral-slate-700">
+      <h1 className="text-2xl font-bold text-mortgage-neutral-slate-900">
+        Mortgage Calculator
+      </h1>
+      <button className="mt-2 underline" type="reset">
+        Clear All
+      </button>
+      <div className="mt-[23px] flex w-full flex-col gap-[23px]">
+        <label htmlFor="" className="relative flex flex-col gap-[11px]">
+          <span>Mortgage Amount</span>
+          <input
+            className="peer h-[50px] w-full rounded border border-mortgage-neutral-slate-500 pl-16 text-sm focus-visible:border-mortgage-primary-lime focus-visible:outline focus-visible:outline-transparent text-mortgage-neutral-slate-900"
+            type="text"
+          />
+          <span className="absolute bottom-0 left-0 flex h-[50px] w-11 flex-col items-center justify-center rounded-l border border-r-0 border-mortgage-neutral-slate-500 bg-mortgage-neutral-slate-100 text-lg font-bold peer-focus-visible:border-mortgage-primary-lime peer-focus-visible:bg-mortgage-primary-lime peer-focus-visible:text-mortgage-neutral-slate-900">
+            &pound;
+          </span>
+        </label>
+        <label htmlFor="" className="relative flex flex-col gap-[11px]">
+          <span>Mortgage Term</span>
+          <input
+            className="peer h-[50px] w-full rounded border border-mortgage-neutral-slate-500 p-4 pr-16 text-sm focus-visible:border-mortgage-primary-lime focus-visible:outline focus-visible:outline-transparent text-mortgage-neutral-slate-900"
+            type="text"
+          />
+          <span className="absolute bottom-0 right-0 flex h-[50px] w-20 flex-col items-center justify-center rounded-r border border-l-0 border-mortgage-neutral-slate-500 bg-mortgage-neutral-slate-100 text-lg font-bold peer-focus-visible:border-mortgage-primary-lime peer-focus-visible:bg-mortgage-primary-lime peer-focus-visible:text-mortgage-neutral-slate-900">
+            years
+          </span>
+        </label>
+        <label htmlFor="" className="relative flex flex-col gap-[11px]">
+          <span>Interest Rate</span>
+          <input
+            className="peer h-[50px] w-full rounded border border-mortgage-neutral-slate-500 p-4 pr-16 text-sm focus-visible:border-mortgage-primary-lime focus-visible:outline focus-visible:outline-transparent text-mortgage-neutral-slate-900"
+            type="text"
+          />
+          <span className="absolute bottom-0 right-0 flex h-[50px] w-[50px] flex-col items-center justify-center rounded-r border border-l-0 border-mortgage-neutral-slate-500 bg-mortgage-neutral-slate-100 text-lg font-bold peer-focus-visible:border-mortgage-primary-lime peer-focus-visible:bg-mortgage-primary-lime peer-focus-visible:text-mortgage-neutral-slate-900">
+            %
+          </span>
+        </label>
+        <fieldset>
+          <legend>Mortgage Type</legend>
+          <div className="mt-[11px] flex flex-col gap-[10px]">
+            <div className="relative w-full md:flex-1">
+              <input
+                type="radio"
+                value="repayment"
+                name="mortgageType"
+                className={cn(
+                  "peer absolute left-5 top-1/2 -translate-y-1/2 scale-[140%] text-9xl text-mortgage-primary-lime/10 accent-mortgage-primary-lime",
+                  "focus-visible:outline focus-visible:outline-transparent",
+                )}
+                id="repayment"
+              />
+              <label
+                htmlFor="repayment"
+                className={cn(
+                  "group/label flex h-[50px] w-full cursor-pointer items-center gap-4 rounded border border-mortgage-neutral-slate-500 pl-[55px] text-lg/none font-bold text-mortgage-neutral-slate-900 hover:border-mortgage-primary-lime",
+                  "peer-checked:border-mortgage-primary-lime peer-checked:bg-mortgage-primary-lime/10 peer-focus-visible:outline peer-focus-visible:outline-2 peer-focus-visible:outline-offset-1 peer-focus-visible:outline-mortgage-primary-lime",
+                )}
+              >
+                Repayment
+              </label>
+            </div>
+            <div className="relative w-full md:flex-1">
+              <input
+                type="radio"
+                value="interest-only"
+                id="interest-only"
+                name="mortgageType"
+                className={cn(
+                  "peer absolute left-5 top-1/2 -translate-y-1/2 scale-[140%] text-9xl text-mortgage-primary-lime/10 accent-mortgage-primary-lime",
+                  "focus-visible:outline focus-visible:outline-transparent",
+                )}
+              />
+              <label
+                htmlFor="interest-only"
+                className={cn(
+                  "group/label flex h-[50px] w-full cursor-pointer items-center gap-4 rounded border border-mortgage-neutral-slate-500 pl-[55px] text-lg/none font-bold text-mortgage-neutral-slate-900 hover:border-mortgage-primary-lime",
+                  "peer-checked:border-mortgage-primary-lime peer-checked:bg-mortgage-primary-lime/10 peer-focus-visible:outline peer-focus-visible:outline-2 peer-focus-visible:outline-offset-1 peer-focus-visible:outline-mortgage-primary-lime",
+                )}
+              >
+                Interest Only
+              </label>
+            </div>
+          </div>
+        </fieldset>
+        <button
+          type="submit"
+          className="flex h-[56px] w-full items-center justify-center gap-[13px] rounded-full bg-mortgage-primary-lime text-lg/none font-bold text-mortgage-neutral-slate-900"
+        >
+          <CalculatorIcon className="h-[22px] text-mortgage-neutral-slate-900" />
+          <span>Calculate Repayments</span>
+        </button>
+      </div>
+    </form>
+  );
+}
+
 function Main() {
   return (
-    <main>
-      <h1 className="font-bold">Mortgage Calculator</h1>
+    <main className="flex w-full flex-col items-center justify-center">
+      <MortgageForm />
       {/* {`
-            Clear All
-
-            Mortgage Amount
-
-            Mortgage Term
-
-            Interest Rate
-
-            Mortgage Type
-            Repayment
-            Interest Only
-
-            Calculate Repayments
-
-            <!-- Empty results start -->
-
             Results shown here
 
             Complete the form and click “calculate repayments” to see what 
