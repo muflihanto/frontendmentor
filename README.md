a93fe2b675d6b4af620010657b0b880520f3e2c6
muflihanto <muflihanto@gmail.com>
2024-07-18 19:23:26 +0700

    improve index page and readme.md

--- README.md
+++ README.md
@@ -1,34 +1,18 @@
-This is a [Next.js](https://nextjs.org/) project bootstrapped with [`create-next-app`](https://github.com/vercel/next.js/tree/canary/packages/create-next-app).
+# Frontend Mentor - Solutions
 
-## Getting Started
+This is my solutions to the [Frontend Mentor free and free plus challenges](https://www.frontendmentor.io/challenges?type=free%2Cfree-plus). Frontend Mentor challenges help me improve my coding skills by building realistic projects.
 
-First, run the development server:
+## Table of contents
 
-```bash
-npm run dev
-# or
-yarn dev
-```
+- [Frontend Mentor - Solutions](#frontend-mentor---solutions)
+  - [Table of contents](#table-of-contents)
+  - [Overview](#overview)
+    - [Links](#links)
 
-Open [http://localhost:3000](http://localhost:3000) with your browser to see the result.
+## Overview
 
-You can start editing the page by modifying `pages/index.js`. The page auto-updates as you edit the file.
+### Links
 
-[API routes](https://nextjs.org/docs/api-routes/introduction) can be accessed on [http://localhost:3000/api/hello](http://localhost:3000/api/hello). This endpoint can be edited in `pages/api/hello.js`.
-
-The `pages/api` directory is mapped to `/api/*`. Files in this directory are treated as [API routes](https://nextjs.org/docs/api-routes/introduction) instead of React pages.
-
-## Learn More
-
-To learn more about Next.js, take a look at the following resources:
-
-- [Next.js Documentation](https://nextjs.org/docs) - learn about Next.js features and API.
-- [Learn Next.js](https://nextjs.org/learn) - an interactive Next.js tutorial.
-
-You can check out [the Next.js GitHub repository](https://github.com/vercel/next.js/) - your feedback and contributions are welcome!
-
-## Deploy on Vercel
-
-The easiest way to deploy your Next.js app is to use the [Vercel Platform](https://vercel.com/new?utm_medium=default-template&filter=next.js&utm_source=create-next-app&utm_campaign=create-next-app-readme) from the creators of Next.js.
-
-Check out our [Next.js deployment documentation](https://nextjs.org/docs/deployment) for more details.
+- Documentations: [Github](https://github.com/muflihanto/frontendmentor/tree/main/docs)
+- Solutions URL: [Github](https://github.com/muflihanto/frontendmentor)
+- Live Site URL: [Vercel](https://frontendmentor-mfl.vercel.app/)
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,6 +4,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useMemo, useState, type ChangeEvent } from "react";
 import { useIsClient } from "usehooks-ts";
+import pages from "../docs/data.json";
 
 // import getPages from "../utils/getPages";
 // import type { InferGetServerSidePropsType } from "next";
@@ -14,189 +15,6 @@ import { useIsClient } from "usehooks-ts";
 //     props: { ...getPages() },
 //   };
 // }
-
-export const pages = [
-  {
-    title: "3 Column Preview Card Component",
-    path: "3-column-preview-card-component",
-  },
-  { title: "Advice Generator App", path: "advice-generator-app" },
-  { title: "Age Calculator App", path: "age-calculator-app" },
-  {
-    title: "Article Preview Component",
-    path: "article-preview-component",
-  },
-  {
-    title: "Base Apparel Coming Soon",
-    path: "base-apparel-coming-soon",
-  },
-  { title: "Blog preview card", path: "blog-preview-card" },
-  { title: "Blogr Landing Page", path: "blogr-landing-page" },
-  { title: "Bookmark Landing Page", path: "bookmark-landing-page" },
-  { title: "Calculator App", path: "calculator-app" },
-  {
-    title: "Chat App Css Illustration",
-    path: "chat-app-css-illustration",
-  },
-  { title: "Clipboard Landing Page", path: "clipboard-landing-page" },
-  {
-    title: "Coding Bootcamp Testimonials Slider",
-    path: "coding-bootcamp-testimonials-slider",
-  },
-  {
-    title: "Contact Form",
-    path: "contact-form",
-  },
-  {
-    title: "Crowdfunding Product Page",
-    path: "crowdfunding-product-page",
-  },
-  { title: "Easybank Landing Page", path: "easybank-landing-page" },
-  { title: "Ecommerce Product Page", path: "ecommerce-product-page" },
-  {
-    title: "Expenses Chart Component",
-    path: "expenses-chart-component",
-  },
-  { title: "FAQ Accordion", path: "faq-accordion" },
-  { title: "Faq Accordion Card", path: "faq-accordion-card" },
-  {
-    title: "Four Card Feature Section",
-    path: "four-card-feature-section",
-  },
-  {
-    title: "Fylo Dark Theme Landing Page",
-    path: "fylo-dark-theme-landing-page",
-  },
-  {
-    title: "Fylo Data Storage Component",
-    path: "fylo-data-storage-component",
-  },
-  {
-    title: "Fylo Landing Page With Two Column Layout",
-    path: "fylo-landing-page-with-two-column-layout",
-  },
-  {
-    title: "Huddle Landing Page With Alternating Feature Blocks",
-    path: "huddle-landing-page-with-alternating-feature-blocks",
-  },
-  {
-    title: "Huddle Landing Page With Curved Sections",
-    path: "huddle-landing-page-with-curved-sections",
-  },
-  {
-    title: "Huddle Landing Page With Single Introductory Section",
-    path: "huddle-landing-page-with-single-introductory-section",
-  },
-  { title: "Insure Landing Page", path: "insure-landing-page" },
-  {
-    title: "Interactive Card Details Form",
-    path: "interactive-card-details-form",
-  },
-  {
-    title: "Interactive Comments Section",
-    path: "interactive-comments-section",
-  },
-  {
-    title: "Interactive Pricing Component",
-    path: "interactive-pricing-component",
-  },
-  {
-    title: "Interactive Rating Component",
-    path: "interactive-rating-component",
-  },
-  {
-    title: "Intro Component With Signup Form",
-    path: "intro-component-with-signup-form",
-  },
-  {
-    title: "Intro Section With Dropdown Navigation",
-    path: "intro-section-with-dropdown-navigation",
-  },
-  { title: "Ip Address Tracker", path: "ip-address-tracker" },
-  { title: "Launch Countdown Timer", path: "launch-countdown-timer" },
-  {
-    title: "Loopstudios Landing Page",
-    path: "loopstudios-landing-page",
-  },
-  { title: "Manage Landing Page", path: "manage-landing-page" },
-  {
-    title: "Mortgage Repayment Calculator",
-    path: "mortgage-repayment-calculator",
-  },
-  { title: "Multi Step Form", path: "multi-step-form" },
-  { title: "News Homepage", path: "news-homepage" },
-  {
-    title: "Newsletter Sign Up With Success Message",
-    path: "newsletter-sign-up-with-success-message",
-  },
-  {
-    title: "Nft Preview Card Component",
-    path: "nft-preview-card-component",
-  },
-  { title: "Notifications Page", path: "notifications-page" },
-  { title: "Order Summary Component", path: "order-summary-component" },
-  { title: "Ping Coming Soon Page", path: "ping-coming-soon-page" },
-  {
-    title: "Pricing Component With Toggle",
-    path: "pricing-component-with-toggle",
-  },
-  {
-    title: "Product Preview Card Component",
-    path: "product-preview-card-component",
-  },
-  { title: "Profile Card Component", path: "profile-card-component" },
-  {
-    title: "Project Tracking Intro Component",
-    path: "project-tracking-intro-component",
-  },
-  { title: "Qr Code Component", path: "qr-code-component" },
-  { title: "Recipe Page", path: "recipe-page" },
-  {
-    title: "Rest Countries Api With Color Theme Switcher",
-    path: "rest-countries-api-with-color-theme-switcher",
-  },
-  {
-    title: "Results Summary Component",
-    path: "results-summary-component",
-  },
-  {
-    title: "Rock Paper Scissors Bonus",
-    path: "rock-paper-scissors-bonus",
-  },
-  { title: "Rock Paper Scissors", path: "rock-paper-scissors" },
-  { title: "Room Homepage", path: "room-homepage" },
-  {
-    title: "Single Price Grid Component",
-    path: "single-price-grid-component",
-  },
-  {
-    title: "Social Links Profile",
-    path: "social-links-profile",
-  },
-  {
-    title: "Social Media Dashboard With Theme Switcher",
-    path: "social-media-dashboard-with-theme-switcher",
-  },
-  { title: "Social Proof Section", path: "social-proof-section" },
-  { title: "Space Tourism Website", path: "space-tourism-website" },
-  { title: "Static Job Listings", path: "static-job-listings" },
-  {
-    title: "Stats Preview Card Component",
-    path: "stats-preview-card-component",
-  },
-  {
-    title: "Sunnyside Agency Landing Page",
-    path: "sunnyside-agency-landing-page",
-  },
-  {
-    title: "Testimonials Grid Section",
-    path: "testimonials-grid-section",
-  },
-  { title: "Time Tracking Dashboard", path: "time-tracking-dashboard" },
-  { title: "Tip Calculator App", path: "tip-calculator-app" },
-  { title: "Todo App", path: "todo-app" },
-  { title: "Url Shortening Api", path: "url-shortening-api" },
-];
 
 // export default function Home({
 //   pages,
@@ -209,7 +27,7 @@ export function Home() {
   const filteredPages = useMemo(() => {
     if (!input) return pages;
     return pages.filter((page) => {
-      return page.title.toLowerCase().includes(input.toLowerCase());
+      return page.name.toLowerCase().includes(input.toLowerCase());
     });
   }, [input]);
   // }, [input, pages]);
@@ -245,16 +63,34 @@ export function Home() {
           className="mt-4 w-full rounded-md border px-3 py-2 focus-visible:outline focus-visible:outline-sky-600 dark:border-sky-800 dark:bg-sky-900 dark:text-sky-100 dark:focus-visible:outline dark:focus-visible:outline-sky-700 lg:max-w-screen-sm"
           placeholder="Type here..."
         />
-        <ul className="mx-auto mt-8 grid max-w-screen-md grid-cols-1 items-center gap-4 self-start sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4">
+        <ul className="mx-auto mt-8 grid max-w-screen-lg grid-cols-1 items-center gap-4 self-start sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-3">
           {filteredPages.map((page) => {
             return (
-              <li key={page.title}>
-                <Link
-                  href={page.path}
-                  className="mb-2 flex h-[180px] min-w-[180px] items-center justify-center rounded-xl bg-sky-600 p-5 text-center text-[18px] font-semibold text-white shadow-md hover:bg-sky-600/80 hover:underline hover:underline-offset-2 focus-visible:outline focus-visible:outline-offset-2 focus-visible:outline-sky-500 dark:bg-sky-900"
-                >
-                  {page.title}
-                </Link>
+              <li key={page.name}>
+                <div className="mb-2 flex h-[480px] min-w-[180px] flex-col gap-2 overflow-hidden rounded-xl bg-sky-600 text-center text-white shadow-lg shadow-sky-950/20 focus-within:outline focus-within:outline-offset-2 focus-within:outline-sky-500 dark:bg-sky-900">
+                  <Link className="h-[240px] group" href={page.path}>
+                    <div className="h-full w-full overflow-hidden relative">
+                      <div className="w-full h-14 bg-transparent bg-gradient-to-b from-slate-800/10 group-hover:from-slate-800/20 to-transparent transition-all duration-1000 top-0 left-0 absolute z-10" />
+                      <Image
+                        src={page["preview-src"]}
+                        alt={page.name}
+                        className="group-hover:scale-110 transition-all duration-300"
+                        fill
+                      />
+                    </div>
+                  </Link>
+                  <div className="flex flex-col gap-2 p-5">
+                    <Link
+                      href={page.path}
+                      className="text-left text-xl font-semibold hover:underline hover:underline-offset-2"
+                    >
+                      {page.name}
+                    </Link>
+                    <p className="block self-start text-left">
+                      {page.description}
+                    </p>
+                  </div>
+                </div>
               </li>
             );
           })}
