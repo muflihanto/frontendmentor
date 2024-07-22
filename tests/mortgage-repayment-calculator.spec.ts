9829c5f8eb8c3b050fc57fcff8c52ab56cb7b047
muflihanto <muflihanto@gmail.com>
2024-07-22 20:12:06 +0700

    mortgage-repayment-calculator: add more test

--- tests/mortgage-repayment-calculator.spec.ts
+++ tests/mortgage-repayment-calculator.spec.ts
@@ -13,6 +13,33 @@ test.describe("FrontendMentor Challenge - Mortgage Repayment Calculator Page", (
     );
   });
 
+  /** Test if the page has all form fields */
+  test("has all form fields", async ({ page }) => {
+    const form = page.locator("form");
+    await expect(form).toBeVisible();
+    await expect(
+      form.getByRole("heading", { name: "Mortgage Calculator" }),
+    ).toBeVisible();
+    await expect(form.getByRole("button", { name: "Clear All" })).toBeVisible();
+    await expect(
+      form.getByText("Mortgage Amount", { exact: true }),
+    ).toBeVisible();
+    await expect(form.getByLabel("Mortgage Amount£")).toBeVisible();
+    await expect(
+      form.getByText("Mortgage Term", { exact: true }),
+    ).toBeVisible();
+    await expect(form.getByLabel("Mortgage Termyears")).toBeVisible();
+    await expect(
+      form.getByText("Interest Rate", { exact: true }),
+    ).toBeVisible();
+    await expect(form.getByLabel("Interest Rate%")).toBeVisible();
+    await expect(form.getByText("Repayment", { exact: true })).toBeVisible();
+    await expect(form.getByText("Interest Only")).toBeVisible();
+    await expect(
+      form.getByRole("button", { name: "Calculate Repayments" }),
+    ).toBeVisible();
+  });
+
   /** Test if the page has a footer */
   test("has a footer", async ({ page }) => {
     await expect(
