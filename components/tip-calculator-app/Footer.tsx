fe0ead29994e760348338df13faae7f76c834448
muflihanto <muflihanto@gmail.com>
2024-08-29 00:38:17 +0700

    tip-calculator-app: fix styling issues affected by next-themes

--- components/tip-calculator-app/Footer.tsx
+++ components/tip-calculator-app/Footer.tsx
@@ -1,9 +1,6 @@
 export default function Footer() {
   return (
-    <div
-      className="absolute bottom-2 left-0 mx-auto w-full text-center text-[10px] lg:bottom-4 [&_a]:text-tip-neutral-600 [&_a]:underline [&_a]:decoration-wavy
-    "
-    >
+    <div className="absolute bottom-2 left-0 mx-auto w-full text-center text-[10px] text-tip-neutral-500 lg:bottom-4 [&_a]:text-tip-neutral-600 [&_a]:underline [&_a]:decoration-wavy">
       Challenge by{" "}
       <a
         href="https://www.frontendmentor.io?ref=challenge"
