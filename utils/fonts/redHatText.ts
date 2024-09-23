7324318caabdfd2d9a0c2eaabb202b15f619678b
muflihanto <muflihanto@gmail.com>
2024-09-23 13:59:04 +0700

    add tailwind config for product-list-with-cart page

--- utils/fonts/redHatText.ts
+++ utils/fonts/redHatText.ts
@@ -2,7 +2,6 @@ import localFont from "next/font/local";
 
 export const redHatText = localFont({
   src: "../../public/launch-countdown-timer/fonts/RedHatText-VariableFont_wght.ttf",
-  weight: "700",
   style: "normal",
   variable: "--font-red-hat-text",
 });
