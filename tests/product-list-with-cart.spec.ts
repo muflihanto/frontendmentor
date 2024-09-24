a25adc112c1eef14ac3a9927a4ee612d557a12b4
muflihanto <muflihanto@gmail.com>
2024-09-24 16:45:39 +0700

    write initial e2e tests for product-list-with-cart page

---
+++ tests/product-list-with-cart.spec.ts
@@ -0,0 +1,27 @@
+import { test, expect } from "@playwright/test";
+
+test.describe("FrontendMentor Challenge - Product list with cart page", () => {
+  /** Go to Product list with cart page before each test */
+  test.beforeEach("Open", async ({ page }) => {
+    await page.goto("/product-list-with-cart");
+  });
+
+  /** Test if the page has a correct title */
+  test("has title", async ({ page }) => {
+    await expect(page).toHaveTitle("Frontend Mentor | Product list with cart");
+  });
+
+  /** Test if the page has a header */
+  test("has a header", async ({ page }) => {
+    await expect(
+      page.getByRole("heading", { name: "Desserts", level: 1 }),
+    ).toBeVisible();
+  });
+
+  /** Test if the page has a footer */
+  test("has a footer", async ({ page }) => {
+    await expect(
+      page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
+    ).toBeVisible();
+  });
+});
