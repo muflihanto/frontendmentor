e512faf1e8ff3094de0a5b9a4e44f1870a0e949d
muflihanto <muflihanto@gmail.com>
2024-09-26 20:29:38 +0700

    product-list-with-cart: add initial quantity counter for product ListItem

--- pages/product-list-with-cart.tsx
+++ pages/product-list-with-cart.tsx
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Image, { type ImageLoaderProps } from "next/image";
 import _products from "../public/product-list-with-cart/data.json";
 import { redHatText } from "../utils/fonts/redHatText";
+import { useState } from "react";
+import { cn } from "../utils/cn";
 
 // import dynamic from "next/dynamic";
 // const Slider = dynamic(() => import("../components/SliderTs"), { ssr: false });
@@ -28,7 +30,7 @@ export default function ProductListWithCart() {
         <Footer />
         {/* <Slider
           basePath="/product-list-with-cart/design"
-          absolutePath="/product-list-with-cart/design/mobile-design-empty.jpg"
+          absolutePath="/product-list-with-cart/design/mobile-design-selected.jpg"
         /> */}
       </div>
     </>
@@ -69,6 +71,8 @@ function productImageloader({
 }
 
 function ListItem(product: Product) {
+  const [quantity, setQuantity] = useState(0);
+
   return (
     <div>
       <div className="relative aspect-[327/212] w-full">
@@ -76,18 +80,62 @@ function ListItem(product: Product) {
           alt={product.name}
           src={product.image.thumbnail}
           loader={productImageloader}
-          className="rounded-lg object-contain"
+          className={cn(
+            "rounded-lg object-contain",
+            quantity > 0 && "rinng ring-2 ring-product-list-red",
+          )}
           fill
         />
-        <button
-          className="absolute bottom-0 left-1/2 flex h-[44px] w-[160px] -translate-x-1/2 translate-y-1/2 items-center justify-center gap-2 rounded-full border border-product-list-rose-300 bg-white px-4 text-sm font-semibold"
-          type="button"
-        >
-          <svg className="aspect-[21/20] h-5" aria-hidden="true">
-            <use href="/product-list-with-cart/assets/images/icon-add-to-cart.svg#add-to-cart" />
-          </svg>
-          Add to Cart
-        </button>
+        {quantity === 0 ? (
+          <button
+            className="absolute bottom-0 left-1/2 flex h-[44px] w-[160px] -translate-x-1/2 translate-y-1/2 items-center justify-center gap-2 rounded-full border border-product-list-rose-300 bg-white px-4 text-sm font-semibold"
+            type="button"
+            onClick={() => {
+              setQuantity(1);
+            }}
+          >
+            <svg className="aspect-[21/20] h-5" aria-hidden="true">
+              <use href="/product-list-with-cart/assets/images/icon-add-to-cart.svg#add-to-cart" />
+            </svg>
+            Add to Cart
+          </button>
+        ) : (
+          <div className="absolute bottom-0 left-1/2 flex h-[44px] w-[160px] -translate-x-1/2 translate-y-1/2 items-center justify-between rounded-full bg-product-list-red px-[14px] text-sm font-semibold">
+            <button
+              className="flex aspect-square w-4 items-center justify-center rounded-full text-white ring-1 ring-product-list-rose-50 hover:bg-white hover:text-product-list-red"
+              type="button"
+              onClick={() => {
+                setQuantity((p) => p - 1);
+              }}
+            >
+              <svg
+                className="aspect-[5/1] h-[2px]"
+                role="graphics-symbol"
+                aria-label="Decrement Quantity"
+                aria-hidden="true"
+              >
+                <use href="/product-list-with-cart/assets/images/icon-decrement-quantity.svg#decrement-quantity" />
+              </svg>
+            </button>
+            <span className="text-product-list-rose-50">{quantity}</span>
+            <button
+              className="flex aspect-square w-4 items-center justify-center rounded-full text-white ring-1 ring-product-list-rose-50 hover:bg-white hover:text-product-list-red"
+              type="button"
+              onClick={() => {
+                setQuantity((p) => p + 1);
+              }}
+            >
+              <svg
+                className="aspect-square h-[10px]"
+                role="graphics-symbol"
+                aria-label="Increment Quantity"
+                aria-hidden="true"
+              >
+                <use href="/product-list-with-cart/assets/images/icon-increment-quantity.svg#increment-quantity" />
+              </svg>
+            </button>
+          </div>
+        )}
       </div>
       <p className="mt-[37px] text-sm text-product-list-rose-500">
         {product.category}
