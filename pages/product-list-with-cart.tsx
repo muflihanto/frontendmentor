27a8851edfd1c671965ea25eb2c6f41f49a90878
muflihanto <muflihanto@gmail.com>
2024-09-27 18:11:48 +0700

    product-list-with-cart: add CartContext

--- pages/product-list-with-cart.tsx
+++ pages/product-list-with-cart.tsx
@@ -2,7 +2,13 @@ import Head from "next/head";
 import Image, { type ImageLoaderProps } from "next/image";
 import _products from "../public/product-list-with-cart/data.json";
 import { redHatText } from "../utils/fonts/redHatText";
-import { useState } from "react";
+import {
+  type Dispatch,
+  type SetStateAction,
+  createContext,
+  useState,
+  useContext,
+} from "react";
 import { cn } from "../utils/cn";
 
 // import dynamic from "next/dynamic";
@@ -16,6 +22,20 @@ const products = _products as Product[];
 // TODO: Reset their selections when they click "Start New Order"
 // TODO: View the optimal layout for the interface depending on their device's screen size
 // TODO: See hover and focus states for all interactive elements on the page
+
+type Cart = Array<
+  Pick<Product, "name" | "price"> & {
+    quantity: number;
+    totalPrice: number;
+  }
+>;
+
+type CartAction = Dispatch<SetStateAction<Cart>>;
+
+const CartContext = createContext<{ cart: Cart; setCart: CartAction }>({
+  cart: [],
+  setCart: {} as CartAction,
+});
 
 export default function ProductListWithCart() {
   return (
@@ -72,6 +92,51 @@ function productImageloader({
 
 function ListItem(product: Product) {
   const [quantity, setQuantity] = useState(0);
+  const { setCart } = useContext(CartContext);
+
+  const addToCart = () => {
+    setQuantity(1);
+    setCart((cart) => {
+      return [
+        {
+          name: product.name,
+          price: product.price,
+          quantity: 1,
+          totalPrice: product.price,
+        },
+        ...cart,
+      ];
+    });
+  };
+
+  const updateCart = (quantity: number) => {
+    if (quantity === 0) {
+      setCart((cart) => {
+        const updatedCart = cart.filter((c) => c.name !== product.name);
+        return updatedCart;
+      });
+    } else {
+      setCart((cart) => {
+        const currentCart = cart.filter((c) => c.name === product.name)[0];
+        const updatedCart = Object.assign({}, currentCart, {
+          quantity,
+          totalPrice: quantity * currentCart.price,
+        });
+        const remainingCart = cart.filter((c) => c.name !== product.name);
+        return [updatedCart, ...remainingCart];
+      });
+    }
+  };
+
+  const decrementQuantity = () => {
+    updateCart(quantity - 1);
+    setQuantity((p) => p - 1);
+  };
+
+  const incrementQuantity = () => {
+    updateCart(quantity + 1);
+    setQuantity((p) => p + 1);
+  };
 
   return (
     <div>
@@ -90,9 +155,7 @@ function ListItem(product: Product) {
           <button
             className="absolute bottom-0 left-1/2 flex h-[44px] w-[160px] -translate-x-1/2 translate-y-1/2 items-center justify-center gap-2 rounded-full border border-product-list-rose-300 bg-white px-4 text-sm font-semibold"
             type="button"
-            onClick={() => {
-              setQuantity(1);
-            }}
+            onClick={addToCart}
           >
             <svg className="aspect-[21/20] h-5" aria-hidden="true">
               <use href="/product-list-with-cart/assets/images/icon-add-to-cart.svg#add-to-cart" />
@@ -104,9 +167,7 @@ function ListItem(product: Product) {
             <button
               className="flex aspect-square w-4 items-center justify-center rounded-full text-white ring-1 ring-product-list-rose-50 hover:bg-white hover:text-product-list-red"
               type="button"
-              onClick={() => {
-                setQuantity((p) => p - 1);
-              }}
+              onClick={decrementQuantity}
             >
               <svg
                 className="aspect-[5/1] h-[2px]"
@@ -121,9 +182,7 @@ function ListItem(product: Product) {
             <button
               className="flex aspect-square w-4 items-center justify-center rounded-full text-white ring-1 ring-product-list-rose-50 hover:bg-white hover:text-product-list-red"
               type="button"
-              onClick={() => {
-                setQuantity((p) => p + 1);
-              }}
+              onClick={incrementQuantity}
             >
               <svg
                 className="aspect-square h-[10px]"
@@ -149,35 +208,39 @@ function ListItem(product: Product) {
 }
 
 function Main() {
+  const [cart, setCart] = useState<Cart>([]);
+
   return (
-    <main className="p-6">
-      <h1 className="text-[40px] font-bold leading-[1.15]">Desserts</h1>
-      <ul className="mb-4 mt-[34px] flex flex-col gap-[23px]">
-        {products.map((el) => {
-          return (
-            <li key={el.name}>
-              <ListItem {...el} />
-            </li>
-          );
-        })}
-      </ul>
-      <div className="mt-[31px] flex min-h-[300px] w-full flex-col items-center bg-white px-6 py-[23px]">
-        <h2 className="self-start text-[24px] font-bold text-product-list-red">
-          Your Cart (0)
-        </h2>
-        <svg
-          role="graphics-symbol"
-          aria-label="Empty Cart"
-          className="mt-[38px] aspect-square w-32"
-          aria-hidden="true"
-        >
-          <use href="/product-list-with-cart/assets/images/illustration-empty-cart.svg#empty-cart" />
-        </svg>
-        <p className="mt-[14px] text-center text-sm font-semibold text-product-list-rose-500">
-          Your added items will appear here
-        </p>
-      </div>
-    </main>
+    <CartContext.Provider value={{ cart, setCart }}>
+      <main className="p-6">
+        <h1 className="text-[40px] font-bold leading-[1.15]">Desserts</h1>
+        <ul className="mb-4 mt-[34px] flex flex-col gap-[23px]">
+          {products.map((el) => {
+            return (
+              <li key={el.name}>
+                <ListItem {...el} />
+              </li>
+            );
+          })}
+        </ul>
+        <div className="mt-[31px] flex min-h-[300px] w-full flex-col items-center bg-white px-6 py-[23px]">
+          <h2 className="self-start text-[24px] font-bold text-product-list-red">
+            Your Cart (0)
+          </h2>
+          <svg
+            role="graphics-symbol"
+            aria-label="Empty Cart"
+            className="mt-[38px] aspect-square w-32"
+            aria-hidden="true"
+          >
+            <use href="/product-list-with-cart/assets/images/illustration-empty-cart.svg#empty-cart" />
+          </svg>
+          <p className="mt-[14px] text-center text-sm font-semibold text-product-list-rose-500">
+            Your added items will appear here
+          </p>
+        </div>
+      </main>
+    </CartContext.Provider>
   );
 }
 
