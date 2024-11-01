c79d85f1f33f7856c098cb3f9319cbfaa685109b
muflihanto <muflihanto@gmail.com>
2024-11-01 20:30:09 +0700

    add tailwind config for bento-grid page

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -975,6 +975,18 @@ export default {
             900: "hsl(14,65%,9%)",
           },
         },
+        bento: {
+          purple: {
+            100: "hsl(254,88%,90%)",
+            500: "hsl(256,67%,59%)",
+          },
+          yellow: {
+            100: "hsl(31,66%,93%)",
+            500: "hsl(39,100%,71%)",
+          },
+          white: "hsl(0,0%,100%)",
+          black: "hsl(0,0%,7%)",
+        },
       },
       fontFamily: {
         "plus-jakarta": [
