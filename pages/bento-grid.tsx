736cb05de619e3861db1e1c798800ff0f5411667
muflihanto <muflihanto@gmail.com>
2024-11-05 19:00:08 +0700

    bento-grid: add first grid content

--- pages/bento-grid.tsx
+++ pages/bento-grid.tsx
@@ -1,7 +1,9 @@
 import Head from "next/head";
-// import Image from "next/image";
-import dynamic from "next/dynamic";
-const Slider = dynamic(() => import("../components/SliderTs"), { ssr: false });
+import Image from "next/image";
+import { dmSans } from "../utils/fonts/dmSans";
+
+// import dynamic from "next/dynamic";
+// const Slider = dynamic(() => import("../components/SliderTs"), { ssr: false });
 
 export default function BentoGrid() {
   return (
@@ -9,10 +11,12 @@ export default function BentoGrid() {
       <Head>
         <title>Frontend Mentor | Bento grid</title>
       </Head>
-      <div className="App min-h-[100svh] relative">
+      <div
+        className={`App relative min-h-[100svh] overflow-x-hidden bg-white font-dm-sans ${dmSans.variable}`}
+      >
         <Main />
         <Footer />
-        <Slider basePath="/bento-grid/design" />
+        {/* <Slider basePath="/bento-grid/design" /> */}
       </div>
     </>
   );
@@ -20,11 +24,24 @@ export default function BentoGrid() {
 
 function Main() {
   return (
-    <>
+    <div className="bg-bento-purple-100/25 grid w-full grid-cols-1 px-4 py-[32px]">
+      <section className="bg-bento-purple-500 flex h-[290px] w-full flex-col items-center rounded-lg">
+        <h1 className="text-bento-white p-6 pt-[40px] text-center text-[45px] font-medium leading-[42px] -tracking-[1.25px]">
+          Social Media <span className="text-bento-yellow-500">10x</span>{" "}
+          <span className="italic">Faster</span> with AI
+        </h1>
+        <div className="relative aspect-[384/64] w-[192px]">
+          <Image
+            src="/bento-grid/assets/images/illustration-five-stars.webp"
+            alt="Illustration five stars"
+            fill
+          />
+        </div>
+        <p className="text-bento-yellow-100 mt-1 text-lg">
+          Over 4,000 5-star reviews
+        </p>
+      </section>
       {/* {`
-        Social Media 10x Faster with AI
-        Over 4,000 5-star reviews
-
         Manage multiple accounts and platforms.
 
         Maintain a consistent posting schedule.
@@ -40,13 +57,13 @@ function Main() {
 
         Write your content using AI.
       `} */}
-    </>
+    </div>
   );
 }
 
 function Footer() {
   return (
-    <footer className="absolute bottom-3 w-full text-center text-[11px] [&_a]:font-bold [&_a]:underline [&_a]:decoration-red-500 [&_a]:decoration-wavy">
+    <footer className="absolute bottom-3 w-full text-center text-[11px] text-black [&_a]:font-bold [&_a]:underline [&_a]:decoration-red-500 [&_a]:decoration-wavy">
       Challenge by{" "}
       <a
         href="https://www.frontendmentor.io?ref=challenge"
--- utils/fonts/dmSans.ts
+++ utils/fonts/dmSans.ts
@@ -2,6 +2,6 @@ import { DM_Sans } from "next/font/google";
 
 export const dmSans = DM_Sans({
   subsets: ["latin"],
-  weight: ["400", "700"],
+  weight: ["400", "500", "600", "700"],
   variable: "--font-dm-sans",
 });
