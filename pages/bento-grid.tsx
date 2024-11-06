471d54efea337aa17b87d152ad2f7bbf7c3d0797
muflihanto <muflihanto@gmail.com>
2024-11-06 19:02:30 +0700

    bento-grid: add grid content

--- pages/bento-grid.tsx
+++ pages/bento-grid.tsx
@@ -24,8 +24,8 @@ export default function BentoGrid() {
 
 function Main() {
   return (
-    <div className="bg-bento-purple-100/25 grid w-full grid-cols-1 gap-8 px-4 py-[32px]">
-      <section className="bg-bento-purple-500 flex h-[290px] w-full flex-col items-center rounded-lg p-6 pt-[40px]">
+    <div className="bg-bento-purple-100/25 grid w-full grid-cols-[repeat(1,minmax(0,400px))] place-content-center gap-8 px-4 py-[32px]">
+      <section className="bg-bento-purple-500 flex h-[290px] w-full flex-col items-center justify-center rounded-lg p-6 pt-[28px]">
         <h1 className="text-bento-white text-center text-[45px] font-medium leading-[42px] -tracking-[1.25px]">
           Social Media <span className="text-bento-yellow-500">10x</span>{" "}
           <span className="italic">Faster</span> with AI
@@ -41,7 +41,7 @@ function Main() {
           Over 4,000 5-star reviews
         </p>
       </section>
-      <section className="bg-bento-white flex h-[163px] w-full flex-col items-center rounded-lg py-4 pl-4 pr-[10px]">
+      <section className="bg-bento-white flex min-h-[163px] w-full flex-col items-center justify-center rounded-lg py-4 pl-4 pr-[10px]">
         <div className="relative aspect-[633/134] w-full">
           <Image
             src="/bento-grid/assets/images/illustration-multiple-platforms.webp"
@@ -60,16 +60,41 @@ function Main() {
         <div className="relative mt-4 aspect-[414/314] w-[208px]">
           <Image
             src="/bento-grid/assets/images/illustration-consistent-schedule.webp"
-            alt="Illustration Multiple Platforms"
+            alt="Illustration Consistent Schedule"
             fill
           />
         </div>
       </section>
+      <section className="bg-bento-purple-100 flex min-h-[469px] w-full flex-col items-center justify-center overflow-hidden rounded-lg p-[18.25px] py-8">
+        <h2 className="text-bento-black text-center text-[26px] font-medium leading-[24px] -tracking-[1.5px]">
+          Schedule to social media.
+        </h2>
+        <div className="relative mt-6 aspect-[715/638] w-full">
+          <Image
+            src="/bento-grid/assets/images/illustration-schedule-posts.webp"
+            alt="Illustration Schedule Posts"
+            fill
+          />
+        </div>
+        <p className="text-bento-black mt-[23px] px-2 text-center text-lg leading-[1.125]">
+          Optimize post timings to publish content at the perfect time for your
+          audience.
+        </p>
+      </section>
+      <section className="bg-bento-purple-500 flex h-[372px] w-full flex-col items-center justify-center overflow-hidden rounded-lg p-4 py-[23px]">
+        <div className="relative aspect-[456/402] w-[228px]">
+          <Image
+            src="/bento-grid/assets/images/illustration-grow-followers.webp"
+            alt="Illustration Grow Followers"
+            fill
+          />
+        </div>
+        <h2 className="text-bento-white mt-[41px] w-[240px] text-center text-[31px] font-medium leading-[28px] -tracking-[1.5px]">
+          Grow followers with non-stop content.
+        </h2>
+      </section>
       {/* {`
-        Schedule to social media.
-        Optimize post timings to publish content at the perfect time for your audience.
         
-        Grow followers with non-stop content.
 
         >56% faster audience growth
         
