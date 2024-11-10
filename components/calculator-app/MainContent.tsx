acff48e8dba86b4eddfa7913a1a31548197d825e
muflihanto <muflihanto@gmail.com>
2024-11-10 14:53:07 +0700

    calculator-app: update TODO comments

--- components/calculator-app/MainContent.tsx
+++ components/calculator-app/MainContent.tsx
@@ -104,13 +104,8 @@ export default function MainContent() {
   );
 }
 
-// TODO:
-// - See the size of the elements adjust based on their device's screen size
-// - Perform mathematical operations like addition, subtraction, multiplication, and division
-// - Adjust the color theme based on their preference
-// - **Bonus**: Have their initial theme preference checked using `prefers-color-scheme` and have any additional changes saved in the browser
-
-// TODO: Format display
+// TODO: **Bonus**: Have their initial theme preference checked using `prefers-color-scheme` and have any additional changes saved in the browser
+// TODO: Format display with comma separator
 
 function Header() {
   const classes = useAtomValue(themeClassAtom);
