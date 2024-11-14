7dbb1e74813a1fec2500d2a07d43bd8404622521
muflihanto <muflihanto@gmail.com>
2024-11-14 18:57:26 +0700

    bento-grid: improve styling

--- pages/bento-grid.tsx
+++ pages/bento-grid.tsx
@@ -25,7 +25,7 @@ export default function BentoGrid() {
 function Main() {
   return (
     <div className="grid h-full w-full grid-cols-[repeat(1,minmax(0,400px))] place-content-center gap-8 bg-bento-purple-100/25 px-4 pb-[41px] pt-[32px] md:grid-cols-[repeat(2,minmax(0,400px))] md:py-16 lg:grid-cols-[repeat(4,minmax(0,256px))] lg:grid-rows-[324px,247px,247px] lg:pb-[64px] lg:pt-[83px]">
-      <section className="flex h-[290px] w-full flex-col items-center justify-center rounded-lg bg-bento-purple-500 p-6 pt-[28px] md:col-span-2 lg:col-start-2 lg:h-full">
+      <section className="flex h-[290px] w-full flex-col items-center justify-center rounded-[10px] bg-bento-purple-500 p-6 pt-[28px] md:col-span-2 lg:col-start-2 lg:h-full">
         <h1 className="text-center text-[45px] font-medium leading-[42px] -tracking-[1.25px] text-bento-white lg:text-[62px] lg:leading-[58px] lg:-tracking-[3px]">
           Social Media <span className="text-bento-yellow-500">10x</span>{" "}
           <span className="italic">Faster</span> with AI
@@ -41,7 +41,7 @@ function Main() {
           Over 4,000 5-star reviews
         </p>
       </section>
-      <section className="flex min-h-[163px] w-full flex-col items-center justify-center rounded-lg bg-bento-white py-4 pl-4 pr-[10px] lg:col-start-2 lg:row-start-2 lg:overflow-hidden lg:p-6">
+      <section className="flex min-h-[163px] w-full flex-col items-center justify-center rounded-[10px] bg-bento-white py-4 pl-4 pr-[10px] lg:col-start-2 lg:row-start-2 lg:overflow-hidden lg:p-6">
         <div className="relative aspect-[633/134] w-full lg:h-[67px] lg:w-auto lg:shrink-0 lg:self-start">
           <Image
             src="/bento-grid/assets/images/illustration-multiple-platforms.webp"
@@ -53,7 +53,7 @@ function Main() {
           Manage multiple accounts and platforms.
         </h2>
       </section>
-      <section className="flex h-[216px] w-full flex-col items-start overflow-hidden rounded-lg bg-bento-yellow-500 p-4 pb-0 lg:col-start-3 lg:row-start-2 lg:h-full lg:p-6">
+      <section className="flex h-[216px] w-full flex-col items-start overflow-hidden rounded-[10px] bg-bento-yellow-500 p-4 pb-0 lg:col-start-3 lg:row-start-2 lg:h-full lg:p-6">
         <h2 className="text-left text-[26px] font-medium leading-[24px] -tracking-[1.5px] text-bento-black lg:text-[32px] lg:leading-[28px] lg:-tracking-[2px]">
           Maintain a consistent posting schedule.
         </h2>
@@ -65,7 +65,7 @@ function Main() {
           />
         </div>
       </section>
-      <section className="flex min-h-[469px] w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-bento-purple-100 p-[18.25px] py-8 lg:row-span-2 lg:items-start lg:p-8">
+      <section className="flex min-h-[469px] w-full flex-col items-center justify-center overflow-hidden rounded-[10px] bg-bento-purple-100 p-[18.25px] py-8 lg:row-span-2 lg:items-start lg:p-8">
         <h2 className="text-center text-[26px] font-medium leading-[24px] -tracking-[1.5px] text-bento-black lg:text-left lg:text-[32px] lg:leading-[28px] lg:-tracking-[2px]">
           Schedule to social media.
         </h2>
@@ -81,7 +81,7 @@ function Main() {
           audience.
         </p>
       </section>
-      <section className="flex h-[372px] w-full flex-col items-center justify-center overflow-hidden rounded-lg bg-bento-purple-500 p-4 py-[23px] md:col-span-2 md:flex-row lg:col-start-3 lg:row-start-3 lg:h-auto lg:justify-between lg:gap-3 lg:px-6">
+      <section className="flex h-[372px] w-full flex-col items-center justify-center overflow-hidden rounded-[10px] bg-bento-purple-500 p-4 py-[23px] md:col-span-2 md:flex-row lg:col-start-3 lg:row-start-3 lg:h-auto lg:justify-between lg:gap-3 lg:px-6">
         <div className="relative aspect-[456/402] w-[228px] md:w-[300px] lg:w-[228px] lg:shrink-0">
           <Image
             src="/bento-grid/assets/images/illustration-grow-followers.webp"
@@ -93,7 +93,7 @@ function Main() {
           Grow followers with non-stop content.
         </h2>
       </section>
-      <section className="flex h-[215px] w-full flex-col items-start overflow-hidden rounded-lg bg-bento-white p-6 pt-[22px] md:h-full lg:col-start-2 lg:row-start-3 lg:pt-[21px]">
+      <section className="flex h-[215px] w-full flex-col items-start overflow-hidden rounded-[10px] bg-bento-white p-6 pt-[22px] md:h-full lg:col-start-2 lg:row-start-3 lg:pt-[21px]">
         <h2 className="flex items-center text-[46px] font-medium leading-none text-bento-black lg:text-[62px]">
           <span className="-translate-y-[4.5px] lg:-translate-y-[4.9px]">
             {">"}
@@ -114,7 +114,7 @@ function Main() {
         </div>
       </section>
       <div className="grid w-full grid-cols-1 gap-8 md:col-span-2 md:contents md:grid-cols-2 lg:col-span-1 lg:col-start-1 lg:row-span-3 lg:row-start-1 lg:grid lg:grid-cols-1">
-        <section className="flex h-[234px] w-full flex-col items-start overflow-hidden rounded-lg bg-bento-yellow-100 p-8 pr-[30px] lg:col-start-1 lg:row-start-1 lg:h-[415px] lg:justify-center lg:pr-[28px]">
+        <section className="flex h-[234px] w-full flex-col items-start overflow-hidden rounded-[10px] bg-bento-yellow-100 p-8 pr-[30px] lg:col-start-1 lg:row-start-1 lg:h-[415px] lg:justify-center lg:pr-[28px]">
           <h2 className="text-[32px] font-medium leading-[28px] -tracking-[1.8px] text-bento-black lg:text-[39px] lg:leading-[36.5px] lg:-tracking-[3.5px]">
             Create and schedule content{" "}
             <span className="italic text-bento-purple-500">quicker</span>.
@@ -127,7 +127,7 @@ function Main() {
             />
           </div>
         </section>
-        <section className="flex h-[351px] w-full flex-col items-start overflow-hidden rounded-lg bg-bento-yellow-500 p-6 md:col-start-2 md:row-start-3 md:h-full lg:col-start-1 lg:row-start-2 lg:h-[434px]">
+        <section className="flex h-[351px] w-full flex-col items-start overflow-hidden rounded-[10px] bg-bento-yellow-500 p-6 md:col-start-2 md:row-start-3 md:h-full lg:col-start-1 lg:row-start-2 lg:h-[434px]">
           <h2 className="text-[32px] font-medium leading-[28px] -tracking-[1.8px] text-bento-black lg:text-[39px] lg:leading-[36px] lg:-tracking-[3.5px]">
             Write your content using AI.
           </h2>
