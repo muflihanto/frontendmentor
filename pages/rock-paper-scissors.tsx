d59ccaa066791ee16f070bfbf803581f95bcda88
muflihanto <muflihanto@gmail.com>
2024-11-18 10:53:27 +0700

    rock-paper-scissors: update todo comments

--- pages/rock-paper-scissors.tsx
+++ pages/rock-paper-scissors.tsx
@@ -16,10 +16,6 @@ import { barlowSemiCondensed } from "../utils/fonts/barlowSemiCondensed";
  * TODO:
  * Your users should be able to:
  * - View the optimal layout for the game depending on their device's screen size
- * - Play Rock, Paper, Scissors against the computer
- * - Maintain the state of the score after refreshing the browser _(optional)_
- * - **Bonus**: Play Rock, Paper, Scissors, Lizard, Spock against the computer _(optional)_
- *
  * ### Rules
  * If the player wins, they gain 1 point. If the computer wins, the player loses one point.
  */
