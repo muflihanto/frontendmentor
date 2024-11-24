964515604053fa4eac3b1e74765db87d53b90e4f
muflihanto <muflihanto@gmail.com>
2024-11-24 20:26:56 +0700

    recipe-page: align listitem markers vertically

--- pages/recipe-page.tsx
+++ pages/recipe-page.tsx
@@ -2,6 +2,8 @@ import Head from "next/head";
 import Image from "next/image";
 import { youngSerif } from "../utils/fonts/youngSerif";
 import { outfit } from "../utils/fonts/outfit";
+import { cn } from "../utils/cn";
+
 // import dynamic from "next/dynamic";
 // const Slider = dynamic(() => import("../components/Slider"), { ssr: false });
 
@@ -12,7 +14,7 @@ export default function RecipePage() {
         <title>Frontend Mentor | Recipe Page</title>
       </Head>
       <div
-        className={`App relative min-h-[100svh] ${outfit.className} md:bg-recipe-neutral-300 font-outfit md:flex md:flex-col md:items-center md:justify-center md:py-[123px]`}
+        className={`App relative min-h-[100svh] ${outfit.className} bg-white font-outfit md:flex md:flex-col md:items-center md:justify-center md:bg-recipe-neutral-300 md:py-[123px]`}
       >
         <Main />
         <Footer />
@@ -39,13 +41,17 @@ function Header() {
 
 function Preparation() {
   return (
-    <section className="bg-recipe-neutral-200 mt-[33px] rounded-lg px-6 pb-[22px] pt-5 md:px-7 md:pb-[26px] md:pt-6">
-      <h3 className="text-recipe-primary-dark-raspberry text-xl font-semibold">
+    <section className="mt-[33px] rounded-lg bg-recipe-neutral-200 px-6 pb-[22px] pt-5 md:px-7 md:pb-[26px] md:pt-6">
+      <h3 className="text-xl font-semibold text-recipe-primary-dark-raspberry">
         Preparation time
       </h3>
 
-      {/* FIXME: align the markers */}
-      <ul className="text-recipe-neutral-500 marker:text-recipe-primary-dark-raspberry mt-3 list-disc space-y-2 *:ml-[18px] *:pl-[22px] marker:inline-block marker:text-[12px]">
+      <ul
+        className={cn(
+          "mt-3 space-y-2 text-recipe-neutral-500 *:ml-[18px] *:pl-[22px]",
+          "[&_li::before]:absolute [&_li::before]:-left-[10px] [&_li::before]:top-1/2 [&_li::before]:block [&_li::before]:h-1 [&_li::before]:w-1 [&_li::before]:-translate-y-[80%] [&_li::before]:rounded-full [&_li::before]:bg-recipe-primary-dark-raspberry [&_li::before]:content-[''] [&_li]:relative",
+        )}
+      >
         <li>
           <strong>Total</strong>: Approximately 10 minutes
         </li>
@@ -64,12 +70,17 @@ function Ingredients() {
   return (
     <section className="my-[27px]">
       <h2
-        className={`text-recipe-primary-nutmeg font-young-serif text-[28px] ${youngSerif.className}`}
+        className={`font-young-serif text-[28px] text-recipe-primary-nutmeg ${youngSerif.className}`}
       >
         Ingredients
       </h2>
 
-      <ul className="marker:text-recipe-primary-nutmeg text-recipe-neutral-500 relative mt-4 list-disc space-y-2 *:ml-[18px] *:pl-[22px] marker:text-xs">
+      <ul
+        className={cn(
+          "relative mt-4 space-y-2 text-recipe-neutral-500 *:ml-[18px] *:pl-[22px]",
+          "[&_li::before]:absolute [&_li::before]:-left-[10px] [&_li::before]:top-1/2 [&_li::before]:block [&_li::before]:h-1 [&_li::before]:w-1 [&_li::before]:-translate-y-1/2 [&_li::before]:rounded-full [&_li::before]:bg-recipe-primary-dark-raspberry [&_li::before]:content-[''] [&_li]:relative",
+        )}
+      >
         <li>2-3 large eggs</li>
         <li>Salt, to taste</li>
         <li>Pepper, to taste</li>
@@ -86,12 +97,12 @@ function Instructions() {
   return (
     <section className="my-[25px]">
       <h2
-        className={`text-recipe-primary-nutmeg font-young-serif text-[28px] ${youngSerif.className}`}
+        className={`font-young-serif text-[28px] text-recipe-primary-nutmeg ${youngSerif.className}`}
       >
         Instructions
       </h2>
 
-      <ol className="marker:text-recipe-primary-nutmeg text-recipe-neutral-500 relative mt-4 list-decimal space-y-2 *:ml-[25px] *:pl-[15px] marker:font-outfit marker:font-bold">
+      <ol className="relative mt-4 list-decimal space-y-2 text-recipe-neutral-500 *:ml-[25px] *:pl-[15px] marker:font-outfit marker:font-bold marker:text-recipe-primary-nutmeg">
         <li>
           <strong>Beat the eggs</strong>: In a bowl, beat the eggs with a pinch
           of salt and pepper until they are well mixed. You can add a tablespoon
@@ -152,12 +163,12 @@ function Nutrition() {
   return (
     <section className="my-[25px]">
       <h2
-        className={`text-recipe-primary-nutmeg font-young-serif text-[28px] ${youngSerif.className}`}
+        className={`font-young-serif text-[28px] text-recipe-primary-nutmeg ${youngSerif.className}`}
       >
         Nutrition
       </h2>
 
-      <p className="text-recipe-neutral-500 mt-4">
+      <p className="mt-4 text-recipe-neutral-500">
         The table below shows nutritional values per serving without the
         additional fillings.
       </p>
@@ -167,7 +178,7 @@ function Nutrition() {
           return (
             <li key={`${index}-${name}`} className="w-full pl-8 pr-4 *:flex-1">
               <div className="text-recipe-neutral-500">{name}</div>
-              <div className="text-recipe-primary-dark-raspberry font-bold">
+              <div className="font-bold text-recipe-primary-dark-raspberry">
                 {value}
               </div>
             </li>
@@ -180,15 +191,15 @@ function Nutrition() {
 
 function Main() {
   return (
-    <div className="md:bg-recipe-neutral-100 md:w-[736px] md:overflow-hidden md:rounded-3xl md:p-10 md:pb-1">
+    <div className="md:w-[736px] md:overflow-hidden md:rounded-3xl md:bg-recipe-neutral-100 md:p-10 md:pb-1">
       <Header />
       <main className="p-8 pb-[28.5px] pt-[39px] md:p-0 md:pt-[40px]">
         <h1
-          className={`text-recipe-neutral-600 text-4xl leading-none ${youngSerif.className} font-young-serif md:text-[40px]/10`}
+          className={`text-4xl leading-none text-recipe-neutral-600 ${youngSerif.className} font-young-serif md:text-[40px]/10`}
         >
           Simple Omelette Recipe
         </h1>
-        <p className="text-recipe-neutral-500 mt-6 tracking-[-.025px]">
+        <p className="mt-6 tracking-[-.025px] text-recipe-neutral-500">
           An easy and quick dish, perfect for any meal. This classic omelette
           combines beaten eggs cooked to perfection, optionally filled with your
           choice of cheese, vegetables, or meats.
@@ -206,7 +217,7 @@ function Main() {
 
 function Footer() {
   return (
-    <footer className="text-recipe-neutral-600 absolute bottom-3 w-full text-center text-[11px] [&_a]:font-bold [&_a]:underline [&_a]:decoration-red-500 [&_a]:decoration-wavy">
+    <footer className="absolute bottom-3 w-full text-center text-[11px] text-recipe-neutral-600 [&_a]:font-bold [&_a]:underline [&_a]:decoration-red-500 [&_a]:decoration-wavy">
       Challenge by{" "}
       <a
         href="https://www.frontendmentor.io?ref=challenge"
