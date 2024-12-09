ad738e18d74b968b0dd9a5770165cf02df0965bb
muflihanto <muflihanto@gmail.com>
2024-12-09 16:26:23 +0700

    sunnyside-agency-landing-page: implement navigation menu accessibility pattern (2)

--- components/sunnyside-agency-landing-page/Header.tsx
+++ components/sunnyside-agency-landing-page/Header.tsx
@@ -60,18 +60,40 @@ export default function Header() {
           <ul
             className="invisible absolute left-1/2 top-[106px] flex w-[calc(100%-48px)] -translate-x-1/2 flex-col items-center justify-center gap-[27px] bg-sunny-neutral-100 pb-[38px] pt-[37px] before:absolute before:-top-[25px] before:right-0 before:z-20 before:h-0 before:w-0 before:border-b-[25px] before:border-r-[25px] before:border-t-[25px] before:border-b-transparent before:border-r-sunny-neutral-100 before:border-t-transparent before:content-[''] focus-visible:outline-none group-focus-within:visible
             lg:visible lg:static lg:left-0 lg:w-fit lg:translate-x-0 lg:flex-row lg:gap-[49px] lg:bg-transparent lg:px-1 lg:before:hidden"
+            role="menu"
+            id="menu"
           >
-            <li className="font-barlow text-[20px] -tracking-[0.1px] text-sunny-neutral-300 hover:text-sunny-primary-blue lg:h-[25px] lg:text-[18px] lg:font-medium lg:text-sunny-neutral-100">
-              <a href="">About</a>
+            <li
+              className="font-barlow text-[20px] -tracking-[0.1px] text-sunny-neutral-300 hover:text-sunny-primary-blue lg:h-[25px] lg:text-[18px] lg:font-medium lg:text-sunny-neutral-100"
+              role="none"
+            >
+              <a role="menuitem" href="">
+                About
+              </a>
             </li>
-            <li className="font-barlow text-[20px] -tracking-[0.1px] text-sunny-neutral-300 hover:text-sunny-primary-blue lg:h-[25px] lg:text-[18px] lg:font-medium lg:text-sunny-neutral-100">
-              <a href="">Services</a>
+            <li
+              className="font-barlow text-[20px] -tracking-[0.1px] text-sunny-neutral-300 hover:text-sunny-primary-blue lg:h-[25px] lg:text-[18px] lg:font-medium lg:text-sunny-neutral-100"
+              role="none"
+            >
+              <a role="menuitem" href="">
+                Services
+              </a>
             </li>
-            <li className="font-barlow text-[20px] -tracking-[0.1px] text-sunny-neutral-300 hover:text-sunny-primary-blue lg:h-[25px] lg:text-[18px] lg:font-medium lg:text-sunny-neutral-100">
-              <a href="">Projects</a>
+            <li
+              className="font-barlow text-[20px] -tracking-[0.1px] text-sunny-neutral-300 hover:text-sunny-primary-blue lg:h-[25px] lg:text-[18px] lg:font-medium lg:text-sunny-neutral-100"
+              role="none"
+            >
+              <a role="menuitem" href="">
+                Projects
+              </a>
             </li>
-            <li className="mr-[4px] mt-[2px] rounded-full bg-sunny-primary-yellow px-8 py-[17px] text-center font-fraunces text-[15px] font-bold uppercase text-sunny-neutral-500 hover:cursor-pointer lg:-ml-[2px] lg:mt-0 lg:bg-sunny-neutral-100 lg:font-extrabold lg:-tracking-[0.2px] lg:hover:bg-sunny-neutral-100/50 lg:hover:text-sunny-neutral-100">
-              <a href="">Contact</a>
+            <li
+              className="mr-[4px] mt-[2px] rounded-full bg-sunny-primary-yellow px-8 py-[17px] text-center font-fraunces text-[15px] font-bold uppercase text-sunny-neutral-500 hover:cursor-pointer lg:-ml-[2px] lg:mt-0 lg:bg-sunny-neutral-100 lg:font-extrabold lg:-tracking-[0.2px] lg:hover:bg-sunny-neutral-100/50 lg:hover:text-sunny-neutral-100"
+              role="none"
+            >
+              <a role="menuitem" href="">
+                Contact
+              </a>
             </li>
           </ul>
         </nav>
