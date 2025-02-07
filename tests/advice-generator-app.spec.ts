75dfdf9fd2b8808f8cc4b0e48cbfce26ce752ab0
muflihanto <muflihanto@gmail.com>
2025-02-07 14:03:06 +0700

    advice-generator-app: use @axe-core/playwright to scan automatically detectable accessibility issues

--- tests/advice-generator-app.spec.ts
+++ tests/advice-generator-app.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
 
 test.describe("FrontendMentor Challenge - Advice generator app Page", () => {
   /** Go to Advice generator app page before each test */
@@ -49,5 +50,15 @@ test.describe("FrontendMentor Challenge - Advice generator app Page", () => {
     await expect(
       page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
     ).toBeVisible();
+  });
+
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .disableRules(["color-contrast"])
+      .analyze();
+    // console.log({ violations: accessibilityScanResults.violations });
+    expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
