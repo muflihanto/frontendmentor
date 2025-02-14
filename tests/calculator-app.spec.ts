ef730fa5be66aff31e61831a7483484f13af04ee
muflihanto <muflihanto@gmail.com>
2025-02-14 13:19:51 +0700

    calculator-app: use @axe-core/playwright to scan automatically detectable accessibility issues

--- tests/calculator-app.spec.ts
+++ tests/calculator-app.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
 
 // TODO: add theme switcher test case
 
@@ -75,5 +76,15 @@ test.describe("FrontendMentor Challenge - Calculator app Page", () => {
     await expect(
       page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
     ).toBeVisible();
+  });
+
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .disableRules(["color-contrast"])
+      .analyze();
+    // console.log({ violations: accessibilityScanResults.violations });
+    expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
