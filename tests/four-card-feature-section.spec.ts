982557447d446750e99f4b7bb09ad5c6dc7e0de3
muflihanto <muflihanto@gmail.com>
2025-02-25 14:56:25 +0700

    four-card-feature-section: use @axe-core/playwright to scan automatically detectable accessibility issues

--- tests/four-card-feature-section.spec.ts
+++ tests/four-card-feature-section.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
 
 test.describe("FrontendMentor Challenge - Four card feature section Page", () => {
   /** Go to Four card feature section page before each test */
@@ -59,5 +60,15 @@ test.describe("FrontendMentor Challenge - Four card feature section Page", () =>
     await expect(
       page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
     ).toBeVisible();
+  });
+
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .disableRules(["color-contrast"])
+      .analyze();
+    // console.log({ violations: accessibilityScanResults.violations });
+    expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
