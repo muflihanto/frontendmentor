b34157094c51bb4f19f3c9c21b114e8a0de04e81
muflihanto <muflihanto@gmail.com>
2025-02-26 18:00:03 +0700

    fylo-dark-theme-landing-page: use @axe-core/playwright to scan automatically detectable accessibility issues

--- tests/fylo-dark-theme-landing-page.spec.ts
+++ tests/fylo-dark-theme-landing-page.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
 
 test.describe("FrontendMentor Challenge - Fylo landing page with dark theme and features grid Page", () => {
   /** Go to Fylo landing page with dark theme and features grid page before each test */
@@ -243,5 +244,15 @@ test.describe("FrontendMentor Challenge - Fylo landing page with dark theme and 
     await expect(
       footer.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
     ).toBeVisible();
+  });
+
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .disableRules(["color-contrast"])
+      .analyze();
+    // console.log({ violations: accessibilityScanResults.violations });
+    expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
