83e6e600e4971872ff478710403b7226756772cb
muflihanto <muflihanto@gmail.com>
2025-03-01 13:06:28 +0700

    huddle-landing-page-with-alternating-feature-blocks: use @axe-core/playwright to scan automatically detectable accessibility issues

--- tests/huddle-landing-page-with-alternating-feature-blocks.spec.ts
+++ tests/huddle-landing-page-with-alternating-feature-blocks.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
 
 test.describe("FrontendMentor Challenge - Huddle landing page with alternating feature blocks Page", () => {
   /** Go to Huddle landing page with alternating feature blocks page before each test */
@@ -123,5 +124,15 @@ test.describe("FrontendMentor Challenge - Huddle landing page with alternating f
     await expect(
       footer.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
     ).toBeVisible();
+  });
+
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .disableRules(["color-contrast"])
+      .analyze();
+    // console.log({ violations: accessibilityScanResults.violations });
+    expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
