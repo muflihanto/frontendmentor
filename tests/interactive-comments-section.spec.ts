60363e947d529a77bb5fafe8c08fb405e1d4e383
muflihanto <muflihanto@gmail.com>
2025-03-06 00:57:51 +0700

    interactive-comments-section: use @axe-core/playwright to scan automatically detectable accessibility issues

--- tests/interactive-comments-section.spec.ts
+++ tests/interactive-comments-section.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
 
 import raw_data from "../public/interactive-comments-section/data.json";
 
@@ -124,5 +125,15 @@ test.describe("FrontendMentor Challenge - Interactive comments section Page", ()
     await expect(
       page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
     ).toBeVisible();
+  });
+
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .disableRules(["color-contrast"])
+      .analyze();
+    // console.log({ violations: accessibilityScanResults.violations });
+    expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
