36dd334c4d8d4b1993f8c8e6fd4912aea3598346
muflihanto <muflihanto@gmail.com>
2025-03-07 05:17:50 +0700

    interactive-rating-component: use @axe-core/playwright to scan automatically detectable accessibility issues

--- tests/interactive-rating-component.spec.ts
+++ tests/interactive-rating-component.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect, type Page, type Locator } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
 
 const pageUrl = "/interactive-rating-component";
 
@@ -105,5 +106,15 @@ test.describe("FrontendMentor Challenge - Interactive rating component Page", ()
     await expect(
       page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
     ).toBeVisible();
+  });
+
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .disableRules(["color-contrast"])
+      .analyze();
+    // console.log({ violations: accessibilityScanResults.violations });
+    expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
