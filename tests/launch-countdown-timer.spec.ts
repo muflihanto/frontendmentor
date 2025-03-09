96b97acd09028d577126b274352cf3f74503c1b3
muflihanto <muflihanto@gmail.com>
2025-03-10 04:39:49 +0700

    launch-countdown-timer: use @axe-core/playwright to scan automatically detectable accessibility issues

--- tests/launch-countdown-timer.spec.ts
+++ tests/launch-countdown-timer.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
 
 test.describe("FrontendMentor Challenge - Launch countdown timer Page", () => {
   /** Go to Launch countdown timer page before each test */
@@ -53,5 +54,15 @@ test.describe("FrontendMentor Challenge - Launch countdown timer Page", () => {
     await expect(
       page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
     ).toBeVisible();
+  });
+
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .disableRules(["color-contrast"])
+      .analyze();
+    // console.log({ violations: accessibilityScanResults.violations });
+    expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
