90083783922a8c4c86e0afa204e916b0f55114c0
muflihanto <muflihanto@gmail.com>
2025-03-14 16:20:06 +0700

    mortgage-repayment-calculator: use @axe-core/playwright to scan automatically detectable accessibility issues

--- tests/mortgage-repayment-calculator.spec.ts
+++ tests/mortgage-repayment-calculator.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
 
 test.describe("FrontendMentor Challenge - Mortgage Repayment Calculator Page", () => {
   /** Go to Mortgage Repayment Calculator page before each test */
@@ -121,5 +122,15 @@ test.describe("FrontendMentor Challenge - Mortgage Repayment Calculator Page", (
     await expect(
       page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
     ).toBeVisible();
+  });
+
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .disableRules(["color-contrast"])
+      .analyze();
+    // console.log({ violations: accessibilityScanResults.violations });
+    expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
