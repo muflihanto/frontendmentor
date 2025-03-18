da8ebd96093bf712db19d95326f9145a20fa9d04
muflihanto <muflihanto@gmail.com>
2025-03-19 05:12:28 +0700

    pricing-component-with-toggle: use @axe-core/playwright to scan automatically detectable accessibility issues

--- tests/pricing-component-with-toggle.spec.ts
+++ tests/pricing-component-with-toggle.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
 
 const prices = {
   basic: {
@@ -103,5 +104,15 @@ test.describe("FrontendMentor Challenge - Pricing component with toggle Page", (
     await expect(
       page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
     ).toBeVisible();
+  });
+
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .disableRules(["color-contrast"])
+      .analyze();
+    // console.log({ violations: accessibilityScanResults.violations });
+    expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
