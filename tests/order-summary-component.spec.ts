29b7e89999c602c88755f1dc346f9abb575c462d
muflihanto <muflihanto@gmail.com>
2025-03-18 13:34:19 +0700

    order-summary-component: use @axe-core/playwright to scan automatically detectable accessibility issues

--- tests/order-summary-component.spec.ts
+++ tests/order-summary-component.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
 
 test.describe("FrontendMentor Challenge - Order summary card Page", () => {
   /** Go to Order summary card page before each test */
@@ -52,5 +53,15 @@ test.describe("FrontendMentor Challenge - Order summary card Page", () => {
     // Test hover state
     await button.hover();
     await expect(button).toHaveCSS("background-color", "rgb(117, 106, 241)");
+  });
+
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .disableRules(["color-contrast"])
+      .analyze();
+    // console.log({ violations: accessibilityScanResults.violations });
+    expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
