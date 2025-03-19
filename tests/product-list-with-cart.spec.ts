a4ae2e5c5e3b1417c75e5eac8956ae8c29b7cc98
muflihanto <muflihanto@gmail.com>
2025-03-20 02:53:15 +0700

    product-list-with-cart: use @axe-core/playwright to scan automatically detectable accessibility issues

--- tests/product-list-with-cart.spec.ts
+++ tests/product-list-with-cart.spec.ts
@@ -1,6 +1,7 @@
 import { test, expect } from "@playwright/test";
 import _products from "../public/product-list-with-cart/data.json";
 import type { Product } from "../pages/product-list-with-cart";
+import AxeBuilder from "@axe-core/playwright";
 const products = _products as Product[];
 
 test.describe("FrontendMentor Challenge - Product list with cart page", () => {
@@ -245,5 +246,15 @@ test.describe("FrontendMentor Challenge - Product list with cart page", () => {
     await expect(
       page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
     ).toBeVisible();
+  });
+
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .disableRules(["color-contrast"])
+      .analyze();
+    // console.log({ violations: accessibilityScanResults.violations });
+    expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
