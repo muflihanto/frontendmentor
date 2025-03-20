a4ccae3938126a491aa9e9c69d840d471a2f45b8
muflihanto <muflihanto@gmail.com>
2025-03-21 02:27:10 +0700

    product-preview-card-component: use @axe-core/playwright to scan automatically detectable accessibility issues

--- tests/product-preview-card-component.spec.ts
+++ tests/product-preview-card-component.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
 
 test.describe("FrontendMentor Challenge - Product preview card component page", () => {
   /** Go to "Product preview card component" page before each test */
@@ -84,5 +85,15 @@ test.describe("FrontendMentor Challenge - Product preview card component page", 
     await expect(
       page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
     ).toBeVisible();
+  });
+
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .disableRules(["color-contrast"])
+      .analyze();
+    // console.log({ violations: accessibilityScanResults.violations });
+    expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
