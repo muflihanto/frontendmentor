6a0b9abb2122bba94be44cf897e421e67f61d204
muflihanto <muflihanto@gmail.com>
2025-03-23 02:04:43 +0700

    qr-code-component: use @axe-core/playwright to scan automatically detectable accessibility issues

--- tests/qr-code-component.spec.ts
+++ tests/qr-code-component.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
 
 test.describe("FrontendMentor Challenge - QR code component Page", () => {
   /** Go to QR code component page before each test */
@@ -42,5 +43,15 @@ test.describe("FrontendMentor Challenge - QR code component Page", () => {
     await expect(
       page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
     ).toBeVisible();
+  });
+
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .disableRules(["color-contrast"])
+      .analyze();
+    // console.log({ violations: accessibilityScanResults.violations });
+    expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
