f4aafb0d7078ff87403ff267ec2fb026cc8caefe
muflihanto <muflihanto@gmail.com>
2025-03-26 18:54:26 +0700

    results-summary-component: use @axe-core/playwright to scan automatically detectable accessibility issues

--- tests/results-summary-component.spec.ts
+++ tests/results-summary-component.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
 
 test.describe("FrontendMentor Challenge - Results summary component Page", () => {
   /** Go to results summary component page before each test */
@@ -40,5 +41,15 @@ test.describe("FrontendMentor Challenge - Results summary component Page", () =>
     await expect(
       page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
     ).toBeVisible();
+  });
+
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .disableRules(["color-contrast"])
+      .analyze();
+    // console.log({ violations: accessibilityScanResults.violations });
+    expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
