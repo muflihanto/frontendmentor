ab1d6fa7b01407e4884c52947ee3d07a8ca5c8a9
muflihanto <muflihanto@gmail.com>
2025-03-31 22:00:55 +0700

    social-media-dashboard-with-theme-switcher: use @axe-core/playwright to scan automatically detectable accessibility issues

--- tests/social-media-dashboard-with-theme-switcher.spec.ts
+++ tests/social-media-dashboard-with-theme-switcher.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
 
 const data = {
   facebook: {
@@ -196,5 +197,15 @@ test.describe("FrontendMentor Challenge - Social media dashboard with theme swit
     await expect(
       page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
     ).toBeVisible();
+  });
+
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .disableRules(["color-contrast"])
+      .analyze();
+    // console.log({ violations: accessibilityScanResults.violations });
+    expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
