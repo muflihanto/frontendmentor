d93c12fa2c7ac1dd7063b82a481ce2d9482ea649
muflihanto <muflihanto@gmail.com>
2025-04-02 05:52:27 +0700

    social-proof-section: use @axe-core/playwright to scan automatically detectable accessibility issues

--- tests/social-proof-section.spec.ts
+++ tests/social-proof-section.spec.ts
@@ -1,4 +1,5 @@
 import { test, expect } from "@playwright/test";
+import AxeBuilder from "@axe-core/playwright";
 
 test.describe("FrontendMentor Challenge - Social proof section Page", () => {
   /** Go to Social proof section page before each test */
@@ -63,5 +64,15 @@ test.describe("FrontendMentor Challenge - Social proof section Page", () => {
     await expect(
       page.getByText("Challenge by Frontend Mentor . Coded by Muflihanto."),
     ).toBeVisible();
+  });
+
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .disableRules(["color-contrast"])
+      .analyze();
+    // console.log({ violations: accessibilityScanResults.violations });
+    expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
