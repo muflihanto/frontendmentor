c9372fed47b10486ba2529f3db1601e33e947761
muflihanto <muflihanto@gmail.com>
2025-11-05 16:34:16 +0700

    conference-ticket-generator: import styling guide config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -987,6 +987,20 @@ export default {
           white: "hsl(0,0%,100%)",
           black: "hsl(0,0%,7%)",
         },
+        "conference-ticket-generator": {
+          neutral: {
+            0: "hsl(0,0%,100%)",
+            300: "hsl(252,6%,83%)",
+            500: "hsl(245,15%,58%)",
+            700: "hsl(245,19%,35%)",
+            900: "hsl(248,70%,10%)",
+          },
+          orange: {
+            500: "hsl(7,88%,67%)",
+            700: "hsl(7,71%,60%)",
+            gradient: "hsl(7,86%,67%)",
+          },
+        },
       },
       fontFamily: {
         "plus-jakarta": [
@@ -1119,6 +1133,11 @@ export default {
           "var(--font-young-serif)",
           "Young Serif",
           ...fontFamily.serif,
+        ],
+        inconsolata: [
+          "var(--font-inconsolata)",
+          "Inconsolata",
+          ...fontFamily.sans,
         ],
       },
     },
---
+++ utils/fonts/inconsolata.ts
@@ -0,0 +1,7 @@
+import localFont from "next/font/local";
+
+export const inconsolata = localFont({
+  src: "../../public/conference-ticket-generator/assets/fonts/Inconsolata-VariableFont_wdth,wght.ttf",
+  style: "normal",
+  variable: "--font-inconsolata",
+});
