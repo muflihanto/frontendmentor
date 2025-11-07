7f76fbd13d52b3408b080ab34f3292d667184a9d
muflihanto <muflihanto@gmail.com>
2025-11-07 16:16:02 +0700

    conference-ticket-generator: add header content

--- pages/conference-ticket-generator.tsx
+++ pages/conference-ticket-generator.tsx
@@ -1,6 +1,7 @@
 import dynamic from "next/dynamic";
 import Head from "next/head";
 import Image from "next/image";
+import { inconsolata } from "../utils/fonts/inconsolata";
 
 const Slider = dynamic(() => import("../components/SliderTs"), { ssr: false });
 
@@ -10,13 +11,15 @@ export default function ConferenceTicketGenerator() {
       <Head>
         <title>Frontend Mentor | Conference ticket generator</title>
       </Head>
-      <div className="App relative min-h-[100svh] overflow-x-hidden bg-white">
+      <div
+        className={`App relative min-h-[100svh] overflow-x-hidden bg-white bg-[url('/conference-ticket-generator/assets/images/background-mobile.png')] bg-cover bg-top font-inconsolata md:bg-[url('/conference-ticket-generator/assets/images/background-tablet.png')] lg:bg-[url('/conference-ticket-generator/assets/images/background-desktop.png')] ${inconsolata.variable}`}
+      >
         <Main />
         <Footer />
-        <Slider
+        {/* <Slider
           basePath="/conference-ticket-generator/design"
           absolutePath="/conference-ticket-generator/design/mobile-design-form.jpg"
-        />
+        /> */}
       </div>
     </>
   );
@@ -24,13 +27,25 @@ export default function ConferenceTicketGenerator() {
 
 function Main() {
   return (
-    <>
-      {`
-        <!-- Form starts -->
+    <main className="flex h-full w-full flex-col items-center px-6 py-[30px] text-[20px] leading-6 text-conference-ticket-generator-neutral-0">
+      <Image
+        src={"/conference-ticket-generator/assets/images/logo-full.svg"}
+        width={209}
+        height={30}
+        alt="Coding Conf"
+        className="scale-[80%]"
+      />
 
+      <h1 className="mt-[38px] text-center text-[30px] font-extrabold leading-8 tracking-[-0.035em]">
         Your Journey to Coding Conf 2025 Starts Here!
+      </h1>
 
-        Secure your spot at next year's biggest coding conference.
+      <p className="mt-[21px] text-center tracking-tight text-conference-ticket-generator-neutral-300">
+        Secure your spot at next year&rsquo;s biggest coding conference.
+      </p>
+
+      {/* 
+        <!-- Form starts -->
 
         Upload Avatar
         Drag and drop or click to upload
@@ -58,8 +73,8 @@ function Main() {
         Jan 31, 2025 / Austin, TX
 
         <!-- Generated tickets ends -->
-      `}
-    </>
+      */}
+    </main>
   );
 }
 
