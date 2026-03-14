ae8a748521f46367a957e2685595816d5ff481eb
muflihanto <muflihanto@gmail.com>
2026-03-14 10:15:52 +0700

    weather-app: use @axe-core/playwright to scan automatically detectable accessibility issues

--- tests/weather-app.spec.ts
+++ tests/weather-app.spec.ts
@@ -1,3 +1,4 @@
+import AxeBuilder from "@axe-core/playwright";
 import { expect, test } from "@playwright/test";
 
 test.describe("FrontendMentor Challenge - Weather App page", () => {
@@ -84,5 +85,15 @@ test.describe("FrontendMentor Challenge - Weather App page", () => {
     await expect(
       page.getByText("Challenge by Frontend Mentor. Coded by Muflihanto."),
     ).toBeVisible();
+  });
+
+  test("should not have any automatically detectable accessibility issues", async ({
+    page,
+  }) => {
+    const accessibilityScanResults = await new AxeBuilder({ page })
+      .disableRules(["color-contrast"])
+      .analyze();
+    // console.log({ violations: accessibilityScanResults.violations });
+    expect(accessibilityScanResults.violations).toEqual([]);
   });
 });
